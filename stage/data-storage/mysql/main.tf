355d1b70d2313ea25616363698c5b1bde0e14e2d
Martin Fenner <mfenner@datacite.org>
2018-01-02 14:51:19 +0100

    added test-db management

---
+++ stage/data-storage/mysql/main.tf
@@ -0,0 +1,114 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+  version    = "~> 1.6"
+}
+
+resource "aws_db_instance" "db-test" {
+  identifier = "db-test"
+  snapshot_identifier = "db-labs-final-snapshot"
+  storage_type = "gp2"
+  allocated_storage = 300
+  engine = "mysql"
+  engine_version = "5.7.19"
+  instance_class = "db.m4.large"
+  username = "${var.mysql_user}"
+  password = "${var.mysql_password}"
+  maintenance_window = "thu:00:00-thu:00:30"
+  backup_window = "01:00-01:30"
+  backup_retention_period = 8
+  availability_zone = "eu-west-1a"
+  db_subnet_group_name = "datacite"
+  vpc_security_group_ids = ["${data.aws_security_group.datacite-private.id}"]
+  parameter_group_name = "${aws_db_parameter_group.datacite-test.id}"
+  auto_minor_version_upgrade = "true"
+  allow_major_version_upgrade = "true"
+  final_snapshot_identifier = "db-test-final-snapshot"
+  tags {
+    Name = "test"
+  }
+  lifecycle {
+      prevent_destroy = "true"
+  }
+  apply_immediately = "true"
+}
+
+resource "aws_db_parameter_group" "datacite-test" {
+    name = "datacite-test"
+    family = "mysql5.7"
+    description = "RDS datacite-test parameter group"
+
+    parameter {
+      name = "character_set_server"
+      value = "utf8"
+    }
+
+    parameter {
+      name = "collation_server"
+      value = "utf8_unicode_ci"
+    }
+
+    parameter {
+      name = "character_set_client"
+      value = "utf8"
+    }
+
+    parameter {
+      name = "max_allowed_packet"
+      value = 50000000
+    }
+
+    parameter {
+      name = "read_only"
+      value = "0"
+    }
+
+    parameter {
+      name = "slow_query_log"
+      value = "1"
+    }
+
+    parameter {
+      name = "log_queries_not_using_indexes"
+      value = "1"
+    }
+
+    parameter {
+      name = "log_throttle_queries_not_using_indexes"
+      value = "0"
+    }
+}
+
+data "aws_security_group" "datacite-private" {
+  id = "${var.security_group_id}"
+}
+
+data "aws_route53_zone" "internal" {
+  name         = "datacite.org"
+  private_zone = true
+}
+
+resource "aws_route53_record" "internal-db-test-ec2" {
+  zone_id = "${data.aws_route53_zone.internal.zone_id}"
+  name = "db.test.ec2.datacite.org"
+  type = "CNAME"
+  ttl = "3600"
+  records = ["${aws_db_instance.db-test.address}"]
+}
+
+resource "aws_route53_record" "internal-db-test" {
+  zone_id = "${data.aws_route53_zone.internal.zone_id}"
+  name = "db.test.datacite.org"
+  type = "CNAME"
+  ttl = "30"
+  records = ["${aws_db_instance.db-test.address}"]
+}
+
+resource "aws_route53_record" "rds-test" {
+  zone_id = "${data.aws_route53_zone.internal.zone_id}"
+  name = "test.rds.datacite.org"
+  type = "CNAME"
+  ttl = "3600"
+  records = ["${aws_db_instance.db-test.address}"]
+}
---
+++ stage/data-storage/mysql/terraform.tf
@@ -0,0 +1,7 @@
+terraform {
+  required_version = ">= 0.11"
+
+  backend "atlas" {
+    name         = "datacite-ng/stage-data-storage-mysql"
+  }
+}
