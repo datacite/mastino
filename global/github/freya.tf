3095709766c700d9b64256181b448d4ec7ea18d7
Martin Fenner <mfenner@datacite.org>
2018-01-02 10:04:32 +0100

    generate team with terraform

--- global/github/freya.tf
+++ global/github/freya.tf
@@ -1,33 +1,35 @@
-data "github_team" "freya" {
-  slug = "freya"
+resource "github_team" "freya" {
+  name        = "freya"
+  description = "The Freya Team"
+  privacy     = "secret"
 }
 
 resource "github_team_membership" "freya_membership_mfenner" {
-  team_id  = "${data.github_team.freya.id}"
+  team_id  = "${github_team.freya.id}"
   username = "mfenner"
   role     = "maintainer"
 }
 
 resource "github_team_membership" "freya_membership_kjgarza" {
-  team_id  = "${data.github_team.freya.id}"
+  team_id  = "${github_team.freya.id}"
   username = "kjgarza"
   role     = "maintainer"
 }
 
 resource "github_team_membership" "freya_membership_pcruse" {
-  team_id  = "${data.github_team.freya.id}"
+  team_id  = "${github_team.freya.id}"
   username = "pcruse"
   role     = "maintainer"
 }
 
 resource "github_team_membership" "freya_membership_brittadreyer" {
-  team_id  = "${data.github_team.freya.id}"
+  team_id  = "${github_team.freya.id}"
   username = "brittadreyer"
   role     = "member"
 }
 
 resource "github_team_membership" "freya_membership_richardhallett" {
-  team_id  = "${data.github_team.freya.id}"
+  team_id  = "${github_team.freya.id}"
   username = "richardhallett"
   role     = "maintainer"
 }
