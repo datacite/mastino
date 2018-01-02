c62c44eaf721f6ab9fba469707666892daae2eda
Martin Fenner <mfenner@datacite.org>
2018-01-02 22:36:46 +0100

    stage memcached

---
+++ stage/data-storage/memcached/main.tf
@@ -0,0 +1,30 @@
+resource "aws_elasticache_cluster" "memcached-stage" {
+    cluster_id = "memcached-stage"
+    engine = "memcached"
+    engine_version = "1.4.24"
+    node_type = "cache.m3.medium"
+    port = 11211
+    num_cache_nodes = 1
+    security_group_ids = ["${data.aws_security_group.datacite-private.id}"]
+    /* subnet_group_name = "${aws_elasticache_subnet_group.datacite.name}" */
+    apply_immediately = true
+    lifecycle {
+      prevent_destroy = "true"
+    }
+}
+
+resource "aws_route53_record" "memcached-test" {
+    zone_id = "${data.aws_route53_zone.internal.zone_id}"
+    name = "memcached.test.datacite.org"
+    type = "CNAME"
+    ttl = "${var.ttl}"
+    records = ["${aws_elasticache_cluster.memcached-stage.cache_nodes.0.address}"]
+}
+
+resource "aws_route53_record" "memcached-stage" {
+    zone_id = "${data.aws_route53_zone.internal.zone_id}"
+    name = "memcached.stage.datacite.org"
+    type = "CNAME"
+    ttl = "${var.ttl}"
+    records = ["${aws_elasticache_cluster.memcached-stage.cache_nodes.0.address}"]
+}
---
+++ stage/data-storage/memcached/var.tf
@@ -0,0 +1,11 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+    default = "eu-west-1"
+}
+
+variable "security_group_id" {}
+
+variable "ttl" {
+  default = "300"
+}
