03c9232a5d0778929a27ba6c073903a464b65a4a
Martin Fenner <mfenner@datacite.org>
2018-01-02 21:42:52 +0100

    fix variable names

--- stage/services/assets/input.tf
+++ stage/services/assets/input.tf
@@ -12,11 +12,11 @@ provider "aws" {
   alias = "use1"
 }
 
-data "template_file" "blog-stage" {
+data "template_file" "assets-stage" {
   template = "${file("s3_cloudfront.json")}"
 
   vars {
-    bucket_name = "blog.stage.datacite.org"
+    bucket_name = "assets.stage.datacite.org"
   }
 }
 
