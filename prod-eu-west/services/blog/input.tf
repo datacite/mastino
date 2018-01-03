bc49f546116ae667efdbbecf775796ca2c40c0ef
Martin Fenner <mfenner@datacite.org>
2018-01-03 09:25:02 +0100

    prod blog

---
+++ prod-eu-west/services/blog/input.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+provider "aws" {
+  # us-east-1 instance
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "us-east-1"
+  alias = "use1"
+}
+
+data "template_file" "blog" {
+  template = "${file("s3_cloudfront.json")}"
+
+  vars {
+    bucket_name = "blog.datacite.org"
+  }
+}
+
+data "aws_acm_certificate" "cloudfront" {
+  provider = "aws.use1"
+  domain = "*.datacite.org"
+  statuses = ["ISSUED"]
+}
+
+data "aws_route53_zone" "production" {
+  name         = "datacite.org"
+}
+
+data "aws_route53_zone" "internal" {
+  name         = "datacite.org"
+  private_zone = true
+}
+
+data "aws_s3_bucket" "logs" {
+  bucket = "logs.datacite.org"
+}
