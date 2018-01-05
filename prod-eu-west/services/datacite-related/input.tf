fad9da11efca5436592d2113480474d9b549a64a
Martin Fenner <mfenner@datacite.org>
2018-01-05 15:54:31 +0100

    prod datacite-related

---
+++ prod-eu-west/services/datacite-related/input.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+data "template_file" "datacite_related_task" {
+  template = "${file("datacite-related.json")}"
+
+  vars {
+    access_token = "${var.access_token}"
+    source_token = "${var.source_token}"
+    push_url     = "${var.push_url}"
+    webhook_url  = "${var.webhook_url}"
+  }
+}
+data "aws_iam_role" "lambda" {
+  name = "lambda"
+}
+
+data "aws_security_group" "datacite-private" {
+  id = "${var.security_group_id}"
+}
+
+data "aws_subnet" "datacite-private" {
+  id = "${var.subnet_datacite-private_id}"
+}
+
+data "aws_subnet" "datacite-alt" {
+  id = "${var.subnet_datacite-alt_id}"
+}
