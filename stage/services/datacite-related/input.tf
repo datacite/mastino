db666170af8515e0999ece74d419876d5af21cb4
Martin Fenner <mfenner@datacite.org>
2018-01-05 15:27:19 +0100

    stage datacite-related

---
+++ stage/services/datacite-related/input.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+data "template_file" "datacite_related_test_task" {
+  template = "${file("datacite-related.json")}"
+}
+data "aws_iam_role" "lambda" {
+  name = "lambda"
+}
+
+data "aws_security_group" "datacite-private" {
+  id = "${var.security_group_id}"
+}
+
+data "aws_subnet" "datacite-private" {
+  id = "${var.subnet_datacite-private_id}"
+}
+
+data "aws_subnet" "datacite-alt" {
+  id = "${var.subnet_datacite-alt_id}"
+}
---
+++ stage/services/datacite-related/main.tf
@@ -0,0 +1,43 @@
+resource "aws_ecs_task_definition" "datacite-related-test" {
+  family = "datacite-related-test"
+  container_definitions =  "${data.template_file.datacite_related_test_task.rendered}"
+}
+
+resource "aws_cloudwatch_event_rule" "datacite-related-test" {
+  name = "datacite-related-test"
+  description = "Run datacite-related container via cron"
+  schedule_expression = "cron(40 4 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "datacite-related-test" {
+  target_id = "datacite-related-test"
+  rule = "${aws_cloudwatch_event_rule.datacite-related-test.name}"
+  arn = "${aws_lambda_function.datacite-related-test.arn}"
+}
+
+resource "aws_lambda_function" "datacite-related-test" {
+  filename = "files/ecs_task_runner.js.zip"
+  function_name = "datacite-related-test"
+  role = "${data.aws_iam_role.lambda.arn}"
+  handler = "ecs_task_runner.handler"
+  runtime = "nodejs4.3"
+  vpc_config {
+    subnet_ids = ["${data.aws_subnet.datacite-private.id}", "${data.aws_subnet.datacite-alt.id}"]
+    security_group_ids = ["${data.aws_security_group.datacite-private.id}"]
+  }
+  environment {
+    variables = {
+      ecs_task_def = "datacite-related-test"
+      cluster = "test"
+      count = 1
+    }
+  }
+}
+
+resource "aws_lambda_permission" "datacite-related-test" {
+  statement_id = "AllowExecutionFromCloudWatch"
+  action = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.datacite-related-test.function_name}"
+  principal = "events.amazonaws.com"
+  source_arn = "${aws_cloudwatch_event_rule.datacite-related-test.arn}"
+}
