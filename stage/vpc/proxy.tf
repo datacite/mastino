0777f87106850fd4ad85ce2137680d403f610d51
Martin Fenner <mfenner@datacite.org>
2018-01-09 00:28:56 +0100

    shorten ttl

--- stage/vpc/proxy.tf
+++ stage/vpc/proxy.tf
@@ -2,7 +2,7 @@ resource "aws_route53_record" "internal-proxy" {
     zone_id = "${data.aws_route53_zone.internal.zone_id}"
     name = "proxy.datacite.org"
     type = "A"
-    ttl = "300"
+    ttl = "${var.ttl}"
     records = ["10.0.0.116"]
 }
 
@@ -10,7 +10,7 @@ resource "aws_route53_record" "ec2-proxy-public" {
     zone_id = "${data.aws_route53_zone.production.zone_id}"
     name = "proxy.ec2.datacite.org"
     type = "A"
-    ttl = "3600"
+    ttl = "${var.ttl}"
     records = ["52.208.28.107"]
 }
 
@@ -18,6 +18,6 @@ resource "aws_route53_record" "ec2-proxy" {
     zone_id = "${data.aws_route53_zone.internal.zone_id}"
     name = "proxy.ec2.datacite.org"
     type = "A"
-    ttl = "300"
+    ttl = "${var.ttl}"
     records = ["10.0.0.116"]
 }
