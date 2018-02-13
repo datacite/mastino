93b50b210528f15495fec2965d86e80d67932bf8
Martin Fenner <mfenner@datacite.org>
2018-02-13 06:40:48 +0100

    trigger update for lambda function changes

--- stage/services/set-state/main.tf
+++ stage/services/set-state/main.tf
@@ -16,6 +16,8 @@ resource "aws_lambda_function" "set-state-test" {
   role = "${data.aws_iam_role.lambda.arn}"
   handler = "set_state_runner.handler"
   runtime = "nodejs4.3"
+  source_code_hash = "${base64sha256(file("set_state_runner.js.zip"))"
+
   vpc_config {
     subnet_ids = ["${data.aws_subnet.datacite-private.id}", "${data.aws_subnet.datacite-alt.id}"]
     security_group_ids = ["${data.aws_security_group.datacite-private.id}"]
@@ -25,7 +27,6 @@ resource "aws_lambda_function" "set-state-test" {
       host     = "${var.host}"
       username = "${var.username}"
       password = "${var.password}"
-      version = 1
     }
   }
 }
