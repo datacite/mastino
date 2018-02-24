2f713dee6cb2f9111cc7857c2f116e45bc546b97
Martin Fenner <mfenner@datacite.org>
2018-02-24 16:16:46 +0100

    fixed variables

--- dev/services/message-queue/main.tf
+++ dev/services/message-queue/main.tf
@@ -1,39 +1,39 @@
-resource "aws_sqs_queue" "elastic-test" {
-  name                      = "stage_elastic"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-test.arn}\",\"maxReceiveCount\":4}"
+resource "aws_sqs_queue" "elastic-dev" {
+  name                      = "development_elastic"
+  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-dev.arn}\",\"maxReceiveCount\":4}"
 
   tags {
-    Environment = "test"
+    Environment = "dev"
   }
 }
 
-resource "aws_sqs_queue" "lupo-test" {
-  name                      = "stage_lupo"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-test.arn}\",\"maxReceiveCount\":4}"
+resource "aws_sqs_queue" "lupo-dev" {
+  name                      = "development_lupo"
+  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-dev.arn}\",\"maxReceiveCount\":4}"
 
   tags {
-    Environment = "test"
+    Environment = "dev"
   }
 }
 
-resource "aws_sqs_queue" "levriero-test" {
-  name                      = "stage_levriero"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-test.arn}\",\"maxReceiveCount\":4}"
+resource "aws_sqs_queue" "levriero-dev" {
+  name                      = "development_levriero"
+  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-dev.arn}\",\"maxReceiveCount\":4}"
 
   tags {
-    Environment = "test"
+    Environment = "dev"
   }
 }
 
-resource "aws_sqs_queue" "dead-letter-test" {
-  name                      = "stage_dead-letter"
+resource "aws_sqs_queue" "dead-letter-dev" {
+  name                      = "development_dead-letter"
 
   tags {
-    Environment = "test"
+    Environment = "dev"
   }
 }
 
-resource "aws_iam_policy" "sqs-test" {
-  name = "sqs-test"
-  policy = "${data.template_file.queue-test.rendered}"
+resource "aws_iam_policy" "sqs-dev" {
+  name = "sqs-dev"
+  policy = "${data.template_file.queue-dev.rendered}"
 }
