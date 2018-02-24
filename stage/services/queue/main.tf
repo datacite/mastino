4e38ada4b6e43c5b67faaf5f49c6e884cf7b15d8
Martin Fenner <mfenner@datacite.org>
2018-02-24 08:34:05 +0100

    define dead letter queue

--- stage/services/queue/main.tf
+++ stage/services/queue/main.tf
@@ -1,6 +1,14 @@
 resource "aws_sqs_queue" "elastic-test" {
   name                      = "elastic-test"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.terraform_queue_deadletter.arn}\",\"maxReceiveCount\":4}"
+  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-test.arn}\",\"maxReceiveCount\":4}"
+
+  tags {
+    Environment = "test"
+  }
+}
+
+resource "aws_sqs_queue" "dead-letter-test" {
+  name                      = "dead-letter-test"
 
   tags {
     Environment = "test"
