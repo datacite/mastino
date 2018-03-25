126e6c704f0743f29ac8c27689c61dc6cf021053
Martin Fenner <mfenner@datacite.org>
2018-03-25 23:15:24 +0200

    stage data

---
+++ stage/services/data/_content-negotiation.auto.tfvars
@@ -0,0 +1,4 @@
+content-negotiation_tags = {
+  sha = "eb66109"
+  version = "1.1.6"
+}
---
+++ stage/services/data/main.tf
@@ -0,0 +1,65 @@
+resource "aws_ecs_service" "data-stage" {
+  name = "data-stage"
+  cluster = "${data.aws_ecs_cluster.stage.id}"
+  task_definition = "${aws_ecs_task_definition.data-stage.arn}"
+  desired_count = 1
+  iam_role        = "${data.aws_iam_role.ecs_service.arn}"
+
+  placement_strategy {
+    type  = "binpack"
+    field = "cpu"
+  }
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.data-stage.id}"
+    container_name   = "data-stage"
+    container_port   = "80"
+  }
+}
+
+resource "aws_ecs_task_definition" "data-stage" {
+  family = "data-stage"
+  container_definitions =  "${data.template_file.data_task.rendered}"
+}
+
+resource "aws_lb_target_group" "data-stage" {
+  name     = "data-stage"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "${var.vpc_id}"
+
+  health_check {
+    path = "/heartbeat"
+  }
+}
+
+resource "aws_lb_listener_rule" "data-stage" {
+  listener_arn = "${aws_lb_listener.stage.arn}"
+  priority     = 60
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.data-stage.arn}"
+  }
+
+  condition {
+    field  = "host-header"
+    values = ["${aws_route53_record.data-stage.name}"]
+  }
+}
+
+resource "aws_route53_record" "data-stage" {
+    zone_id = "${data.aws_route53_zone.production.zone_id}"
+    name = "data.test.datacite.org"
+    type = "CNAME"
+    ttl = "${var.ttl}"
+    records = ["${data.aws_lb.stage.dns_name}"]
+}
+
+resource "aws_route53_record" "split-data-stage" {
+    zone_id = "${data.aws_route53_zone.internal.zone_id}"
+    name = "data.test.datacite.org"
+    type = "CNAME"
+    ttl = "${var.ttl}"
+    records = ["${data.aws_lb.stage.dns_name}"]
+}
