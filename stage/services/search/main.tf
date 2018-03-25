c9cf26d03e65a41e1bef98040b1e040bfc2e406d
Martin Fenner <mfenner@datacite.org>
2018-03-25 21:38:23 +0200

    fix variables

--- stage/services/search/main.tf
+++ stage/services/search/main.tf
@@ -15,7 +15,7 @@ resource "aws_ecs_service" "search-stage" {
   cluster = "${data.aws_ecs_cluster.stage.id}"
   task_definition = "${aws_ecs_task_definition.search-stage.arn}"
   desired_count = 1
-  iam_role        = "${aws_iam_role.ecs_service.arn}"
+  iam_role        = "${data.aws_iam_role.ecs_service.arn}"
 
   placement_strategy {
     type  = "binpack"
@@ -40,39 +40,25 @@ resource "aws_lb_target_group" "search-stage" {
   }
 }
 
-data "template_file" "search_stage_task" {
-  template = "${file("search-stage.json")}"
 
-  vars {
-    jwt_secret_key     = "${var.jwt_secret_key}"
-    jwt_public_key     = "${var.jwt_public_key}"
-    jwt_host           = "${var.jwt_host}"
-    orcid_update_uuid  = "${var.orcid_update_uuid}"
-    orcid_update_url   = "${var.orcid_update_url}"
-    orcid_update_token = "${var.orcid_update_token}"
-    api_url            = "${var.api_url}"
-    secret_key_base    = "${var.secret_key_base}"
-    version            = "${var.doi-metadata-search_tags["sha"]}"
-  }
-}
 
 resource "aws_ecs_task_definition" "search-stage" {
   family = "search-stage"
-  container_definitions =  "${data.template_file.search_test_task.rendered}"
+  container_definitions =  "${data.template_file.search_stage_task.rendered}"
 }
 
 resource "aws_route53_record" "search-stage" {
-   zone_id = "${aws_route53_zone.production.zone_id}"
+   zone_id = "${data.aws_route53_zone.production.zone_id}"
    name = "search.test.datacite.org"
    type = "CNAME"
    ttl = "${var.ttl}"
-   records = ["${aws_lb.test.dns_name}"]
+   records = ["${data.aws_lb.stage.dns_name}"]
 }
 
 resource "aws_route53_record" "split-search-stage" {
-   zone_id = "${aws_route53_zone.internal.zone_id}"
+   zone_id = "${data.aws_route53_zone.internal.zone_id}"
    name = "search.test.datacite.org"
    type = "CNAME"
    ttl = "${var.ttl}"
-   records = ["${aws_lb.test.dns_name}"]
+   records = ["${data.aws_lb.stage.dns_name}"]
 }
