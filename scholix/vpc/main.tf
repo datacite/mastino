9169587826bf64467b840dbf3169b8d88395fea0
Martin Fenner <mfenner@datacite.org>
2018-04-04 22:34:27 +0200

    scholix vpc

---
+++ scholix/vpc/main.tf
@@ -0,0 +1,34 @@
+resource "aws_route53_zone" "scholix" {
+  name = "scholix.org"
+
+  tags {
+    Environment = "production"
+  }
+}
+
+resource "aws_route53_record" "scholix-ns" {
+  zone_id = "${aws_route53_zone.scholix.zone_id}"
+  name = "${aws_route53_zone.scholix.name}"
+  type = "NS"
+  ttl = "300"
+  records = [
+    "${aws_route53_zone.scholix.name_servers.0}",
+    "${aws_route53_zone.scholix.name_servers.1}",
+    "${aws_route53_zone.scholix.name_servers.2}",
+    "${aws_route53_zone.scholix.name_servers.3}"
+  ]
+}
+
+resource "aws_route53_record" "mx-scholix" {
+  zone_id = "${aws_route53_zone.scholix.zone_id}"
+  name = "${aws_route53_zone.scholix.name}"
+  type = "MX"
+  ttl = "300"
+  records = [
+    "1 aspmx.l.google.com",
+    "5 alt1.aspmx.l.google.com",
+    "5 alt2.aspmx.l.google.com",
+    "10 aspmx2.googlemail.com",
+    "10 aspmx3.googlemail.com"
+  ]
+}
