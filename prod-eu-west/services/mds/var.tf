009ef5fa4d7fab07302fbd236debca54b3c41acb
Martin Fenner <martin.fenner@datacite.org>
2018-05-12 10:50:07 +0200

    new prod-eu-west mds

---
+++ prod-eu-west/services/mds/var.tf
@@ -0,0 +1,16 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+    default = "eu-west-1"
+}
+variable "vpc_id" {}
+
+variable "ttl" {
+  default = "300"
+}
+
+variable "lb_name" {
+  default = "lb"
+}
+
+variable "main_id" {}
