754abfecd862d8946ed4423c7cb31bba0e40d7de
Kristian Garza <kj.garza@gmail.com>
2018-05-16 17:23:48 +0200

    remove dependecy

--- prod-eu-west/services/metrics-api/main.tf
+++ prod-eu-west/services/metrics-api/main.tf
@@ -22,9 +22,6 @@ resource "aws_lb_target_group" "metrics-api" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = "${var.vpc_id}"
-  depends_on = [
-    "aws_lb_listener.default",
-  ]
 }
 
 resource "aws_lb_listener_rule" "metrics-api" {
