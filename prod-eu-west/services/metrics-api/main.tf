3e0377fb03afb9810206a02a5579a03898725d57
Kristian Garza <kj.garza@gmail.com>
2018-05-16 17:18:33 +0200

    added dependcy in elb

--- prod-eu-west/services/metrics-api/main.tf
+++ prod-eu-west/services/metrics-api/main.tf
@@ -22,6 +22,9 @@ resource "aws_lb_target_group" "metrics-api" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = "${var.vpc_id}"
+  depends_on = [
+    "aws_lb_listener.default",
+  ]
 }
 
 resource "aws_lb_listener_rule" "metrics-api" {
