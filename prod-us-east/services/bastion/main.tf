5c5bd4efb7a25341445bf5d00e563183035a0ef6
Martin Fenner <martin.fenner@datacite.org>
2018-05-16 09:01:40 +0200

    fix parameter

--- prod-us-east/services/bastion/main.tf
+++ prod-us-east/services/bastion/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "bastion-us" {
-  ami                         = "${var.ami["eu-east-1"]}"
+  ami                         = "${var.ami["us-east-1"]}"
   instance_type               = "t2.micro"
   vpc_security_group_ids      = ["${aws_security_group.bastion-us.id}"]
   subnet_id                   = "${data.aws_subnet.datacite-public.id}"
