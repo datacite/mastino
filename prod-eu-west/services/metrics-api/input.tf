8c69c29384831552019a54a696657881068548e9
Kristian Garza <kj.garza@gmail.com>
2018-05-16 13:31:55 +0200

    sashimi api to production

---
+++ prod-eu-west/services/metrics-api/input.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+data "aws_route53_zone" "production" {
+  name = "datacite.org"
+}
+
+data "aws_route53_zone" "internal" {
+  name = "datacite.org"
+  private_zone = true
+}
+
+data "aws_ecs_cluster" "default" {
+  cluster_name = "default"
+}
+
+data "aws_iam_role" "ecs_service" {
+  name = "ecs_service"
+}
+
+data "aws_lb" "default" {
+  name = "${var.lb_name}"
+}
+
+data "aws_lb_listener" "default" {
+  load_balancer_arn = "${data.aws_lb.default.arn}"
+  port = 443
+}
+
+data "template_file" "metrics-api_task" {
+  template = "${file("metrics-api.json")}"
+
+  vars {
+    jwt_public_key     = "${var.jwt_public_key}"
+    jwt_private_key    = "${var.jwt_private_key}"
+    memcache_servers   = "${var.memcache_servers}"
+    librato_email      = "${var.librato_email}"
+    librato_token      = "${var.librato_token}"
+    librato_suites     = "${var.librato_suites}"
+    access_key         = "${var.access_key}"
+    secret_key         = "${var.secret_key}"
+    region             = "${var.region}"
+    s3_bucket          = "${var.s3_bucket}"
+    mysql_database     = "${var.mysql_database}"
+    mysql_user         = "${var.mysql_user}"
+    mysql_password     = "${var.mysql_password}"
+    mysql_host         = "${var.mysql_host}"
+    bugsnag_key        = "${var.bugsnag_key}"
+    version            = "${var.sashimi_tags["version"]}"
+  }
+}
---
+++ prod-eu-west/services/metrics-api/main.tf
@@ -0,0 +1,53 @@
+resource "aws_ecs_service" "metrics-api" {
+  name            = "metrics-api"
+  cluster         = "${data.aws_ecs_cluster.default.id}"
+  task_definition = "${aws_ecs_task_definition.metrics-api.arn}"
+  desired_count   = 1
+  iam_role        = "${data.aws_iam_role.ecs_service.arn}"
+
+  placement_strategy {
+    type  = "binpack"
+    field = "cpu"
+  }
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.metrics-api.id}"
+    container_name   = "metrics-api"
+    container_port   = "80"
+  }
+}
+
+resource "aws_lb_target_group" "metrics-api" {
+  name     = "metrics-api"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "${var.vpc_id}"
+}
+
+resource "aws_lb_listener_rule" "metrics-api" {
+  listener_arn = "${data.aws_lb_listener.default.arn}"
+  priority     = 115
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.metrics-api.arn}"
+  }
+
+  condition {
+    field  = "host-header"
+    values = ["${aws_route53_record.metrics-api.name}"]
+  }
+}
+
+resource "aws_ecs_task_definition" "metrics-api" {
+  family = "metrics-api"
+  container_definitions =  "${data.template_file.metrics-api_task.rendered}"
+}
+
+resource "aws_route53_record" "metrics-api" {
+  zone_id = "${data.aws_route53_zone.production.zone_id}"
+  name = "metrics.datacite.org"
+  type = "CNAME"
+  ttl = "${var.ttl}"
+  records = ["${data.aws_lb.default.dns_name}"]
+}
