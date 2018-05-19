a724656720a2973784abc92a9cbea3b5a8d40664
Martin Fenner <martin.fenner@datacite.org>
2018-05-19 08:32:56 +0200

    upgrade nodejs in lambda tasks 4.3 -> 6.10. #20

--- prod-eu-west/services/sitemaps-generator/main.tf
+++ prod-eu-west/services/sitemaps-generator/main.tf
@@ -20,7 +20,7 @@ resource "aws_lambda_function" "sitemaps-generator" {
   function_name = "sitemaps-generator"
   role = "${data.aws_iam_role.lambda.arn}"
   handler = "ecs_task_runner.handler"
-  runtime = "nodejs4.3"
+  runtime = "nodejs6.10"
   vpc_config {
     subnet_ids = ["${data.aws_subnet.datacite-private.id}", "${data.aws_subnet.datacite-alt.id}"]
     security_group_ids = ["${data.aws_security_group.datacite-private.id}"]
