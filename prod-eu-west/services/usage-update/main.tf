42bea3e981f7934debf9438be3ee2961b0fe154b
Kristian Garza <kj.garza@gmail.com>
2018-05-21 18:56:19 +0200

    added missing variables

--- prod-eu-west/services/usage-update/main.tf
+++ prod-eu-west/services/usage-update/main.tf
@@ -6,7 +6,7 @@ resource "aws_ecs_task_definition" "usage-update" {
 resource "aws_cloudwatch_event_rule" "usage-update" {
   name = "usage-update"
   description = "Run usage-update container via cron"
-  schedule_expression = "cron(20	*	*	*	? *)"
+  schedule_expression = "cron(30	*	*	*	? *)"
 }
 
 resource "aws_cloudwatch_event_target" "usage-update" {
