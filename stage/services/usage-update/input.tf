cc5e6c9571ae7551ae76254fd399967b25ce0db7
Kristian Garza <kj.garza@gmail.com>
2018-05-21 16:51:23 +0200

    adding keys becasue we might not have access

--- stage/services/usage-update/input.tf
+++ stage/services/usage-update/input.tf
@@ -8,6 +8,8 @@ data "template_file" "usage_update_stage_task" {
   template = "${file("usage-update.json")}"
 
   vars {
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
     access_token = "${var.access_token}"
     source_token = "${var.source_token}"
     push_url     = "${var.push_url}"
