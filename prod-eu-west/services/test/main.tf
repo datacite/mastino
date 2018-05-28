a86df6f745b9d0b9ba21ddb97bfbe339c813f2a6
Martin Fenner <martin.fenner@datacite.org>
2018-05-28 17:45:09 +0200

    properly link routing-rules

--- prod-eu-west/services/test/main.tf
+++ prod-eu-west/services/test/main.tf
@@ -5,6 +5,7 @@ resource "aws_s3_bucket" "test" {
     website {
         index_document = "index.html"
         error_document = "404.html"
+        routing_rules = "${file("routing-rules.json")}"
     }
     tags {
         Name = "Test"
