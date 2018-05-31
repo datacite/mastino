d709fc90cb93dc33ac0a53cc6d57740ce2278d94
Martin Fenner <martin.fenner@datacite.org>
2018-05-31 16:48:39 +0200

    rename lambda function

--- prod-eu-west/services/datadog/main.tf
+++ prod-eu-west/services/datadog/main.tf
@@ -1,10 +1,10 @@
 resource "aws_lambda_function" "logs" {
-  filename = "lambda_function.py.zip"
-  function_name = "lambda_function"
+  filename = "logs.py.zip"
+  function_name = "logs"
   role = "${data.aws_iam_role.lambda.arn}"
-  handler = "lambda_function.lambda_handler"
+  handler = "logs.lambda_handler"
   runtime = "python2.7"
-  source_code_hash = "${base64sha256(file("lambda_function.py.zip"))}"
+  source_code_hash = "${base64sha256(file("logs.py.zip"))}"
   timeout = "120"
 
   vpc_config {
