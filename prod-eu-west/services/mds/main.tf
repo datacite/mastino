bfae052c98297628df55a8dbde5b30a3f3e4b722
Martin Fenner <martin.fenner@datacite.org>
2018-07-11 00:55:52 +0200

    unique priority

--- prod-eu-west/services/mds/main.tf
+++ prod-eu-west/services/mds/main.tf
@@ -145,7 +145,7 @@ resource "aws_lb_listener_rule" "mds-heartbeat" {
 
 resource "aws_lb_listener_rule" "mds-ng" {
   listener_arn = "${data.aws_lb_listener.default.arn}"
-  priority     = 10
+  priority     = 11
 
   action {
     type             = "forward"
