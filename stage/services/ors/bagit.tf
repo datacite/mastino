61266433eaec6eec77c7634c3d5877544b3c2caf
Martin Fenner <martin.fenner@datacite.org>
2018-07-10 08:26:30 +0200

    add required attribute for stage

--- stage/services/ors/bagit.tf
+++ stage/services/ors/bagit.tf
@@ -58,5 +58,6 @@ resource "aws_route53_record" "bagit-stage" {
    alias {
      name = "${aws_service_discovery_service.bagit-stage.name}.${aws_service_discovery_private_dns_namespace.internal-stage.name}"
      zone_id = "${aws_service_discovery_private_dns_namespace.internal-stage.hosted_zone}"
+     evaluate_target_health = true
    }
 }
