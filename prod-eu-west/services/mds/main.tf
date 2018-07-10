4231dc6542ff14f43245862b184419eaf1e49d9c
Martin Fenner <martin.fenner@datacite.org>
2018-07-10 12:33:07 +0200

    use new mds instance

--- prod-eu-west/services/mds/main.tf
+++ prod-eu-west/services/mds/main.tf
@@ -25,7 +25,8 @@ resource "aws_lb_target_group" "mds" {
 
 resource "aws_lb_target_group_attachment" "mds" {
   target_group_arn = "${aws_lb_target_group.mds.arn}"
-  target_id        = "${data.aws_instance.main.id}"
+  #target_id        = "${data.aws_instance.main.id}"
+  target_id        = "${aws_instance.mds.id}"
 }
 
 resource "aws_lb_listener_rule" "mds" {
