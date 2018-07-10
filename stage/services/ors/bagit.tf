829f90697ab1d3e00657b92b5fc2f375b548f120
Martin Fenner <martin.fenner@datacite.org>
2018-07-10 08:19:10 +0200

    alias for service discovery dns

--- stage/services/ors/bagit.tf
+++ stage/services/ors/bagit.tf
@@ -49,3 +49,14 @@ resource "aws_service_discovery_service" "bagit-stage" {
     }
   }
 }
+
+resource "aws_route53_record" "bagit-stage" {
+   zone_id = "${data.aws_route53_zone.internal.zone_id}"
+   name = "bagit.test.datacite.org"
+   type = "A"
+
+   alias {
+     name = "${aws_service_discovery_service.bagit-stage.name}.${aws_service_discovery_private_dns_namespace.internal-stage.name}"
+     zone_id = "${aws_service_discovery_private_dns_namespace.internal-stage.hosted_zone}"
+   }
+}
