e4589ef8369c5a48cdce54f7bc56ae614af38201
Martin Fenner <martin.fenner@datacite.org>
2018-07-11 16:48:26 +0200

    use load balancer instead of nginx reverse proxy

--- stage/services/ors/wsgi.tf
+++ stage/services/ors/wsgi.tf
@@ -6,6 +6,12 @@ resource "aws_ecs_service" "wsgi-stage" {
    task_definition = "${aws_ecs_task_definition.wsgi-stage.arn}"
    desired_count = 1
 
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.wsgi-stage.id}"
+    container_name   = "wsgi-stage"
+    container_port   = "80"
+  }
+
    network_configuration {
       security_groups = ["${data.aws_security_group.datacite-private.id}"]
       subnets         = [
@@ -14,9 +20,13 @@ resource "aws_ecs_service" "wsgi-stage" {
       ]
   }
 
-  service_registries {
-    registry_arn = "${aws_service_discovery_service.wsgi-stage.arn}"
-  }
+  depends_on = [
+    "data.aws_lb_listener.stage",
+  ]
+
+  // service_registries {
+  //   registry_arn = "${aws_service_discovery_service.wsgi-stage.arn}"
+  // }
 }
 
 resource "aws_cloudwatch_log_group" "wsgi-stage" {
@@ -34,30 +44,73 @@ resource "aws_ecs_task_definition" "wsgi-stage" {
    container_definitions = "${data.template_file.wsgi_task.rendered}"
 }
 
-resource "aws_service_discovery_service" "wsgi-stage" {
-  name = "wsgi"
+resource "aws_lb_target_group" "wsgi-stage" {
+   name     = "wsgi-stage"
+   port     = 80
+   protocol = "HTTP"
+   vpc_id   = "${var.vpc_id}"
+   target_type = "ip"
 
-  health_check_custom_config {
-    failure_threshold = 1
-  }
-
-  dns_config {
-    namespace_id = "${aws_service_discovery_private_dns_namespace.internal-stage.id}"
-    dns_records {
-      ttl = 300
-      type = "A"
-    }
-  }
-}
-
-resource "aws_route53_record" "wsgi-stage" {
-   zone_id = "${data.aws_route53_zone.internal.zone_id}"
-   name = "wsgi.test.datacite.org"
-   type = "A"
-
-   alias {
-     name = "${aws_service_discovery_service.wsgi-stage.name}.${aws_service_discovery_private_dns_namespace.internal-stage.name}"
-     zone_id = "${aws_service_discovery_private_dns_namespace.internal-stage.hosted_zone}"
-     evaluate_target_health = true
+   health_check {
+      path = "/"
    }
 }
+
+resource "aws_lb_listener_rule" "wsgi-stage" {
+   listener_arn = "${data.aws_lb_listener.stage.arn}"
+   priority     = 124
+
+   action {
+      type             = "forward"
+      target_group_arn = "${aws_lb_target_group.wsgi-stage.arn}"
+   }
+
+   condition {
+      field  = "host-header"
+      values = ["ors.test.datacite.org"]
+   }
+}
+
+resource "aws_route53_record" "ors-stage" {
+   zone_id = "${data.aws_route53_zone.production.zone_id}"
+   name = "ors.test.datacite.org"
+   type = "CNAME"
+   ttl = "${var.ttl}"
+   records = ["${data.aws_lb.stage.dns_name}"]
+}
+
+resource "aws_route53_record" "split-ors-stage" {
+   zone_id = "${data.aws_route53_zone.internal.zone_id}"
+   name = "ors.test.datacite.org"
+   type = "CNAME"
+   ttl = "${var.ttl}"
+   records = ["${data.aws_lb.stage.dns_name}"]
+}
+
+// resource "aws_service_discovery_service" "wsgi-stage" {
+//   name = "wsgi"
+
+//   health_check_custom_config {
+//     failure_threshold = 1
+//   }
+
+//   dns_config {
+//     namespace_id = "${aws_service_discovery_private_dns_namespace.internal-stage.id}"
+//     dns_records {
+//       ttl = 300
+//       type = "A"
+//     }
+//   }
+// }
+
+// resource "aws_route53_record" "wsgi-stage" {
+//    zone_id = "${data.aws_route53_zone.internal.zone_id}"
+//    name = "wsgi.test.datacite.org"
+//    type = "A"
+
+//    alias {
+//      name = "${aws_service_discovery_service.wsgi-stage.name}.${aws_service_discovery_private_dns_namespace.internal-stage.name}"
+//      zone_id = "${aws_service_discovery_private_dns_namespace.internal-stage.hosted_zone}"
+//      evaluate_target_health = true
+//    }
+// }
