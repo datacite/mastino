e2c5db97bd5da227d7f47972146c24b8d2ad2862
Martin Fenner <martin.fenner@datacite.org>
2018-07-11 09:06:11 +0200

    move cheetoh service stage to fargate

--- stage/services/cheetoh/main.tf
+++ stage/services/cheetoh/main.tf
@@ -1,13 +1,16 @@
 resource "aws_ecs_service" "cheetoh-stage" {
   name = "cheetoh-stage"
   cluster = "${data.aws_ecs_cluster.stage.id}"
+  launch_type = "FARGATE"
   task_definition = "${aws_ecs_task_definition.cheetoh-stage.arn}"
   desired_count = 1
-  iam_role        = "${data.aws_iam_role.ecs_service.arn}"
 
-  placement_strategy {
-    type  = "binpack"
-    field = "cpu"
+  network_configuration {
+    security_groups = ["${data.aws_security_group.datacite-private.id}"]
+    subnets         = [
+      "${data.aws_subnet.datacite-private.id}",
+      "${data.aws_subnet.datacite-alt.id}"
+    ]
   }
 
   load_balancer {
@@ -15,6 +18,10 @@ resource "aws_ecs_service" "cheetoh-stage" {
     container_name   = "cheetoh-stage"
     container_port   = "80"
   }
+
+  depends_on = [
+    "data.aws_lb_listener.stage",
+  ]
 }
 
 resource "aws_cloudwatch_log_group" "cheetoh-stage" {
@@ -24,6 +31,11 @@ resource "aws_cloudwatch_log_group" "cheetoh-stage" {
 resource "aws_ecs_task_definition" "cheetoh-stage" {
   family = "cheetoh-stage"
   execution_role_arn = "${data.aws_iam_role.ecs_task_execution_role.arn}"
+  network_mode = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu = "512"
+  memory = "1024"
+
   container_definitions =  "${data.template_file.cheetoh_task.rendered}"
 }
 
