f890f7c194235153685485ff594b68e3568f387c
Martin Fenner <martin.fenner@datacite.org>
2018-07-16 00:18:51 +0200

    load balance new mds 25%

--- prod-eu-west/services/mds/main.tf
+++ prod-eu-west/services/mds/main.tf
@@ -204,7 +204,7 @@ resource "aws_route53_record" "mds-legacy-rr" {
   ttl     = "${var.ttl}"
 
   weighted_routing_policy {
-    weight = 60
+    weight = 75
   }
 
   set_identifier = "legacy"
@@ -218,7 +218,7 @@ resource "aws_route53_record" "mds-ng-rr" {
   ttl     = "${var.ttl}"
 
   weighted_routing_policy {
-    weight = 40
+    weight = 25
   }
 
   set_identifier = "ng"
@@ -232,7 +232,7 @@ resource "aws_route53_record" "split-mds-legacy-rr" {
   ttl     = "5"
 
   weighted_routing_policy {
-    weight = 60
+    weight = 75
   }
 
   set_identifier = "split-legacy"
@@ -246,7 +246,7 @@ resource "aws_route53_record" "split-mds-ng-rr" {
   ttl     = "5"
 
   weighted_routing_policy {
-    weight = 40
+    weight = 25
   }
 
   set_identifier = "split-ng"
