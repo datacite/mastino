a86d9b39faad2337f028b6249bb841de4b79f357
Martin Fenner <martin.fenner@datacite.org>
2018-07-15 09:56:13 +0200

    reduce priority of legacy mds

--- prod-eu-west/services/mds/legacy.tf
+++ prod-eu-west/services/mds/legacy.tf
@@ -25,13 +25,12 @@ resource "aws_lb_target_group" "mds-legacy" {
 
 resource "aws_lb_target_group_attachment" "mds-legacy" {
   target_group_arn = "${aws_lb_target_group.mds-legacy.arn}"
-  #target_id        = "${data.aws_instance.main.id}"
   target_id        = "${aws_instance.mds.id}"
 }
 
 resource "aws_lb_listener_rule" "mds-legacy" {
   listener_arn = "${data.aws_lb_listener.default.arn}"
-  priority     = 2
+  priority     = 12
 
   action {
     type             = "forward"
