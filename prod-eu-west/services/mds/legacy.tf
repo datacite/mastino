78c2b64ca9ca0286802fef5ec2b3a398f05e321f
Martin Fenner <martin.fenner@datacite.org>
2018-07-15 14:04:06 +0200

    fix variables

--- prod-eu-west/services/mds/legacy.tf
+++ prod-eu-west/services/mds/legacy.tf
@@ -55,7 +55,22 @@ resource "aws_lb_listener_rule" "mds-legacy" {
 
   condition {
     field  = "host-header"
-    values = ["mds.datacite.org", "mds-legacy.datacite.org"]
+    values = ["mds.datacite.org"]
+  }
+}
+
+resource "aws_lb_listener_rule" "mds-legacy-legacy" {
+  listener_arn = "${data.aws_lb_listener.default.arn}"
+  priority     = 14
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.mds-legacy.arn}"
+  }
+
+  condition {
+    field  = "host-header"
+    values = ["mds-legacy.datacite.org"]
   }
 }
 
