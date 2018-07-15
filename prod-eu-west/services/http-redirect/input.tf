7ea47a9ed9d8a9d2a4f992122b9ed9d9587c969a
Martin Fenner <martin.fenner@datacite.org>
2018-07-15 08:27:30 +0200

    prod-eu-west http-redirect

---
+++ prod-eu-west/services/http-redirect/input.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+data "aws_security_group" "datacite-private" {
+  id = "${var.security_group_id}"
+}
+
+data "aws_subnet" "datacite-private" {
+  id = "${var.subnet_datacite-private_id}"
+}
+
+data "aws_subnet" "datacite-alt" {
+  id = "${var.subnet_datacite-alt_id}"
+}
+
+data "aws_ecs_cluster" "default" {
+  cluster_name = "default"
+}
+
+data "aws_iam_role" "ecs_service" {
+  name = "ecs_service"
+}
+
+data "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+}
+
+data "aws_lb" "default" {
+  name = "${var.lb_name}"
+}
+
+data "template_file" "http-redirect_task" {
+  template = "${file("http-redirect.json")}"
+
+  vars {
+    version            = "${var.http-redirect_tags["version"]}"
+  }
+}
