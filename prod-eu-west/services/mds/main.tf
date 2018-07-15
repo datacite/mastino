1a3d72971edd0b0ef4b3840c24e28bf5616f5822
Martin Fenner <martin.fenner@datacite.org>
2018-07-16 01:10:49 +0200

    prioritize old mds

--- prod-eu-west/services/mds/main.tf
+++ prod-eu-west/services/mds/main.tf
@@ -87,7 +87,7 @@ resource "aws_lb_listener_rule" "mds-metadatas" {
 
   condition {
     field  = "path-pattern"
-    values = ["/metadatas"]
+    values = ["/metadatas*"]
   }
 }
 
@@ -168,7 +168,7 @@ resource "aws_lb_listener_rule" "mds-ng" {
 
 resource "aws_lb_listener_rule" "mds-legacy" {
   listener_arn = "${data.aws_lb_listener.default.arn}"
-  priority     = 14
+  priority     = 2
 
   action {
     type             = "forward"
