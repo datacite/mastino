aed33244ca9358ca0ab8a8e5968de1a1a236dd10
Martin Fenner <martin.fenner@datacite.org>
2018-07-18 07:16:56 +0200

    use launch configuration for legacy mds

--- prod-eu-west/services/mds/legacy.tf
+++ prod-eu-west/services/mds/legacy.tf
@@ -1,14 +1,28 @@
-resource "aws_instance" "mds" {
-  ami = "${var.mds_ami}"
-  instance_type = "m1.large"
-  vpc_security_group_ids = ["${data.aws_security_group.datacite-private.id}"]
-  subnet_id = "${data.aws_subnet.datacite-private.id}"
-  key_name = "${var.key_name}"
-  tags {
-      Name = "MDS"
-  }
+resource "aws_launch_configuration" "mds" {
+  name_prefix          = "MDS-"
+  image_id             = "${var.mds_ami}"
+  security_groups      = ["${aws_security_group.datacite-private.id}"]
+  instance_type        = "m1.large"
+  key_name             = "${var.key_name}"
+
   lifecycle {
-      create_before_destroy = "true"
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "mds" {
+  name                 = "mds"
+  vpc_zone_identifier  = ["${aws_subnet.datacite-private.id}"]
+  launch_configuration = "${aws_launch_configuration.mds.name}"
+  min_size             = 1
+  max_size             = 2
+  desired_capacity     = 1
+  health_check_type    = "ELB"
+  health_check_grace_period = 300
+  target_group_arns    = ["${aws_lb_target_group.mds-legacy.arn}"]
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -53,10 +67,10 @@ resource "aws_lb_listener_rule" "mds-legacy" {
   }
 }
 
-resource "aws_lb_target_group_attachment" "mds-legacy" {
-  target_group_arn = "${aws_lb_target_group.mds-legacy.arn}"
-  target_id        = "${aws_instance.mds.id}"
-}
+// resource "aws_lb_target_group_attachment" "mds-legacy" {
+//   target_group_arn = "${aws_lb_target_group.mds-legacy.arn}"
+//   target_id        = "${aws_instance.mds.id}"
+// }
 
 resource "aws_route53_record" "mds-legacy" {
    zone_id = "${data.aws_route53_zone.production.zone_id}"
