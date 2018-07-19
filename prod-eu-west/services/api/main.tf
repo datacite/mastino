7ddc2a37836fcd314f402c426d3501a6a44dcfea
Martin Fenner <martin.fenner@datacite.org>
2018-07-19 17:50:03 +0200

    set api routes

--- prod-eu-west/services/api/main.tf
+++ prod-eu-west/services/api/main.tf
@@ -51,9 +51,9 @@ resource "aws_lb_target_group" "api" {
   }
 }
 
-resource "aws_lb_listener_rule" "api" {
+resource "aws_lb_listener_rule" "api-works" {
   listener_arn = "${data.aws_lb_listener.default.arn}"
-  priority     = 41
+  priority     = 21
 
   action {
     type             = "forward"
@@ -63,6 +63,86 @@ resource "aws_lb_listener_rule" "api" {
   condition {
     field  = "host-header"
     values = ["${aws_route53_record.api.name}"]
+  }
+  condition {
+    field  = "path-pattern"
+    values = ["/works*"]
+  }
+}
+
+resource "aws_lb_listener_rule" "api-pages" {
+  listener_arn = "${data.aws_lb_listener.default.arn}"
+  priority     = 22
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.api.arn}"
+  }
+
+  condition {
+    field  = "host-header"
+    values = ["${aws_route53_record.api.name}"]
+  }
+  condition {
+    field  = "path-pattern"
+    values = ["/pages*"]
+  }
+}
+
+resource "aws_lb_listener_rule" "api-milestones" {
+  listener_arn = "${data.aws_lb_listener.default.arn}"
+  priority     = 23
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.api.arn}"
+  }
+
+  condition {
+    field  = "host-header"
+    values = ["${aws_route53_record.api.name}"]
+  }
+  condition {
+    field  = "path-pattern"
+    values = ["/milestones*"]
+  }
+}
+
+resource "aws_lb_listener_rule" "api-user-stories" {
+  listener_arn = "${data.aws_lb_listener.default.arn}"
+  priority     = 24
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.api.arn}"
+  }
+
+  condition {
+    field  = "host-header"
+    values = ["${aws_route53_record.api.name}"]
+  }
+  condition {
+    field  = "path-pattern"
+    values = ["/user-stories*"]
+  }
+}
+
+resource "aws_lb_listener_rule" "api-datasets" {
+  listener_arn = "${data.aws_lb_listener.default.arn}"
+  priority     = 25
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.api.arn}"
+  }
+
+  condition {
+    field  = "host-header"
+    values = ["${var.api_dns_name}"]
+  }
+  condition {
+    field  = "path-pattern"
+    values = ["/datasets*"]
   }
 }
 
