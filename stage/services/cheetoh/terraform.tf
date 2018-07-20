5df3c0024b159930ba648bcc409b1ac40ac92681
Martin Fenner <martin.fenner@datacite.org>
2018-07-20 21:02:51 +0200

    use correct terraform configuration

--- stage/services/cheetoh/terraform.tf
+++ stage/services/cheetoh/terraform.tf
@@ -2,6 +2,6 @@ terraform {
   required_version = ">= 0.11"
 
   backend "atlas" {
-    name         = "datacite-ng/stage-services-client-api"
+    name         = "datacite-ng/stage-services-cheetoh"
   }
 }
