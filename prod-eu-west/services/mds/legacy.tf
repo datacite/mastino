a39e10d01e47b5bebdae7b97891d4dc5f7297667
Martin Fenner <martin.fenner@datacite.org>
2018-07-24 00:08:30 -0700

    scale down mds legacy instances to 1

--- prod-eu-west/services/mds/legacy.tf
+++ prod-eu-west/services/mds/legacy.tf
@@ -15,8 +15,8 @@ resource "aws_autoscaling_group" "mds" {
   vpc_zone_identifier  = ["${data.aws_subnet.datacite-private.id}", "${data.aws_subnet.datacite-alt.id}"]
   launch_configuration = "${aws_launch_configuration.mds.name}"
   min_size             = 1
-  max_size             = 3
-  desired_capacity     = 3
+  max_size             = 2
+  desired_capacity     = 1
   health_check_type    = "ELB"
   health_check_grace_period = 300
   enabled_metrics      = ["GroupInServiceInstances", "GroupTerminatingInstances"]
