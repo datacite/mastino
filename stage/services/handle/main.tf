5fcb4ee43f425b50a5438e5d0c46585568e01e5e
Richard Hallett <richard@richardhallett.com>
2018-08-02 18:37:11 +0200

    Fargate config

--- stage/services/handle/main.tf
+++ stage/services/handle/main.tf
@@ -1,14 +1,27 @@
 resource "aws_ecs_service" "handle-stage" {
   name            = "handle-stage"
   cluster         = "${data.aws_ecs_cluster.stage.id}"
-  launch_type = "FARGATE"
+  launch_type     = "FARGATE"
   task_definition = "${aws_ecs_task_definition.handle-stage.arn}"
   desired_count   = 1
 
-  placement_strategy {
-    type  = "binpack"
-    field = "cpu"
+  network_configuration {
+    security_groups = ["${data.aws_security_group.datacite-private.id}"]
+    subnets         = [
+      "${data.aws_subnet.datacite-private.id}",
+      "${data.aws_subnet.datacite-alt.id}"
+    ]
   }
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.mds-stage.id}"
+    container_name   = "mds-stage"
+    container_port   = "80"
+  }
+
+  depends_on = [
+    "data.aws_lb_listener.stage",
+  ]
 }
 
 resource "aws_cloudwatch_log_group" "handle-stage" {
@@ -19,7 +32,10 @@ resource "aws_ecs_task_definition" "handle-stage" {
   family                = "pidcheck-stage"
   execution_role_arn = "${data.aws_iam_role.ecs_task_execution_role.arn}"
   container_definitions = "${data.template_file.handle_task.rendered}"
+  network_mode = "awsvpc"
   requires_compatibilities = ["FARGATE"]
+  cpu = "512"
+  memory = "1024"
 }
 
 resource "aws_lb_target_group" "handle-stage" {
