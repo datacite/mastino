1ef238cad908853cc3669122d95e428a60ddf06d
Martin Fenner <martin.fenner@datacite.org>
2018-09-28 14:59:21 +0200

    fix variables

--- stage/services/ors/input.tf
+++ stage/services/ors/input.tf
@@ -85,7 +85,8 @@ data "template_file" "wsgi_task" {
       globus_client = "${var.globus_client}"
       globus_secret = "${var.globus_secret}"
 
-      indexd_user     = "${var.indexd_url}"
+      indexd_url      = "${var.indexd_url}"
+      indexd_username = "${var.indexd_username}"
       indexd_password = "${var.indexd_password}"
 
       datacite_url   = "${var.datacite_url}"
