55cad70789a9c3026e2984bbc7d2cbe6f40baee8
Max Levinson <mlev71@users.noreply.github.com>
2018-09-28 10:23:06 -0400

    update missing vars

--- stage/services/ors/var.tf
+++ stage/services/ors/var.tf
@@ -18,6 +18,17 @@ variable "security_group_id" {}
 variable "subnet_datacite-private_id" {}
 variable "subnet_datacite-alt_id" {}
 
+
+variable "wsgi_tags" {
+  type = "map"
+}
+
+variable "bagit_tags" {
+  type = "map"
+}
+
+// uwsgi container variables
+
 variable "neo_user" {}
 variable "neo_password" {}
 
@@ -36,3 +47,31 @@ variable "proxy_url" {
 variable "datacite_url" {
    default = "https://mds.test.datacite.org"
 }
+
+variable "root_url" {
+   default = "https://ors.test.datacite.org"
+}
+
+variable "ezid_user" {}
+variable "ezid_password" {}
+
+variable "datacite_user" {}
+variable "datacite_password" {}
+
+variable "admin_username" {}
+variable "admin_password" {}
+
+variable "globus_client" {}
+
+variable "globus_username" {}
+variable "globus_secret" {}
+
+variable "bugsnag_key" {
+   default = "2c30fce5bccd2542eba01246b6983049"
+}
+
+variable "indexd_url" {
+   default = "indexd.test.datacite.org"
+}
+variable "indexd_username" {}
+variable "indexd_password" {}
