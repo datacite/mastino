168dc26ead5ea4ed8c01870f26627bf7688a793f
Richard Hallett <richard@richardhallett.com>
2018-10-17 12:16:04 +0200

    New lambda for storing crawler results

---
+++ stage/services/store-crawler-results/main.tf
@@ -0,0 +1,43 @@
+resource "aws_cloudwatch_event_rule" "store-crawler-results-test" {
+  name                = "store-crawler-results-test"
+  description         = "Run store-crawler-results API call via cron"
+  schedule_expression = "cron(42 2 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "store-crawler-results-test" {
+  target_id = "store-crawler-results-test"
+  rule      = "${aws_cloudwatch_event_rule.store-crawler-results-test.name}"
+  arn       = "${aws_lambda_function.store-crawler-results-test.arn}"
+}
+
+resource "aws_lambda_function" "store-crawler-results-test" {
+  filename         = "store_crawler_results.py.zip"
+  function_name    = "store-crawler-results-test"
+  role             = "${data.aws_iam_role.lambda.arn}"
+  handler          = "store_crawler_results_runner.lambda_handler"
+  runtime          = "python3.6"
+  source_code_hash = "${base64sha256(file("store_crawler_results.py.zip"))}"
+  timeout          = "270"
+
+  vpc_config {
+    subnet_ids         = ["${data.aws_subnet.datacite-private.id}", "${data.aws_subnet.datacite-alt.id}"]
+    security_group_ids = ["${data.aws_security_group.datacite-private.id}"]
+  }
+
+  environment {
+    variables = {
+      REDIS_HOST     = "${var.redis_host}"
+      API_ENDPOINT   = "${var.api_endpoint}"
+      ADMIN_USERNAME = "${var.admin_username}"
+      ADMIN_PASSWORD = "${var.admin_password}"
+    }
+  }
+}
+
+resource "aws_lambda_permission" "store-crawler-results-test" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.store-crawler-results-test.function_name}"
+  principal     = "events.amazonaws.com"
+  source_arn    = "${aws_cloudwatch_event_rule.store-crawler-results-test.arn}"
+}
