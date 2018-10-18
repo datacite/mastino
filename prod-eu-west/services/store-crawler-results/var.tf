e47ad1375e642347c40486eac4808216b9524bfd
Richard Hallett <richard@richardhallett.com>
2018-10-18 11:18:54 +0200

    Production crawler lambda for storing results

---
+++ prod-eu-west/services/store-crawler-results/var.tf
@@ -0,0 +1,15 @@
+variable "access_key" {}
+variable "secret_key" {}
+
+variable "region" {
+  default = "eu-west-1"
+}
+
+variable "security_group_id" {}
+variable "subnet_datacite-private_id" {}
+variable "subnet_datacite-alt_id" {}
+
+variable "api_endpoint" {}
+variable "redis_host" {}
+variable "admin_username" {}
+variable "admin_password" {}
