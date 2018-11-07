e18e947880966b553e27b385bec061c799bcdfff
Kristian Garza <kj.garza@gmail.com>
2018-11-07 19:58:36 +0100

    adding usage and resolution variables

--- prod-eu-west/services/levriero/var.tf
+++ prod-eu-west/services/levriero/var.tf
@@ -58,3 +58,5 @@ variable "datacite_other_source_token" {}
 variable "datacite_url_source_token" {}
 variable "datacite_funder_source_token" {}
 variable "datacite_orcid_auto_update_source_token" {}
+variable "datacite_resolution_source_token" {}
+variable "datacite_usage_source_token" {}
