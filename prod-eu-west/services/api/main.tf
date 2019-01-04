12d0c6dda3d1c7ea30d746ebb67e91d06f4608d4
Martin Fenner <martin.fenner@datacite.org>
2019-01-04 09:46:18 +0100

    use lupo api for works endpoint

--- prod-eu-west/services/api/main.tf
+++ prod-eu-west/services/api/main.tf
@@ -52,24 +52,24 @@ resource "aws_lb_target_group" "api" {
   }
 }
 
-resource "aws_lb_listener_rule" "api-works" {
-  listener_arn = "${data.aws_lb_listener.default.arn}"
-  priority     = 21
+// resource "aws_lb_listener_rule" "api-works" {
+//   listener_arn = "${data.aws_lb_listener.default.arn}"
+//   priority     = 21
 
-  action {
-    type             = "forward"
-    target_group_arn = "${aws_lb_target_group.api.arn}"
-  }
+//   action {
+//     type             = "forward"
+//     target_group_arn = "${aws_lb_target_group.api.arn}"
+//   }
 
-  condition {
-    field  = "host-header"
-    values = ["${aws_route53_record.api.name}"]
-  }
-  condition {
-    field  = "path-pattern"
-    values = ["/works*"]
-  }
-}
+//   condition {
+//     field  = "host-header"
+//     values = ["${aws_route53_record.api.name}"]
+//   }
+//   condition {
+//     field  = "path-pattern"
+//     values = ["/works*"]
+//   }
+// }
 
 resource "aws_lb_listener_rule" "api-pages" {
   listener_arn = "${data.aws_lb_listener.default.arn}"
