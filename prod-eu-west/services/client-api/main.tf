81dafd2a2540ba8e3e546cb3327997f255c29c75
Martin Fenner <martin.fenner@datacite.org>
2019-03-04 10:01:19 +0100

    increase number of client-api containers

--- prod-eu-west/services/client-api/main.tf
+++ prod-eu-west/services/client-api/main.tf
@@ -3,7 +3,7 @@ resource "aws_ecs_service" "client-api" {
   cluster = "${data.aws_ecs_cluster.default.id}"
   launch_type = "FARGATE"
   task_definition = "${aws_ecs_task_definition.client-api.arn}"
-  desired_count = 4
+  desired_count = 6
 
   network_configuration {
     security_groups = ["${data.aws_security_group.datacite-private.id}"]
