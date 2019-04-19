9d7ca748414e93a4f7ea3822a52021637c7fbf77
Martin Fenner <martin.fenner@datacite.org>
2019-04-19 17:29:35 +0200

    change url

--- stage/services/ghost/var.tf
+++ stage/services/ghost/var.tf
@@ -27,7 +27,7 @@ variable "mysql_database" {
 variable "mysql_host" {}
 
 variable "url" {
-  default = "https://ghost.test.datacite.org"
+  default = "https://ghost.test.datacite.org:2368"
 }
 
 variable "mailgun_user" {}
