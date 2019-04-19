1bf825191c0668a4cc37384617f542f965b9f3d6
Martin Fenner <martin.fenner@datacite.org>
2019-04-19 17:50:31 +0200

    generic health check

--- stage/services/ghost/main.tf
+++ stage/services/ghost/main.tf
@@ -61,10 +61,6 @@ resource "aws_lb_target_group" "ghost-stage" {
   protocol = "HTTP"
   vpc_id   = "${var.vpc_id}"
   target_type = "ip"
-
-  health_check {
-    path = "/"
-  }
 }
 
 resource "aws_lb_listener_rule" "ghost-stage" {
