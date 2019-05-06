412d684e4ef6cf5977be28627717a3aa3246fdc7
Martin Fenner <martin.fenner@datacite.org>
2019-05-06 13:11:24 +0200

    increase cpu for client api test

--- stage/services/client-api/main.tf
+++ stage/services/client-api/main.tf
@@ -37,7 +37,7 @@ resource "aws_ecs_task_definition" "client-api-stage" {
   execution_role_arn = "${data.aws_iam_role.ecs_task_execution_role.arn}"
   network_mode = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu = "512"
+  cpu = "1024"
   memory = "2048"
   container_definitions =  "${data.template_file.client-api_task.rendered}"
 }
