0806be2588cf835959201476205a64c9e6becbd5
Martin Fenner <martin.fenner@datacite.org>
2019-05-17 16:23:03 +0200

    remove obsolete key

--- stage/services/metrics-api/input.tf
+++ stage/services/metrics-api/input.tf
@@ -62,7 +62,6 @@ data "template_file" "metrics-api_task" {
     mysql_user         = "${var.mysql_user}"
     mysql_password     = "${var.mysql_password}"
     mysql_host         = "${var.mysql_host}"
-    bugsnag_key        = "${var.bugsnag_key}"
     usage_url          = "${var.usage_url}"
     sentry_dsn         = "${var.sentry_dsn}"
     version            = "${var.sashimi_tags["sha"]}"
