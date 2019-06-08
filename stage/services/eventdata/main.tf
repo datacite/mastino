230709da6425d2b01fc83a2298606b7b88070587
Martin Fenner <martin.fenner@datacite.org>
2019-06-08 18:06:41 +0200

    comment out main eventdata stage

--- stage/services/eventdata/main.tf
+++ stage/services/eventdata/main.tf
@@ -1,46 +1,46 @@
-resource "aws_ecs_service" "eventdata-stage" {
-  name = "eventdata-stage"
-  cluster = "${data.aws_ecs_cluster.stage.id}"
-  launch_type = "FARGATE"
-  task_definition = "${aws_ecs_task_definition.eventdata-stage.arn}"
-  desired_count = 0
+// resource "aws_ecs_service" "eventdata-stage" {
+//   name = "eventdata-stage"
+//   cluster = "${data.aws_ecs_cluster.stage.id}"
+//   launch_type = "FARGATE"
+//   task_definition = "${aws_ecs_task_definition.eventdata-stage.arn}"
+//   desired_count = 0
 
-  network_configuration {
-    security_groups = ["${data.aws_security_group.datacite-private.id}"]
-    subnets         = [
-      "${data.aws_subnet.datacite-private.id}",
-      "${data.aws_subnet.datacite-alt.id}"
-    ]
-  }
+//   network_configuration {
+//     security_groups = ["${data.aws_security_group.datacite-private.id}"]
+//     subnets         = [
+//       "${data.aws_subnet.datacite-private.id}",
+//       "${data.aws_subnet.datacite-alt.id}"
+//     ]
+//   }
 
-  load_balancer {
-    target_group_arn = "${aws_lb_target_group.eventdata-stage.id}"
-    container_name   = "eventdata-stage"
-    container_port   = "80"
-  }
+//   load_balancer {
+//     target_group_arn = "${aws_lb_target_group.eventdata-stage.id}"
+//     container_name   = "eventdata-stage"
+//     container_port   = "80"
+//   }
 
-  service_registries {
-    registry_arn = "${aws_service_discovery_service.eventdata-stage.arn}"
-  }
+//   service_registries {
+//     registry_arn = "${aws_service_discovery_service.eventdata-stage.arn}"
+//   }
 
-  depends_on = [
-    "data.aws_lb_listener.stage",
-  ]
-}
+//   depends_on = [
+//     "data.aws_lb_listener.stage",
+//   ]
+// }
 
-resource "aws_cloudwatch_log_group" "eventdata-stage" {
-  name = "/ecs/eventdata-stage"
-}
+// resource "aws_cloudwatch_log_group" "eventdata-stage" {
+//   name = "/ecs/eventdata-stage"
+// }
 
-resource "aws_ecs_task_definition" "eventdata-stage" {
-  family = "eventdata-stage"
-  execution_role_arn = "${data.aws_iam_role.ecs_task_execution_role.arn}"
-  network_mode = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu = "512"
-  memory = "2048"
-  container_definitions =  "${data.template_file.eventdata_task.rendered}"
-}
+// resource "aws_ecs_task_definition" "eventdata-stage" {
+//   family = "eventdata-stage"
+//   execution_role_arn = "${data.aws_iam_role.ecs_task_execution_role.arn}"
+//   network_mode = "awsvpc"
+//   requires_compatibilities = ["FARGATE"]
+//   cpu = "512"
+//   memory = "2048"
+//   container_definitions =  "${data.template_file.eventdata_task.rendered}"
+// }
 
 // resource "aws_lb_target_group" "eventdata-stage" {
 //   name     = "eventdata-stage"
@@ -74,19 +74,19 @@ resource "aws_ecs_task_definition" "eventdata-stage" {
 //   }
 // }
 
-resource "aws_service_discovery_service" "eventdata-stage" {
-  name = "eventdata.test"
+// resource "aws_service_discovery_service" "eventdata-stage" {
+//   name = "eventdata.test"
 
-  health_check_custom_config {
-    failure_threshold = 3
-  }
+//   health_check_custom_config {
+//     failure_threshold = 3
+//   }
 
-  dns_config {
-    namespace_id = "${var.namespace_id}"
+//   dns_config {
+//     namespace_id = "${var.namespace_id}"
     
-    dns_records {
-      ttl = 300
-      type = "A"
-    }
-  }
-}
+//     dns_records {
+//       ttl = 300
+//       type = "A"
+//     }
+//   }
+// }
