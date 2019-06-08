9cf2e67e90933360037ed2ff8dd391bf16a4fefe
Martin Fenner <martin.fenner@datacite.org>
2019-06-08 18:05:04 +0200

    retire eventdata stage target group

--- stage/services/eventdata/main.tf
+++ stage/services/eventdata/main.tf
@@ -42,17 +42,17 @@ resource "aws_ecs_task_definition" "eventdata-stage" {
   container_definitions =  "${data.template_file.eventdata_task.rendered}"
 }
 
-resource "aws_lb_target_group" "eventdata-stage" {
-  name     = "eventdata-stage"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = "${var.vpc_id}"
-  target_type = "ip"
+// resource "aws_lb_target_group" "eventdata-stage" {
+//   name     = "eventdata-stage"
+//   port     = 80
+//   protocol = "HTTP"
+//   vpc_id   = "${var.vpc_id}"
+//   target_type = "ip"
 
-  health_check {
-    path = "/heartbeat"
-  }
-}
+//   health_check {
+//     path = "/heartbeat"
+//   }
+// }
 
 // resource "aws_lb_listener_rule" "eventdata-stage" {
 //   listener_arn = "${data.aws_lb_listener.stage.arn}"
