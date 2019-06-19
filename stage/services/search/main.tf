48fb9a305d8739acc70d792527d2713bf2041240
Kristian Garza <kj.garza@gmail.com>
2019-06-19 12:05:56 +0200

    return to normal state

--- stage/services/search/main.tf
+++ stage/services/search/main.tf
@@ -15,7 +15,7 @@ resource "aws_ecs_service" "search-stage" {
   cluster = "${data.aws_ecs_cluster.stage.id}"
   launch_type = "FARGATE"
   task_definition = "${aws_ecs_task_definition.search-stage.arn}"
-  desired_count = 0
+  desired_count = 1
 
   network_configuration {
     security_groups = ["${data.aws_security_group.datacite-private.id}"]
