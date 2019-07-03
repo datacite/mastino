b7cf7b4093f6cb1396b07d9ba89b545b77d159bc
Martin Fenner <martin.fenner@datacite.org>
2019-07-03 21:47:13 +0200

    upgrade aws provider

--- prod-eu-west/data-storage/elasticsearch/input.tf
+++ prod-eu-west/data-storage/elasticsearch/input.tf
@@ -2,7 +2,7 @@ provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
   region     = "${var.region}"
-  version    = "~> 1.6"
+  version    = "~> 1.55"
 }
 
 data "aws_security_group" "datacite-private" {
