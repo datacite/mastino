85460ba1366ec98040cb8350e836326cd784f3b1
Martin Fenner <martin.fenner@datacite.org>
2019-07-29 12:02:35 +0200

    use custom waf rules

--- prod-eu-west/vpc/waf.tf
+++ prod-eu-west/vpc/waf.tf
@@ -1,18 +1,18 @@
-module "waf" {
-  source  = "trussworks/waf/aws"
-  version = "1.2.0"
+// module "waf" {
+//   source  = "trussworks/waf/aws"
+//   version = "1.2.0"
   
-  environment                         = "${var.environment}"
-  associate_alb                       = true
-  alb_arn                             = "${data.aws_lb.default.arn}"
-  wafregional_rule_f5_id              = "${var.wafregional_rule_id}"
-  ips_disallow                        = "${var.waf_ips_disallow}"
-  regex_path_disallow_pattern_strings = "${var.waf_regex_path_disallow_pattern_strings}"
-  regex_host_allow_pattern_strings    = "${var.waf_regex_host_allow_pattern_strings}"
-  ip_rate_limit                       = "${var.waf_ip_rate_limit}"
-}
+//   environment                         = "${var.environment}"
+//   associate_alb                       = true
+//   alb_arn                             = "${data.aws_lb.default.arn}"
+//   wafregional_rule_f5_id              = "${var.wafregional_rule_id}"
+//   ips_disallow                        = "${var.waf_ips_disallow}"
+//   regex_path_disallow_pattern_strings = "${var.waf_regex_path_disallow_pattern_strings}"
+//   regex_host_allow_pattern_strings    = "${var.waf_regex_host_allow_pattern_strings}"
+//   ip_rate_limit                       = "${var.waf_ip_rate_limit}"
+// }
 
-resource "aws_waf_ipset" "nat" {
+resource "aws_wafregional_ipset" "nat" {
   name = "natIPSet"
 
   ip_set_descriptors {
@@ -21,8 +21,8 @@ resource "aws_waf_ipset" "nat" {
   }
 }
 
-resource "aws_waf_rate_based_rule" "nat" {
-  depends_on  = ["aws_waf_ipset.nat"]
+resource "aws_wafregional_rate_based_rule" "rate" {
+  depends_on  = ["aws_wafregional_ipset.nat"]
   name        = "natWAFRule"
   metric_name = "natWAFRule"
 
@@ -30,23 +30,32 @@ resource "aws_waf_rate_based_rule" "nat" {
   rate_limit = 3000
 
   predicates {
-    data_id = "${aws_waf_ipset.nat.id}"
+    data_id = "${aws_wafregional_ipset.nat.id}"
     negated = true
     type    = "IPMatch"
   }
 }
 
-resource "aws_waf_rule_group" "limit" {
-  name        = "limit"
-  metric_name = "limit"
+resource "aws_wafregional_web_acl" "default" {
+  name        = "default"
+  metric_name = "default"
 
-  activated_rule {
+  default_action {
+    type = "ALLOW"
+  }
+
+  rule {
     action {
       type = "BLOCK"
     }
 
     priority = 1
-    rule_id  = "${aws_waf_rate_based_rule.nat.id}"
-    type = "RATE_BASED"
+    rule_id  = "${aws_wafregional_rate_based_rule.rate.id}"
+    type     = "RATE_BASED"
   }
+}
+
+resource "aws_wafregional_web_acl_association" "default" {
+  resource_arn = "${data.aws_lb.default.arn}"
+  web_acl_id   = "${aws_wafregional_web_acl.default.id}"
 }
