909da47eefa5a078e782734c9b05a2a6e4f56bdd
Martin Fenner <martin.fenner@datacite.org>
2019-07-29 08:43:29 +0200

    fix variable name

--- prod-eu-west/vpc/waf.tf
+++ prod-eu-west/vpc/waf.tf
@@ -6,6 +6,8 @@ module "waf" {
   associate_alb                       = true
   alb_arn                             = "${data.aws_lb.default.arn}"
   wafregional_rule_f5_id              = "${var.wafregional_rule_id}"
-  ips_disallow                        = "${var.waf_ips_diallow}"
+  ips_disallow                        = "${var.waf_ips_disallow}"
+  regex_path_disallow_pattern_strings = "${var.waf_regex_path_disallow_pattern_strings}"
+  regex_host_allow_pattern_strings    = "${var.waf_regex_host_allow_pattern_strings}"
   ip_rate_limit                       = "${var.waf_ip_rate_limit}"
 }
