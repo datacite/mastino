2f8f2ef241a9d1781c7fe0167cfc5e86615b8ebc
Martin Fenner <martin.fenner@datacite.org>
2019-08-15 18:43:43 +0200

    stop dns via route 53

--- pidapalooza/services/website/main.tf
+++ pidapalooza/services/website/main.tf
@@ -11,100 +11,100 @@ resource "aws_s3_bucket" "pidapalooza" {
     }
 }
 
-resource "aws_cloudfront_distribution" "pidapalooza" {
-  origin {
-    domain_name = "${aws_s3_bucket.pidapalooza.website_endpoint}"
-    origin_id   = "pidapalooza.org"
+// resource "aws_cloudfront_distribution" "pidapalooza" {
+//   origin {
+//     domain_name = "${aws_s3_bucket.pidapalooza.website_endpoint}"
+//     origin_id   = "pidapalooza.org"
 
-    custom_origin_config {
-      origin_protocol_policy = "http-only"
-      http_port = "80"
-      https_port = "443"
-      origin_ssl_protocols = ["TLSv1"]
-    }
-  }
+//     custom_origin_config {
+//       origin_protocol_policy = "http-only"
+//       http_port = "80"
+//       https_port = "443"
+//       origin_ssl_protocols = ["TLSv1"]
+//     }
+//   }
 
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
+//   enabled             = true
+//   is_ipv6_enabled     = true
+//   default_root_object = "index.html"
 
-  logging_config {
-    include_cookies = false
-    bucket          = "${data.aws_s3_bucket.logs.bucket_domain_name}"
-    prefix          = "pidapalooza/"
-  }
+//   logging_config {
+//     include_cookies = false
+//     bucket          = "${data.aws_s3_bucket.logs.bucket_domain_name}"
+//     prefix          = "pidapalooza/"
+//   }
 
-  aliases = ["pidapalooza.org"]
+//   aliases = ["pidapalooza.org"]
 
-  default_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "pidapalooza.org"
+//   default_cache_behavior {
+//     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+//     cached_methods   = ["GET", "HEAD"]
+//     target_origin_id = "pidapalooza.org"
 
-    forwarded_values {
-      query_string = true
+//     forwarded_values {
+//       query_string = true
 
-      cookies {
-        forward = "all"
-      }
-    }
+//       cookies {
+//         forward = "all"
+//       }
+//     }
 
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
+//     viewer_protocol_policy = "redirect-to-https"
+//     min_ttl                = 0
+//     default_ttl            = 3600
+//     max_ttl                = 86400
+//   }
 
-  price_class = "PriceClass_All"
+//   price_class = "PriceClass_All"
 
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
+//   restrictions {
+//     geo_restriction {
+//       restriction_type = "none"
+//     }
+//   }
 
-  tags {
-    Environment = "prod"
-  }
+//   tags {
+//     Environment = "prod"
+//   }
 
-  viewer_certificate {
-    acm_certificate_arn = "${data.aws_acm_certificate.pidapalooza.arn}"
-    ssl_support_method  = "sni-only"
-  }
-}
+//   viewer_certificate {
+//     acm_certificate_arn = "${data.aws_acm_certificate.pidapalooza.arn}"
+//     ssl_support_method  = "sni-only"
+//   }
+// }
 
-resource "aws_route53_record" "www" {
-   zone_id = "${data.aws_route53_zone.pidapalooza.zone_id}"
-   name = "www.pidapalooza.org"
-   type = "CNAME"
-   ttl = "300"
-   records = ["www75.wixdns.net"]
-}
+// resource "aws_route53_record" "www" {
+//    zone_id = "${data.aws_route53_zone.pidapalooza.zone_id}"
+//    name = "www.pidapalooza.org"
+//    type = "CNAME"
+//    ttl = "300"
+//    records = ["www75.wixdns.net"]
+// }
 
-resource "aws_route53_record" "apex" {
-  zone_id = "${data.aws_route53_zone.pidapalooza.zone_id}"
-  name = "pidapalooza.org"
-  type = "A"
+// resource "aws_route53_record" "apex" {
+//   zone_id = "${data.aws_route53_zone.pidapalooza.zone_id}"
+//   name = "pidapalooza.org"
+//   type = "A"
 
-  alias {
-    name = "${aws_cloudfront_distribution.pidapalooza.domain_name}"
-    zone_id = "${var.cloudfront_alias_zone_id}"
-    evaluate_target_health = true
-  }
-}
+//   alias {
+//     name = "${aws_cloudfront_distribution.pidapalooza.domain_name}"
+//     zone_id = "${var.cloudfront_alias_zone_id}"
+//     evaluate_target_health = true
+//   }
+// }
 
-resource "aws_route53_record" "staging" {
-    zone_id = "${data.aws_route53_zone.pidapalooza.zone_id}"
-    name = "staging.pidapalooza.org"
-    type = "CNAME"
-    ttl = "300"
-    records = ["www75.wixdns.net"]
-}
+// resource "aws_route53_record" "staging" {
+//     zone_id = "${data.aws_route53_zone.pidapalooza.zone_id}"
+//     name = "staging.pidapalooza.org"
+//     type = "CNAME"
+//     ttl = "300"
+//     records = ["www75.wixdns.net"]
+// }
 
-resource "aws_route53_record" "www-staging" {
-    zone_id = "${data.aws_route53_zone.pidapalooza.zone_id}"
-    name = "www.staging.pidapalooza.org"
-    type = "CNAME"
-    ttl = "300"
-    records = ["www75.wixdns.net"]
-}
+// resource "aws_route53_record" "www-staging" {
+//     zone_id = "${data.aws_route53_zone.pidapalooza.zone_id}"
+//     name = "www.staging.pidapalooza.org"
+//     type = "CNAME"
+//     ttl = "300"
+//     records = ["www75.wixdns.net"]
+// }
