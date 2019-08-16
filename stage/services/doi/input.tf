92b97ee1e6f409b78a412fe6eac7072bffedd46a
Martin Fenner <martin.fenner@datacite.org>
2019-08-16 19:10:27 +0200

    delete obsolete resources

--- stage/services/doi/input.tf
+++ stage/services/doi/input.tf
@@ -4,6 +4,14 @@ provider "aws" {
   region     = "${var.region}"
 }
 
+provider "aws" {
+  # us-east-1 instance
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "us-east-1"
+  alias = "use1"
+}
+
 data "aws_route53_zone" "production" {
   name = "datacite.org"
 }
