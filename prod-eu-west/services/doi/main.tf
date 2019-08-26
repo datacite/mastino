0d124545f0e89ec1256933a124c05cbcf9d4e259
Martin Fenner <martin.fenner@datacite.org>
2019-08-26 11:06:40 +0200

    increase number of doi node servers

--- prod-eu-west/services/doi/main.tf
+++ prod-eu-west/services/doi/main.tf
@@ -3,7 +3,7 @@ resource "aws_ecs_service" "doi" {
   cluster = "${data.aws_ecs_cluster.default.id}"
   launch_type = "FARGATE"
   task_definition = "${aws_ecs_task_definition.doi.arn}"
-  desired_count = 4
+  desired_count = 8
 
   network_configuration {
     security_groups = ["${data.aws_security_group.datacite-private.id}"]
