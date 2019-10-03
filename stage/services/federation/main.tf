2bf9335ee49cc9865c92f27aa762a8b867175a19
Martin Fenner <martin.fenner@datacite.org>
2019-10-03 11:22:04 +0200

    use service registry

--- stage/services/federation/main.tf
+++ stage/services/federation/main.tf
@@ -19,9 +19,9 @@ resource "aws_ecs_service" "federation-stage" {
     container_port   = "80"
   }
 
-  // service_registries {
-  //   registry_arn = "${aws_service_discovery_service.federation-stage.arn}"
-  // }
+  service_registries {
+    registry_arn = "${aws_service_discovery_service.federation-stage.arn}"
+  }
 
   depends_on = [
     "data.aws_lb_listener.stage",
