b3fd5cf0f6054e10939adb143e892ed324aa64b0
Martin Fenner <martin.fenner@datacite.org>
2019-10-27 07:29:47 +0100

    break circular dependency

--- prod-eu-west/vpc/elb-crosscite.tf
+++ prod-eu-west/vpc/elb-crosscite.tf
@@ -25,10 +25,10 @@ resource "aws_lb_listener" "crosscite" {
   ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = "${data.aws_acm_certificate.crosscite.arn}"
 
-  default_action {
-    target_group_arn = "${data.aws_lb_target_group.content-negotation.id}"
-    type             = "forward"
-  }
+  // default_action {
+  //   target_group_arn = "${data.aws_lb_target_group.content-negotation.id}"
+  //   type             = "forward"
+  // }
 }
 
 resource "aws_lb_listener" "crosscite-http" {
