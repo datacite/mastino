2b769c1481848798a9c632271db4532ac3b1a9df
Martin Fenner <martin.fenner@datacite.org>
2019-11-05 20:51:21 +0100

    retire solr stage

--- stage/services/solr/input.tf
+++ stage/services/solr/input.tf
@@ -1,47 +1,47 @@
-provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
-  version    = "~> 1.6"
-}
+// provider "aws" {
+//   access_key = "${var.access_key}"
+//   secret_key = "${var.secret_key}"
+//   region     = "${var.region}"
+//   version    = "~> 1.6"
+// }
 
-data "aws_route53_zone" "production" {
-  name = "datacite.org"
-}
+// data "aws_route53_zone" "production" {
+//   name = "datacite.org"
+// }
 
-data "aws_route53_zone" "internal" {
-  name         = "datacite.org"
-  private_zone = true
-}
+// data "aws_route53_zone" "internal" {
+//   name         = "datacite.org"
+//   private_zone = true
+// }
 
-data "aws_instance" "ecs-stage" {
-  filter {
-    name   = "tag:Name"
-    values = ["ECS-Stage"]
-  }
-}
+// data "aws_instance" "ecs-stage" {
+//   filter {
+//     name   = "tag:Name"
+//     values = ["ECS-Stage"]
+//   }
+// }
 
-data "aws_iam_role" "lambda" {
-  name = "lambda"
-}
+// data "aws_iam_role" "lambda" {
+//   name = "lambda"
+// }
 
-data "aws_security_group" "datacite-private" {
-  id = "${var.security_group_id}"
-}
+// data "aws_security_group" "datacite-private" {
+//   id = "${var.security_group_id}"
+// }
 
-data "aws_subnet" "datacite-private" {
-  id = "${var.subnet_datacite-private_id}"
-}
+// data "aws_subnet" "datacite-private" {
+//   id = "${var.subnet_datacite-private_id}"
+// }
 
-data "aws_subnet" "datacite-alt" {
-  id = "${var.subnet_datacite-alt_id}"
-}
+// data "aws_subnet" "datacite-alt" {
+//   id = "${var.subnet_datacite-alt_id}"
+// }
 
-data "aws_lb" "stage" {
-  name = "${var.lb_name}"
-}
+// data "aws_lb" "stage" {
+//   name = "${var.lb_name}"
+// }
 
-data "aws_lb_listener" "stage" {
-  load_balancer_arn = "${data.aws_lb.stage.arn}"
-  port = 443
-}
+// data "aws_lb_listener" "stage" {
+//   load_balancer_arn = "${data.aws_lb.stage.arn}"
+//   port = 443
+// }
