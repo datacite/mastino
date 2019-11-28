d40ff8794a1afdf3b6054d6d6bccb86e853a7613
Martin Fenner <martin.fenner@datacite.org>
2019-11-28 22:05:51 +0100

    auto-scaling for doi service

--- prod-eu-west/services/doi/main.tf
+++ prod-eu-west/services/doi/main.tf
@@ -3,7 +3,14 @@ resource "aws_ecs_service" "doi" {
   cluster = "${data.aws_ecs_cluster.default.id}"
   launch_type = "FARGATE"
   task_definition = "${aws_ecs_task_definition.doi.arn}"
-  desired_count = 4
+  
+  # Create service with 2 instances to start
+  desired_count = 2
+
+  # Allow external changes without Terraform plan difference
+  lifecycle {
+    ignore_changes = ["desired_count"]
+  }
 
   network_configuration {
     security_groups = ["${data.aws_security_group.datacite-private.id}"]
@@ -28,6 +35,90 @@ resource "aws_ecs_service" "doi" {
   ]
 }
 
+resource "aws_appautoscaling_target" "doi" {
+  max_capacity       = 4
+  min_capacity       = 2
+  resource_id        = "service/default/${aws_ecs_service.doi.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "doi_scale_up" {
+  name               = "scale-up"
+  policy_type        = "StepScaling"
+  resource_id        = "${aws_appautoscaling_target.doi.resource_id}"
+  scalable_dimension = "${aws_appautoscaling_target.doi.scalable_dimension}"
+  service_namespace  = "${aws_appautoscaling_target.doi.service_namespace}"
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 300
+    metric_aggregation_type = "Maximum"
+
+    step_adjustment {
+      metric_interval_upper_bound = 0
+      scaling_adjustment          = 1
+    }
+  }
+}
+
+resource "aws_appautoscaling_policy" "doi_scale_down" {
+  name               = "scale-down"
+  policy_type        = "StepScaling"
+  resource_id        = "${aws_appautoscaling_target.doi.resource_id}"
+  scalable_dimension = "${aws_appautoscaling_target.doi.scalable_dimension}"
+  service_namespace  = "${aws_appautoscaling_target.doi.service_namespace}"
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 300
+    metric_aggregation_type = "Maximum"
+
+    step_adjustment {
+      metric_interval_upper_bound = 0
+      scaling_adjustment          = -1
+    }
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "doi_cpu_scale_up" {
+  alarm_name          = "doi_cpu_scale_up"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "80"
+
+  dimensions {
+    ClusterName = "default"
+    ServiceName = "${aws_ecs_service.doi.name}"
+  }
+
+  alarm_description = "This metric monitors ecs cpu utilization"
+  alarm_actions     = ["${aws_appautoscaling_policy.doi_scale_up.arn}"]
+}
+
+resource "aws_cloudwatch_metric_alarm" "doi_cpu_scale_down" {
+  alarm_name          = "doi_cpu_scale_down"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "20"
+
+  dimensions {
+    ClusterName = "default"
+    ServiceName = "${aws_ecs_service.doi.name}"
+  }
+
+  alarm_description = "This metric monitors ecs cpu utilization"
+  alarm_actions     = ["${aws_appautoscaling_policy.doi_scale_down.arn}"]
+}
+
 resource "aws_cloudwatch_log_group" "doi" {
   name = "/ecs/doi"
 }
@@ -37,8 +128,8 @@ resource "aws_ecs_task_definition" "doi" {
   execution_role_arn = "${data.aws_iam_role.ecs_task_execution_role.arn}"
   network_mode = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu = "2048"
-  memory = "4096"
+  cpu = "1024"
+  memory = "2048"
 
   container_definitions =  "${data.template_file.doi_task.rendered}"
 }
