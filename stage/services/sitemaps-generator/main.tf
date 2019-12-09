50c095203af8d12cf0b1b3502172394408b54065
Martin Fenner <martin.fenner@datacite.org>
2019-12-09 12:13:39 +0000

    use terraform to define scheduled fargate containers

--- stage/services/sitemaps-generator/main.tf
+++ stage/services/sitemaps-generator/main.tf
@@ -17,6 +17,33 @@
 //   security_group_ids = [data.aws_security_group.datacite-private.id]
 // }
 
+resource "aws_cloudwatch_event_rule" "sitemaps-generator-stage" {
+  name = "sitemaps-generator-stage"
+  description = "Run sitemaps-generator-stage container via cron"
+  schedule_expression = "cron(30 12 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "sitemaps-generator-stage" {
+  target_id = "sitemaps-generator-stage"
+  arn = data.aws_ecs_cluster.stage.arn
+  rule = aws_cloudwatch_event_rule.sitemaps-generator-stage.name
+  role_arn  = data.aws_iam_role.ecs_task_execution_role.arn
+
+  ecs_target {
+    task_count          = 1
+    launch_type         = "FARGATE"
+    task_definition_arn = aws_ecs_task_definition.sitemaps-generator-stage.arn
+    
+    network_configuration {
+      security_groups = [data.aws_security_group.datacite-private.id]
+      subnets         = [
+        data.aws_subnet.datacite-private.id,
+        data.aws_subnet.datacite-alt.id
+      ]
+    }
+  }
+}
+
 resource "aws_s3_bucket" "sitemaps-search-test" {
     bucket = "search.test.datacite.org"
     acl = "public-read"
@@ -37,8 +64,8 @@ resource "aws_cloudwatch_log_group" "sitemaps-generator-stage" {
   name = "/ecs/sitemaps-generator-stage"
 }
 
-resource "aws_ecs_task_definition" "sitemaps-generator-test" {
-  family = "sitemaps-generator-test"
+resource "aws_ecs_task_definition" "sitemaps-generator-stage" {
+  family = "sitemaps-generator-stage"
   container_definitions =  templatefile("sitemaps-generator.json",
     {
       access_key  = var.access_key,
