3e08b9e08607b33fb46e1f69ebb762f21b92a73d
Martin Fenner <martin.fenner@datacite.org>
2019-12-09 12:04:35 +0000

    remove module to schedule tasks

--- stage/services/sitemaps-generator/main.tf
+++ stage/services/sitemaps-generator/main.tf
@@ -1,21 +1,21 @@
-module "fargate-scheduled-task" {
-  source  = "baikonur-oss/fargate-scheduled-task/aws"
-  version = "v2.0.1"
+// module "fargate-scheduled-task" {
+//   source  = "baikonur-oss/fargate-scheduled-task/aws"
+//   version = "v2.0.1"
 
-  execution_role_arn  = data.aws_iam_role.ecs_task_execution_role.arn
-  name                = "sitemaps-generator-test"
-  schedule_expression = "cron(55 11 * * ? *)"
-  is_enabled          = "true"
+//   execution_role_arn  = data.aws_iam_role.ecs_task_execution_role.arn
+//   name                = "sitemaps-generator-test"
+//   schedule_expression = "cron(55 11 * * ? *)"
+//   is_enabled          = "true"
 
-  target_cluster_arn = data.aws_ecs_cluster.stage.arn
+//   target_cluster_arn = data.aws_ecs_cluster.stage.arn
 
-  task_definition_arn = aws_ecs_task_definition.sitemaps-generator-test.arn
-  task_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
-  task_count          = 1
+//   task_definition_arn = aws_ecs_task_definition.sitemaps-generator-test.arn
+//   task_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
+//   task_count          = 1
 
-  subnet_ids = [data.aws_subnet.datacite-private.id, data.aws_subnet.datacite-alt.id]
-  security_group_ids = [data.aws_security_group.datacite-private.id]
-}
+//   subnet_ids = [data.aws_subnet.datacite-private.id, data.aws_subnet.datacite-alt.id]
+//   security_group_ids = [data.aws_security_group.datacite-private.id]
+// }
 
 resource "aws_s3_bucket" "sitemaps-search-test" {
     bucket = "search.test.datacite.org"
