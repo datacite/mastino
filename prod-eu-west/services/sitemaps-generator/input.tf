4324ab8dcefc0e4d8419cb36c3ce4e49084e2a20
Martin Fenner <martin.fenner@datacite.org>
2019-12-09 15:41:04 +0000

    terraform 0.12 syntax

--- prod-eu-west/services/sitemaps-generator/input.tf
+++ prod-eu-west/services/sitemaps-generator/input.tf
@@ -1,36 +1,30 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
-}
-
-data "template_file" "sitemaps_generator_task" {
-  template = "${file("sitemaps-generator.json")}"
-
-  vars {
-    access_key  = "${var.access_key}"
-    secret_key  = "${var.secret_key}"
-    region      = "${var.region}"
-  }
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
 }
 
 data "aws_vpc_endpoint" "datacite" {
-  vpc_id       = "${var.vpc_id}"
+  vpc_id       = var.vpc_id
   service_name = "com.amazonaws.eu-west-1.s3"
 }
 
-data "aws_iam_role" "lambda" {
-  name = "lambda"
+data "aws_ecs_cluster" "default" {
+  cluster_name = "default"
+}
+
+data "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
 }
 
 data "aws_security_group" "datacite-private" {
-  id = "${var.security_group_id}"
+  id = var.security_group_id
 }
 
 data "aws_subnet" "datacite-private" {
-  id = "${var.subnet_datacite-private_id}"
+  id = var.subnet_datacite-private_id
 }
 
 data "aws_subnet" "datacite-alt" {
-  id = "${var.subnet_datacite-alt_id}"
+  id = var.subnet_datacite-alt_id
 }
