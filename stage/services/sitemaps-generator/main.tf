2f866f3bba1715c17c4d2eb3a8acfdcc77a9a40d
Martin Fenner <martin.fenner@datacite.org>
2019-12-09 11:43:42 +0000

    use different terraform module

--- stage/services/sitemaps-generator/main.tf
+++ stage/services/sitemaps-generator/main.tf
@@ -1,21 +1,44 @@
-module "fargate-scheduled-task" {
-  source  = "baikonur-oss/fargate-scheduled-task/aws"
-  version = "v2.0.1"
-
-  execution_role_arn  = data.aws_iam_role.ecs_task_execution_role.arn
-  name                = "sitemaps-generator-test"
-  schedule_expression = "cron(30 11 * * ? *)"
-  is_enabled          = "true"
-
-  target_cluster_arn = data.aws_ecs_cluster.stage.arn
-
-  task_definition_arn = aws_ecs_task_definition.sitemaps-generator-test.arn
-  task_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
-  task_count          = "1"
-
-  subnet_ids = [data.aws_subnet.datacite-private.id, data.aws_subnet.datacite-alt.id]
-  security_group_ids = [data.aws_security_group.datacite-private.id]
+module "fargate-scheduled-task-multicontainer" {
+  source  = "halfdanrump/fargate-scheduled-task-multicontainer/aws"
+  version = "12.6.1"
+  
+  name                  = "sitemaps-generator-test"
+  environment           = "stage"
+  network_mode          = "awsvpc"
+  launch_type           = "FARGATE"
+  container_definitions = templatefile("sitemaps-generator.json",
+    {
+      access_key  = var.access_key,
+      secret_key  = var.secret_key,
+      region      = var.region
+    })
+  schedule_expression   = "cron(30 11 * * ? *)"
+  cluster_arn           = data.aws_ecs_cluster.stage.arn
+  memory                = "1024"
+  cpu                   = "256"
+  task_count            = 1
+  subnets               = [data.aws_subnet.datacite-private.id, data.aws_subnet.datacite-alt.id]
+  security_groups       = [data.aws_security_group.datacite-private.id]
 }
+
+// module "fargate-scheduled-task" {
+//   source  = "baikonur-oss/fargate-scheduled-task/aws"
+//   version = "v2.0.1"
+
+//   execution_role_arn  = data.aws_iam_role.ecs_task_execution_role.arn
+//   name                = "sitemaps-generator-test"
+//   schedule_expression = "cron(30 11 * * ? *)"
+//   is_enabled          = "true"
+
+//   target_cluster_arn = data.aws_ecs_cluster.stage.arn
+
+//   task_definition_arn = aws_ecs_task_definition.sitemaps-generator-test.arn
+//   task_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
+//   task_count          = 1
+
+//   subnet_ids = [data.aws_subnet.datacite-private.id, data.aws_subnet.datacite-alt.id]
+//   security_group_ids = [data.aws_security_group.datacite-private.id]
+// }
 
 resource "aws_s3_bucket" "sitemaps-search-test" {
     bucket = "search.test.datacite.org"
@@ -37,12 +60,12 @@ resource "aws_cloudwatch_log_group" "sitemaps-generator-stage" {
   name = "/ecs/sitemaps-generator-stage"
 }
 
-resource "aws_ecs_task_definition" "sitemaps-generator-test" {
-  family = "sitemaps-generator-test"
-  container_definitions =  templatefile("sitemaps-generator.json",
-    {
-      access_key  = var.access_key,
-      secret_key  = var.secret_key,
-      region      = var.region
-    })
-}
+// resource "aws_ecs_task_definition" "sitemaps-generator-test" {
+//   family = "sitemaps-generator-test"
+//   container_definitions =  templatefile("sitemaps-generator.json",
+//     {
+//       access_key  = var.access_key,
+//       secret_key  = var.secret_key,
+//       region      = var.region
+//     })
+// }
