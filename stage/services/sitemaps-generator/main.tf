ed0d7005219845cd0b10c419658a0948de404be0
Martin Fenner <martin.fenner@datacite.org>
2019-12-09 13:34:16 +0000

    move iam role defintion

--- stage/services/sitemaps-generator/main.tf
+++ stage/services/sitemaps-generator/main.tf
@@ -1,57 +1,14 @@
-resource "aws_iam_role" "ecs_events" {
-  name = "ecs_events"
-
-  assume_role_policy = <<DOC
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "events.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-DOC
-}
-
-resource "aws_iam_role_policy" "ecs_events_run_task_with_any_role" {
-  name = "ecs_events_run_task_with_any_role"
-  role = aws_iam_role.ecs_events.id
-
-  policy = <<DOC
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "iam:PassRole",
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": "ecs:RunTask",
-            "Resource": "${replace(aws_ecs_task_definition.sitemaps-generator-stage.arn, "/:\\d+$/", ":*")}"
-        }
-    ]
-}
-DOC
-}
-
 resource "aws_cloudwatch_event_rule" "sitemaps-generator-stage" {
   name = "sitemaps-generator-stage"
   description = "Run sitemaps-generator-stage container via cron"
-  schedule_expression = "cron(20 13 * * ? *)"
+  schedule_expression = "cron(20 6 * * ? *)"
 }
 
 resource "aws_cloudwatch_event_target" "sitemaps-generator-stage" {
   target_id = "sitemaps-generator-stage"
   arn = data.aws_ecs_cluster.stage.arn
   rule = aws_cloudwatch_event_rule.sitemaps-generator-stage.name
-  role_arn  = aws_iam_role.ecs_events.arn
+  role_arn  = data.aws_iam_role.ecs_events.arn
 
   ecs_target {
     task_count          = 1
