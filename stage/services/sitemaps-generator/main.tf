4961d870dfb452f32bddc58388d237cfc008deef
Martin Fenner <martin.fenner@datacite.org>
2019-12-14 07:01:38 +0000

    run sitemaps-generator stage weekly

--- stage/services/sitemaps-generator/main.tf
+++ stage/services/sitemaps-generator/main.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_event_rule" "sitemaps-generator-stage" {
   name = "sitemaps-generator-stage"
   description = "Run sitemaps-generator-stage container via cron"
-  schedule_expression = "cron(15 4 * * ? *)"
+  schedule_expression = "cron(15 4 ? * SUN *)"
 }
 
 resource "aws_cloudwatch_event_target" "sitemaps-generator-stage" {
