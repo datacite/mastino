bbaa647b14eb977dfb2c76ae0c9437c6657a48f0
Martin Fenner <martin.fenner@datacite.org>
2019-12-18 06:47:38 +0100

    terraform 0.12 for client-api

--- prod-eu-west/services/client-api/main.tf
+++ prod-eu-west/services/client-api/main.tf
@@ -168,7 +168,36 @@ resource "aws_ecs_task_definition" "client-api" {
   requires_compatibilities = ["FARGATE"]
   cpu = "2048"
   memory = "8192"
-  container_definitions =  "${data.template_file.client-api_task.rendered}"
+  container_definitions =  templatefile("client-api.json",
+    {
+      re3data_url        = "${var.re3data_url}"
+      api_url            = "${var.api_url}"
+      bracco_url         = "${var.bracco_url}"
+      jwt_public_key     = "${var.jwt_public_key}"
+      jwt_private_key    = "${var.jwt_private_key}"
+      session_encrypted_cookie_salt = "${var.session_encrypted_cookie_salt}"
+      handle_url         = "${var.handle_url}"
+      handle_username    = "${var.handle_username}"
+      handle_password    = "${var.handle_password}"
+      mysql_user         = "${var.mysql_user}"
+      mysql_password     = "${var.mysql_password}"
+      mysql_database     = "${var.mysql_database}"
+      mysql_host         = "${var.mysql_host}"
+      es_name            = "${var.es_name}"
+      es_host            = "${var.es_host}"
+      public_key         = "${var.public_key}"
+      access_key         = "${var.access_key}"
+      secret_key         = "${var.secret_key}"
+      region             = "${var.region}"
+      s3_bucket          = "${var.s3_bucket}"
+      admin_username     = "${var.admin_username}"
+      admin_password     = "${var.admin_password}"
+      sentry_dsn         = "${var.sentry_dsn}"
+      mailgun_api_key    = "${var.mailgun_api_key}"
+      memcache_servers   = "${var.memcache_servers}"
+      slack_webhook_url  = "${var.slack_webhook_url}"
+      version            = "${var.lupo_tags["version"]}"
+    })
 }
 
 resource "aws_lb_target_group" "client-api" {
