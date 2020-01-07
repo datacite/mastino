076211429ce9e2df570aea99314c4a633883437b
kjgarza <kgarza@datacite.org>
2020-01-07 14:34:23 +0100

    CPU capacity gets maxup during event ingestion
    https://app.datadoghq.com/dashboard/kaa-iy2-qkm/datacite-service-health-cloned?from_ts=1578192445818&live=false&tile_size=m&to_ts=1578256224727

--- prod-eu-west/services/levriero/main.tf
+++ prod-eu-west/services/levriero/main.tf
@@ -36,7 +36,7 @@ resource "aws_ecs_service" "levriero" {
 }
 
 resource "aws_appautoscaling_target" "levriero" {
-  max_capacity       = 2
+  max_capacity       = 4
   min_capacity       = 2
   resource_id        = "service/default/${aws_ecs_service.levriero.name}"
   scalable_dimension = "ecs:service:DesiredCount"
