4d0a06c0db7f660275e9731f6232754715c7c858
Martin Fenner <martin.fenner@datacite.org>
2020-01-17 12:34:49 +0100

    adding third subnet to vpc

---
+++ prod-eu-west/vpc/vpc.tf
@@ -0,0 +1,9 @@
+resource "aws_subnet" "datacite-private-3" {
+    vpc_id = "${data.aws_vpc.datacite.id}"
+    cidr_block = "10.0.31.0/24"
+    availability_zone = "eu-west-1c"
+
+    tags {
+        Name = "datacite-private-3"
+    }
+}
