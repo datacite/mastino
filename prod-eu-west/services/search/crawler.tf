acd93f4a097d196c16ddbc85b12ac39e75b74ef2
Martin Fenner <martin.fenner@datacite.org>
2020-01-30 06:49:19 +0000

    remove duplicated resource

--- prod-eu-west/services/search/crawler.tf
+++ prod-eu-west/services/search/crawler.tf
@@ -1,15 +1,3 @@
-resource "aws_s3_bucket" "search" {
-    bucket = "search.datacite.org"
-    acl = "public-read"
-    policy = "${data.template_file.search.rendered}"
-    website {
-        index_document = "index.html"
-    }
-    tags {
-        Name = "Search"
-    }
-}
-
 resource "aws_ecs_service" "search-crawler" {
   name = "search-crawler"
   cluster = "${data.aws_ecs_cluster.default.id}"
