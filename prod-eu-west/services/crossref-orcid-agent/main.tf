a75e05f506d16326e7cd6f4c7c7f56bc6fe814ff
Martin Fenner <martin.fenner@datacite.org>
2020-02-08 17:22:59 +0100

    add crossref-orcid-agent

---
+++ prod-eu-west/services/crossref-orcid-agent/main.tf
@@ -0,0 +1,40 @@
+resource "aws_cloudwatch_event_rule" "crossref-orcid-agent" {
+  name = "crossref-orcid-agent"
+  description = "Trigger crossref-orcid agent via cron"
+  schedule_expression = "cron(55 16 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "crossref-orcid-agent" {
+  target_id = "crossref-orcid-agent"
+  rule = aws_cloudwatch_event_rule.crossref-orcid-agent.name
+  arn = aws_lambda_function.crossref-agent-orcid.arn
+}
+
+resource "aws_lambda_function" "crossref-orcid-agent" {
+  filename = "crossref-orcid-agent_runner.js.zip"
+  function_name = "crossref-orcid-agent"
+  role = data.aws_iam_role.lambda.arn
+  handler = "crossref-orcid-agent_runner.handler"
+  runtime = "nodejs10.x"
+  source_code_hash = base64sha256(file("crossref-orcid-agent_runner.js.zip"))
+  timeout = "270"
+
+  vpc_config {
+    subnet_ids = [data.aws_subnet.datacite-private.id, data.aws_subnet.datacite-alt.id]
+    security_group_ids = [data.aws_security_group.datacite-private.id]
+  }
+  environment {
+    variables = {
+      host     = var.host
+      token    = var.token
+    }
+  }
+}
+
+resource "aws_lambda_permission" "crossref-orcid-agent" {
+  statement_id = "AllowExecutionFromCloudWatch"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.crossref-orcid-agent.function_name
+  principal = "events.amazonaws.com"
+  source_arn = aws_cloudwatch_event_rule.crossref-orcid-agent.arn
+}
---
+++ prod-eu-west/services/crossref-orcid-agent/var.tf
@@ -0,0 +1,14 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+    default = "eu-west-1"
+}
+
+variable "security_group_id" {}
+variable "subnet_datacite-private_id" {}
+variable "subnet_datacite-alt_id" {}
+
+variable "host" {
+    default = "api.datacite.org"
+}
+variable "token" {}
