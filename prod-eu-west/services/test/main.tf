b72ed0e31ad67d3178584feaad0e99e96f6006cf
Richard Hallett <rhallett@datacite.org>
2020-03-02 16:39:06 +0100

    Move test.datacite.org to ELB

--- prod-eu-west/services/test/main.tf
+++ prod-eu-west/services/test/main.tf
@@ -1,86 +1,86 @@
-resource "aws_s3_bucket" "test" {
-    bucket = "test.datacite.org"
-    acl = "public-read"
-    policy = "${data.template_file.test.rendered}"
-    website {
-        index_document = "index.html"
-        error_document = "404.html"
-        routing_rules = "${file("routing-rules.json")}"
-    }
-    tags {
-        Name = "Test"
-    }
-}
+# resource "aws_s3_bucket" "test" {
+#     bucket = "test.datacite.org"
+#     acl = "public-read"
+#     policy = "${data.template_file.test.rendered}"
+#     website {
+#         index_document = "index.html"
+#         error_document = "404.html"
+#         routing_rules = "${file("routing-rules.json")}"
+#     }
+#     tags {
+#         Name = "Test"
+#     }
+# }
 
-resource "aws_cloudfront_distribution" "test" {
-  origin {
-    domain_name = "${aws_s3_bucket.test.website_endpoint}"
-    origin_id   = "test.datacite.org"
+# resource "aws_cloudfront_distribution" "test" {
+#   origin {
+#     domain_name = "${aws_s3_bucket.test.website_endpoint}"
+#     origin_id   = "test.datacite.org"
 
-    custom_origin_config {
-      origin_protocol_policy = "http-only"
-      http_port = "80"
-      https_port = "443"
-      origin_ssl_protocols = ["TLSv1"]
-    }
-  }
+#     custom_origin_config {
+#       origin_protocol_policy = "http-only"
+#       http_port = "80"
+#       https_port = "443"
+#       origin_ssl_protocols = ["TLSv1"]
+#     }
+#   }
 
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
+#   enabled             = true
+#   is_ipv6_enabled     = true
+#   default_root_object = "index.html"
 
-  logging_config {
-    include_cookies = false
-    bucket          = "${data.aws_s3_bucket.logs.bucket_domain_name}"
-    prefix          = "test/"
-  }
+#   logging_config {
+#     include_cookies = false
+#     bucket          = "${data.aws_s3_bucket.logs.bucket_domain_name}"
+#     prefix          = "test/"
+#   }
 
-  aliases = ["test.datacite.org"]
+#   aliases = ["test.datacite.org"]
 
-  default_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "test.datacite.org"
+#   default_cache_behavior {
+#     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+#     cached_methods   = ["GET", "HEAD"]
+#     target_origin_id = "test.datacite.org"
 
-    forwarded_values {
-      query_string = false
+#     forwarded_values {
+#       query_string = false
 
-      cookies {
-        forward = "whitelist"
-        whitelisted_names = ["_datacite_jwt"]
-      }
-    }
+#       cookies {
+#         forward = "whitelist"
+#         whitelisted_names = ["_datacite_jwt"]
+#       }
+#     }
 
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
+#     viewer_protocol_policy = "redirect-to-https"
+#     min_ttl                = 0
+#     default_ttl            = 3600
+#     max_ttl                = 86400
+#   }
 
-  price_class = "PriceClass_All"
+#   price_class = "PriceClass_All"
 
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
+#   restrictions {
+#     geo_restriction {
+#       restriction_type = "none"
+#     }
+#   }
 
-  tags {
-    Environment = "prod"
-  }
+#   tags {
+#     Environment = "prod"
+#   }
 
-  viewer_certificate {
-    acm_certificate_arn = "${data.aws_acm_certificate.cloudfront.arn}"
-    ssl_support_method  = "sni-only"
-  }
-}
+#   viewer_certificate {
+#     acm_certificate_arn = "${data.aws_acm_certificate.cloudfront.arn}"
+#     ssl_support_method  = "sni-only"
+#   }
+# }
 
 resource "aws_route53_record" "test" {
    zone_id = "${data.aws_route53_zone.production.zone_id}"
    name = "test.datacite.org"
    type = "CNAME"
    ttl = "${var.ttl}"
-   records = ["${aws_cloudfront_distribution.test.domain_name}"]
+   records = ["${data.aws_lb.default.dns_name}"]
 }
 
 resource "aws_route53_record" "split-test" {
@@ -88,7 +88,7 @@ resource "aws_route53_record" "split-test" {
    name = "test.datacite.org"
    type = "CNAME"
    ttl = "${var.ttl}"
-   records = ["${aws_cloudfront_distribution.test.domain_name}"]
+   records = ["${data.aws_lb.default.dns_name}"]
 }
 
 resource "aws_lb_listener_rule" "test-support-redirect" {
@@ -99,7 +99,8 @@ resource "aws_lb_listener_rule" "test-support-redirect" {
 
     redirect {
       host = "support.datacite.org"
-      path = "/docs/testing-guide"
+      path = "/docs/testing-guide/"
+      query = ""
       port        = "443"
       protocol    = "HTTPS"
       status_code = "HTTP_301"
@@ -108,6 +109,6 @@ resource "aws_lb_listener_rule" "test-support-redirect" {
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.test.name}"]
+    values = ["test.datacite.org"]
   }
 }
