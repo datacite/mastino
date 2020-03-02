590b506e4d756e5cad623cea45ed05d494d86589
Richard Hallett <rhallett@datacite.org>
2020-03-02 14:39:10 +0100

    Add missing var

--- prod-eu-west/services/test/input.tf
+++ prod-eu-west/services/test/input.tf
@@ -40,6 +40,10 @@ data "aws_s3_bucket" "logs" {
   bucket = "logs.datacite.org"
 }
 
+data "aws_lb" "default" {
+  name = var.lb_name
+}
+
 data "aws_lb_listener" "default" {
   load_balancer_arn = "${data.aws_lb.default.arn}"
   port = 443
--- prod-eu-west/services/test/var.tf
+++ prod-eu-west/services/test/var.tf
@@ -4,6 +4,10 @@ variable "region" {
     default = "eu-west-1"
 }
 
+variable "lb_name" {
+  default = "lb"
+}
+
 variable "ttl" {
   default = "300"
 }
