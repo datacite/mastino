c9f8eaf7dd23182a6f0bdf8ab9db60f10aaeb4d4
Richard Hallett <rhallett@datacite.org>
2020-04-16 11:38:15 +0200

    Fix accidental wrong var usage.

--- pidservices/services/stage/input.tf
+++ pidservices/services/stage/input.tf
@@ -6,8 +6,8 @@ provider "aws" {
 
 provider "aws" {
   # us-east-1 instance
-  access_key = "var.access_key"
-  secret_key = "var.secret_key"
+  access_key = var.access_key
+  secret_key = var.secret_key
   region = "us-east-1"
   alias = "use1"
 }
