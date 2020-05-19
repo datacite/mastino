70c9b8cc07a7cbc4ae585e254f4b617bce82b408
Martin Fenner <martin.fenner@datacite.org>
2020-05-19 20:33:13 +0200

    terraform 0.12 syntax for test vpc

--- test/vpc/elb.tf
+++ test/vpc/elb.tf
@@ -1,14 +1,14 @@
 resource "aws_lb" "test" {
   name = "lb-test"
   internal = false
-  subnets = ["${data.aws_subnet.datacite-public.id}", "${data.aws_subnet.datacite-public-alt.id}"]
-  security_groups = ["${data.aws_security_group.datacite-public.id}"]
+  subnets = [data.aws_subnet.datacite-public.id, data.aws_subnet.datacite-public-alt.id]
+  security_groups = [data.aws_security_group.datacite-public.id]
   idle_timeout = 200
 
   enable_deletion_protection = true
 
   access_logs {
-    bucket = "${aws_s3_bucket.logs-test.bucket}"
+    bucket = aws_s3_bucket.logs-test.bucket
     prefix = "lb-test"
     enabled = false
   }
@@ -22,21 +22,21 @@ resource "aws_lb" "test" {
 resource "aws_s3_bucket" "logs-test" {
   bucket = "logs.test.datacite.org"
   acl    = "private"
-  policy = "${data.template_file.logs-test.rendered}"
+  policy = data.template_file.logs-test.rendered
   tags {
       Name = "lb-test"
   }
 }
 
 resource "aws_lb_listener" "test" {
-  load_balancer_arn = "${aws_lb.test.id}"
+  load_balancer_arn = aws_lb.test.id
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = "${data.aws_acm_certificate.test.arn}"
+  certificate_arn   = data.aws_acm_certificate.test.arn
 
   default_action {
-    target_group_arn = "${data.aws_lb_target_group.api-test.id}"
+    target_group_arn = data.aws_lb_target_group.api-test.id
     type             = "forward"
   }
 }
@@ -45,7 +45,7 @@ resource "aws_lb_target_group" "content-negotiation-test" {
   name     = "content-negotiation-test"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = "${var.vpc_id}"
+  vpc_id   = var.vpc_id
   target_type = "ip"
 
   health_check {
@@ -54,7 +54,7 @@ resource "aws_lb_target_group" "content-negotiation-test" {
 }
 
 resource "aws_lb_listener" "http-test" {
-  load_balancer_arn = "${aws_lb.test.id}"
+  load_balancer_arn = aws_lb.test.id
   port              = "80"
   protocol          = "HTTP"
 
@@ -72,32 +72,32 @@ resource "aws_lb_listener" "http-test" {
 /*
 
 resource "aws_lb_listener_rule" "handle-test" {
-  listener_arn = "${aws_lb_listener.test.arn}"
+  listener_arn = aws_lb_listener.test.arn
   priority     = 3
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.handle-test.arn}"
+    target_group_arn = aws_lb_target_group.handle-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.handle-test.name}"]
+    values = [aws_route53_record.handle-test.name]
   }
 }
 
 resource "aws_lb_listener_rule" "client-api-test-dois" {
-  listener_arn = "${aws_lb_listener.test.arn}"
+  listener_arn = aws_lb_listener.test.arn
   priority     = 20
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.client-api-test.arn}"
+    target_group_arn = aws_lb_target_group.client-api-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.test-api.name}"]
+    values = [aws_route53_record.test-api.name]
   }
   condition {
     field  = "path-pattern"
@@ -106,17 +106,17 @@ resource "aws_lb_listener_rule" "client-api-test-dois" {
 }
 
 resource "aws_lb_listener_rule" "client-api-test-clients" {
-  listener_arn = "${aws_lb_listener.test.arn}"
+  listener_arn = aws_lb_listener.test.arn
   priority     = 21
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.client-api-test.arn}"
+    target_group_arn = aws_lb_target_group.client-api-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.test-api.name}"]
+    values = [aws_route53_record.test-api.name]
   }
   condition {
     field  = "path-pattern"
@@ -125,17 +125,17 @@ resource "aws_lb_listener_rule" "client-api-test-clients" {
 }
 
 resource "aws_lb_listener_rule" "client-api-test-data-centers" {
-  listener_arn = "${aws_lb_listener.test.arn}"
+  listener_arn = aws_lb_listener.test.arn
   priority     = 22
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.client-api-test.arn}"
+    target_group_arn = aws_lb_target_group.client-api-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.test-api.name}"]
+    values = [aws_route53_record.test-api.name]
   }
   condition {
     field  = "path-pattern"
@@ -144,17 +144,17 @@ resource "aws_lb_listener_rule" "client-api-test-data-centers" {
 }
 
 resource "aws_lb_listener_rule" "client-api-test-providers" {
-  listener_arn = "${aws_lb_listener.test.arn}"
+  listener_arn = aws_lb_listener.test.arn
   priority     = 23
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.client-api-test.arn}"
+    target_group_arn = aws_lb_target_group.client-api-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.test-api.name}"]
+    values = [aws_route53_record.test-api.name]
   }
   condition {
     field  = "path-pattern"
@@ -163,17 +163,17 @@ resource "aws_lb_listener_rule" "client-api-test-providers" {
 }
 
 resource "aws_lb_listener_rule" "client-api-test-members" {
-  listener_arn = "${aws_lb_listener.test.arn}"
+  listener_arn = aws_lb_listener.test.arn
   priority     = 24
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.client-api-test.arn}"
+    target_group_arn = aws_lb_target_group.client-api-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.test-api.name}"]
+    values = [aws_route53_record.test-api.name]
   }
   condition {
     field  = "path-pattern"
@@ -182,17 +182,17 @@ resource "aws_lb_listener_rule" "client-api-test-members" {
 }
 
 resource "aws_lb_listener_rule" "client-api-test-prefixes" {
-  listener_arn = "${aws_lb_listener.test.arn}"
+  listener_arn = aws_lb_listener.test.arn
   priority     = 25
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.client-api-test.arn}"
+    target_group_arn = aws_lb_target_group.client-api-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.test-api.name}"]
+    values = [aws_route53_record.test-api.name]
   }
   condition {
     field  = "path-pattern"
@@ -201,17 +201,17 @@ resource "aws_lb_listener_rule" "client-api-test-prefixes" {
 }
 
 resource "aws_lb_listener_rule" "client-api-test-provider-prefixes" {
-  listener_arn = "${aws_lb_listener.test.arn}"
+  listener_arn = aws_lb_listener.test.arn
   priority     = 26
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.client-api-test.arn}"
+    target_group_arn = aws_lb_target_group.client-api-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.test-api.name}"]
+    values = [aws_route53_record.test-api.name]
   }
   condition {
     field  = "path-pattern"
@@ -220,17 +220,17 @@ resource "aws_lb_listener_rule" "client-api-test-provider-prefixes" {
 }
 
 resource "aws_lb_listener_rule" "client-api-test-client-prefixes" {
-  listener_arn = "${aws_lb_listener.test.arn}"
+  listener_arn = aws_lb_listener.test.arn
   priority     = 27
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.client-api-test.arn}"
+    target_group_arn = aws_lb_target_group.client-api-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.test-api.name}"]
+    values = [aws_route53_record.test-api.name]
   }
   condition {
     field  = "path-pattern"
@@ -239,17 +239,17 @@ resource "aws_lb_listener_rule" "client-api-test-client-prefixes" {
 }
 
 resource "aws_lb_listener_rule" "client-api-test-repositories" {
-  listener_arn = "${aws_lb_listener.test.arn}"
+  listener_arn = aws_lb_listener.test.arn
   priority     = 28
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.client-api-test.arn}"
+    target_group_arn = aws_lb_target_group.client-api-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.test-api.name}"]
+    values = [aws_route53_record.test-api.name]
   }
   condition {
     field  = "path-pattern"
@@ -258,17 +258,17 @@ resource "aws_lb_listener_rule" "client-api-test-repositories" {
 }
 
 resource "aws_lb_listener_rule" "client-api-test-status" {
-  listener_arn = "${aws_lb_listener.test.arn}"
+  listener_arn = aws_lb_listener.test.arn
   priority     = 29
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.client-api-test.arn}"
+    target_group_arn = aws_lb_target_group.client-api-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.test-api.name}"]
+    values = [aws_route53_record.test-api.name]
   }
   condition {
     field  = "path-pattern"
@@ -277,17 +277,17 @@ resource "aws_lb_listener_rule" "client-api-test-status" {
 }
 
 resource "aws_lb_listener_rule" "client-api-test-heartbeat" {
-  listener_arn = "${aws_lb_listener.test.arn}"
+  listener_arn = aws_lb_listener.test.arn
   priority     = 30
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.client-api-test.arn}"
+    target_group_arn = aws_lb_target_group.client-api-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.test-api.name}"]
+    values = [aws_route53_record.test-api.name]
   }
   condition {
     field  = "path-pattern"
@@ -296,47 +296,47 @@ resource "aws_lb_listener_rule" "client-api-test-heartbeat" {
 }
 
 resource "aws_lb_listener_rule" "data-test" {
-  listener_arn = "${aws_lb_listener.test.arn}"
+  listener_arn = aws_lb_listener.test.arn
   priority     = 60
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.data-test.arn}"
+    target_group_arn = aws_lb_target_group.data-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.data-test.name}"]
+    values = [aws_route53_record.data-test.name]
   }
 }
 
 resource "aws_lb_listener_rule" "elastic-api-test" {
-  listener_arn = "${aws_lb_listener.test.arn}"
+  listener_arn = aws_lb_listener.test.arn
   priority     = 70
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.elastic-api-test.arn}"
+    target_group_arn = aws_lb_target_group.elastic-api-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.elastic-api-test.name}"]
+    values = [aws_route53_record.elastic-api-test.name]
   }
 }
 
 resource "aws_lb_listener_rule" "solr-test-api" {
-  listener_arn = "${aws_lb_listener.test.arn}"
+  listener_arn = aws_lb_listener.test.arn
   priority     = 80
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.solr-test.arn}"
+    target_group_arn = aws_lb_target_group.solr-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.search-test.name}"]
+    values = [aws_route53_record.search-test.name]
   }
   condition {
     field  = "path-pattern"
@@ -345,17 +345,17 @@ resource "aws_lb_listener_rule" "solr-test-api" {
 }
 
 resource "aws_lb_listener_rule" "solr-test-list" {
-  listener_arn = "${aws_lb_listener.test.arn}"
+  listener_arn = aws_lb_listener.test.arn
   priority     = 81
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.solr-test.arn}"
+    target_group_arn = aws_lb_target_group.solr-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.search-test.name}"]
+    values = [aws_route53_record.search-test.name]
   }
   condition {
     field  = "path-pattern"
@@ -364,17 +364,17 @@ resource "aws_lb_listener_rule" "solr-test-list" {
 }
 
 resource "aws_lb_listener_rule" "solr-test-ui" {
-  listener_arn = "${aws_lb_listener.test.arn}"
+  listener_arn = aws_lb_listener.test.arn
   priority     = 82
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.solr-test.arn}"
+    target_group_arn = aws_lb_target_group.solr-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.search-test.name}"]
+    values = [aws_route53_record.search-test.name]
   }
   condition {
     field  = "path-pattern"
@@ -383,17 +383,17 @@ resource "aws_lb_listener_rule" "solr-test-ui" {
 }
 
 resource "aws_lb_listener_rule" "solr-test-resources" {
-  listener_arn = "${aws_lb_listener.test.arn}"
+  listener_arn = aws_lb_listener.test.arn
   priority     = 83
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.solr-test.arn}"
+    target_group_arn = aws_lb_target_group.solr-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.search-test.name}"]
+    values = [aws_route53_record.search-test.name]
   }
   condition {
     field  = "path-pattern"
@@ -402,61 +402,61 @@ resource "aws_lb_listener_rule" "solr-test-resources" {
 }
 
 resource "aws_lb_listener_rule" "search-test" {
-  listener_arn = "${aws_lb_listener.test.arn}"
+  listener_arn = aws_lb_listener.test.arn
   priority     = 89
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.search-test.arn}"
+    target_group_arn = aws_lb_target_group.search-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.search-test.name}"]
+    values = [aws_route53_record.search-test.name]
   }
 }
 
 resource "aws_lb_listener_rule" "solr-test" {
-  listener_arn = "${aws_lb_listener.test.arn}"
+  listener_arn = aws_lb_listener.test.arn
   priority     = 90
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.solr-test.arn}"
+    target_group_arn = aws_lb_target_group.solr-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.solr-test.name}"]
+    values = [aws_route53_record.solr-test.name]
   }
 }
 
 resource "aws_lb_listener_rule" "oai-test" {
-  listener_arn = "${aws_lb_listener.test.arn}"
+  listener_arn = aws_lb_listener.test.arn
   priority     = 100
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.oai-test.arn}"
+    target_group_arn = aws_lb_target_group.oai-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.oai-test.name}"]
+    values = [aws_route53_record.oai-test.name]
   }
 }
 
 resource "aws_lb_listener_rule" "citation-test" {
-  listener_arn = "${aws_lb_listener.test.arn}"
+  listener_arn = aws_lb_listener.test.arn
   priority     = 110
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.citation-test.arn}"
+    target_group_arn = aws_lb_target_group.citation-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.citation-test.name}"]
+    values = [aws_route53_record.citation-test.name]
   }
 } */
--- test/vpc/input.tf
+++ test/vpc/input.tf
@@ -1,32 +1,32 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
-  version    = "~> 1.6"
+  access_key = testvar.access_keytest
+  secret_key = testvar.secret_keytest
+  region     = testvar.regiontest
+  version    = "~> 2.7"
 }
 
 data "aws_security_group" "datacite-public" {
-  id = "${var.security_group_public_id}"
+  id = testvar.security_group_public_idtest
 }
 
 data "aws_security_group" "datacite-private" {
-  id = "${var.security_group_private_id}"
+  id = testvar.security_group_private_idtest
 }
 
 data "aws_subnet" "datacite-public" {
-  id = "${var.subnet_datacite-public_id}"
+  id = testvar.subnet_datacite-public_idtest
 }
 
 data "aws_subnet" "datacite-private" {
-  id = "${var.subnet_datacite-private_id}"
+  id = testvar.subnet_datacite-private_idtest
 }
 
 data "aws_subnet" "datacite-public-alt" {
-  id = "${var.subnet_datacite-public-alt_id}"
+  id = testvar.subnet_datacite-public-alt_idtest
 }
 
 data "aws_subnet" "datacite-alt" {
-  id = "${var.subnet_datacite-alt_id}"
+  id = testvar.subnet_datacite-alt_idtest
 }
 
 data "aws_route53_zone" "production" {
@@ -57,7 +57,7 @@ data "aws_lb_target_group" "api-test" {
 }
 
 data "template_file" "logs-test" {
-  template = "${file("s3_lb_write_access.json")}"
+  template = testfile("s3_lb_write_access.json")test
 
   vars {
     bucket_name = "logs.test.datacite.org"
