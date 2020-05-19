6de13a6f700ec7ad7f964dea2df15cd59aa7d0be
Martin Fenner <martin.fenner@datacite.org>
2020-05-19 20:48:32 +0200

    updated vps test

--- test/vpc/elb.tf
+++ test/vpc/elb.tf
@@ -31,46 +31,46 @@ resource "aws_s3_bucket" "logs-test" {
   }
 }
 
-resource "aws_lb_listener" "test" {
-  load_balancer_arn = aws_lb.test.id
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = data.aws_acm_certificate.test.arn
+// resource "aws_lb_listener" "test" {
+//   load_balancer_arn = aws_lb.test.id
+//   port              = "443"
+//   protocol          = "HTTPS"
+//   ssl_policy        = "ELBSecurityPolicy-2016-08"
+//   certificate_arn   = data.aws_acm_certificate.test.arn
 
-  default_action {
-    target_group_arn = data.aws_lb_target_group.api-test.id
-    type             = "forward"
-  }
-}
+//   default_action {
+//     target_group_arn = data.aws_lb_target_group.api-test.id
+//     type             = "forward"
+//   }
+// }
 
-resource "aws_lb_target_group" "content-negotiation-test" {
-  name     = "content-negotiation-test"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
-  target_type = "ip"
+// resource "aws_lb_target_group" "content-negotiation-test" {
+//   name     = "content-negotiation-test"
+//   port     = 80
+//   protocol = "HTTP"
+//   vpc_id   = var.vpc_id
+//   target_type = "ip"
 
-  health_check {
-    path = "/heartbeat"
-  }
-}
+//   health_check {
+//     path = "/heartbeat"
+//   }
+// }
 
-resource "aws_lb_listener" "http-test" {
-  load_balancer_arn = aws_lb.test.id
-  port              = "80"
-  protocol          = "HTTP"
+// resource "aws_lb_listener" "http-test" {
+//   load_balancer_arn = aws_lb.test.id
+//   port              = "80"
+//   protocol          = "HTTP"
 
-  default_action {
-    type = "redirect"
+//   default_action {
+//     type = "redirect"
 
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
+//     redirect {
+//       port        = "443"
+//       protocol    = "HTTPS"
+//       status_code = "HTTP_301"
+//     }
+//   }
+// }
 
 /*
 
