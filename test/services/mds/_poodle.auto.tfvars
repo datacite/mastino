5a37ea134aa3c376b7b2b3fa55f4c1ea480b960d
Martin Fenner <martin.fenner@datacite.org>
2020-05-20 05:45:37 +0200

    test services mds

---
+++ test/services/mds/_poodle.auto.tfvars
@@ -0,0 +1,4 @@
+poodle_tags = {
+  sha = "8d5df90"
+  version = "1.1.19"
+}
---
+++ test/services/mds/var.tf
@@ -0,0 +1,35 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+  default = "eu-west-1"
+}
+
+variable "vpc_id" {}
+
+variable "poodle_tags" {
+  type = "map"
+}
+
+variable "lb_name" {
+  default = "lb-test"
+}
+
+variable "security_group_id" {}
+
+variable "ttl" {
+  default = "300"
+}
+
+variable "subnet_datacite-private_id" {}
+variable "subnet_datacite-alt_id" {}
+
+variable "memcache_servers" {
+  default = "memcached.test.datacite.org:11211"
+}
+
+variable "sentry_dsn" {}
+variable "compose_id" {}
+
+variable "api_url" {
+  default = "http://client-api.test.local"
+}
