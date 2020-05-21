09f17a90ddb7637ba93afd67b8322daf7f80f51c
Martin Fenner <martin.fenner@datacite.org>
2020-05-21 20:43:55 +0200

    terraform 0.12

--- test/services/mds/input.tf
+++ test/services/mds/input.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
 }
 
 data "aws_route53_zone" "production" {
@@ -14,15 +14,15 @@ data "aws_route53_zone" "internal" {
 }
 
 data "aws_security_group" "datacite-private" {
-  id = "${var.security_group_id}"
+  id = var.security_group_id
 }
 
 data "aws_subnet" "datacite-private" {
-  id = "${var.subnet_datacite-private_id}"
+  id = var.subnet_datacite-private_id
 }
 
 data "aws_subnet" "datacite-alt" {
-  id = "${var.subnet_datacite-alt_id}"
+  id = var.subnet_datacite-alt_id
 }
 
 data "aws_ecs_cluster" "test" {
@@ -38,21 +38,10 @@ data "aws_iam_role" "ecs_task_execution_role" {
 }
 
 data "aws_lb" "test" {
-  name = "${var.lb_name}"
+  name = var.lb_name
 }
 
 data "aws_lb_listener" "test" {
-  load_balancer_arn = "${data.aws_lb.test.arn}"
+  load_balancer_arn = data.aws_lb.test.arn
   port = 443
-}
-
-data "template_file" "mds_task" {
-  template = "${file("mds.json")}"
-
-  vars {
-    sentry_dsn         = "${var.sentry_dsn}"
-    api_url            = "${var.api_url}"
-    memcache_servers   = "${var.memcache_servers}"
-    version            = "${var.poodle_tags["sha"]}"
-  }
 }
--- test/services/mds/main.tf
+++ test/services/mds/main.tf
@@ -1,20 +1,20 @@
 resource "aws_ecs_service" "mds-test" {
   name = "mds-test"
-  cluster = "${data.aws_ecs_cluster.test.id}"
+  cluster = data.aws_ecs_cluster.test.id
   launch_type = "FARGATE"
-  task_definition = "${aws_ecs_task_definition.mds-test.arn}"
+  task_definition = aws_ecs_task_definition.mds-test.arn
   desired_count = 1
 
   network_configuration {
-    security_groups = ["${data.aws_security_group.datacite-private.id}"]
+    security_groups = [data.aws_security_group.datacite-private.id]
     subnets         = [
-      "${data.aws_subnet.datacite-private.id}",
-      "${data.aws_subnet.datacite-alt.id}"
+      data.aws_subnet.datacite-private.id,
+      data.aws_subnet.datacite-alt.id
     ]
   }
 
   load_balancer {
-    target_group_arn = "${aws_lb_target_group.mds-test.id}"
+    target_group_arn = aws_lb_target_group.mds-test.id
     container_name   = "mds-test"
     container_port   = "80"
   }
@@ -30,36 +30,41 @@ resource "aws_cloudwatch_log_group" "mds-test" {
 
 resource "aws_ecs_task_definition" "mds-test" {
   family = "mds-test"
-  execution_role_arn = "${data.aws_iam_role.ecs_task_execution_role.arn}"
+  execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
   network_mode = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu = "256"
   memory = "512"
-
-  container_definitions =  "${data.template_file.mds_task.rendered}"
+  container_definitions = templatefile("mds.json",
+    {
+      sentry_dsn         = var.sentry_dsn
+      api_url            = var.api_url
+      memcache_servers   = var.memcache_servers
+      version            = var.poodle_tags["sha"]
+    })
 }
 
 resource "aws_route53_record" "mds-test" {
-   zone_id = "${data.aws_route53_zone.production.zone_id}"
+   zone_id = data.aws_route53_zone.production.zone_id
    name = "mds.test.datacite.org"
    type = "CNAME"
-   ttl = "${var.ttl}"
-   records = ["${data.aws_lb.test.dns_name}"]
+   ttl = var.ttl
+   records = [data.aws_lb.test.dns_name]
 }
 
 resource "aws_route53_record" "split-mds-test" {
-   zone_id = "${data.aws_route53_zone.internal.zone_id}"
+   zone_id = data.aws_route53_zone.internal.zone_id
    name = "mds.test.datacite.org"
    type = "CNAME"
-   ttl = "${var.ttl}"
-   records = ["${data.aws_lb.test.dns_name}"]
+   ttl = var.ttl
+   records = [data.aws_lb.test.dns_name]
 }
 
 resource "aws_lb_target_group" "mds-test" {
   name     = "mds-test"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = "${var.vpc_id}"
+  vpc_id   = var.vpc_id
   target_type = "ip"
 
   health_check {
@@ -68,32 +73,32 @@ resource "aws_lb_target_group" "mds-test" {
 }
 
 resource "aws_lb_listener_rule" "mds-test" {
-  listener_arn = "${data.aws_lb_listener.test.arn}"
+  listener_arn = data.aws_lb_listener.test.arn
   priority     = 2
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.mds-test.arn}"
+    target_group_arn = aws_lb_target_group.mds-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.mds-test.name}"]
+    values = [aws_route53_record.mds-test.name]
   }
 }
 
 // resource "aws_lb_listener_rule" "mds-test-doi" {
-//   listener_arn = "${data.aws_lb_listener.test.arn}"
+//   listener_arn = data.aws_lb_listener.test.arn
 //   priority     = 2
 
 //   action {
 //     type             = "forward"
-//     target_group_arn = "${aws_lb_target_group.mds-test.arn}"
+//     target_group_arn = aws_lb_target_group.mds-test.arn
 //   }
 
 //   condition {
 //     field  = "host-header"
-//     values = ["${aws_route53_record.mds-test.name}"]
+//     values = [aws_route53_record.mds-test.name]
 //   }
   
 //   condition {
@@ -103,17 +108,17 @@ resource "aws_lb_listener_rule" "mds-test" {
 // }
 
 // resource "aws_lb_listener_rule" "mds-test-metadata" {
-//   listener_arn = "${data.aws_lb_listener.test.arn}"
+//   listener_arn = data.aws_lb_listener.test.arn
 //   priority     = 5
 
 //   action {
 //     type             = "forward"
-//     target_group_arn = "${aws_lb_target_group.mds-test.arn}"
+//     target_group_arn = aws_lb_target_group.mds-test.arn
 //   }
 
 //   condition {
 //     field  = "host-header"
-//     values = ["${aws_route53_record.mds-test.name}"]
+//     values = [aws_route53_record.mds-test.name]
 //   }
 
 //   condition {
@@ -123,17 +128,17 @@ resource "aws_lb_listener_rule" "mds-test" {
 // }
 
 // resource "aws_lb_listener_rule" "mds-test-media" {
-//   listener_arn = "${data.aws_lb_listener.test.arn}"
+//   listener_arn = data.aws_lb_listener.test.arn
 //   priority     = 6
 
 //   action {
 //     type             = "forward"
-//     target_group_arn = "${aws_lb_target_group.mds-test.arn}"
+//     target_group_arn = aws_lb_target_group.mds-test.arn
 //   }
 
 //   condition {
 //     field  = "host-header"
-//     values = ["${aws_route53_record.mds-test.name}"]
+//     values = [aws_route53_record.mds-test.name]
 //   }
 
 //   condition {
@@ -143,17 +148,17 @@ resource "aws_lb_listener_rule" "mds-test" {
 // }
 
 // resource "aws_lb_listener_rule" "mds-test-heartbeat" {
-//   listener_arn = "${data.aws_lb_listener.test.arn}"
+//   listener_arn = data.aws_lb_listener.test.arn
 //   priority     = 7
 
 //   action {
 //     type             = "forward"
-//     target_group_arn = "${aws_lb_target_group.mds-test.arn}"
+//     target_group_arn = aws_lb_target_group.mds-test.arn
 //   }
 
 //   condition {
 //     field  = "host-header"
-//     values = ["${aws_route53_record.mds-test.name}"]
+//     values = [aws_route53_record.mds-test.name]
 //   }
 
 //   condition {
