5b093aaf705efc04f84e152b75eea6da4ba4fd6e
Martin Fenner <martin.fenner@datacite.org>
2020-05-21 22:16:09 +0200

    terraform 0.12

--- test/services/http-redirect/input.tf
+++ test/services/http-redirect/input.tf
@@ -1,19 +1,19 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
 }
 
 data "aws_security_group" "datacite-private" {
-  id = "${var.security_group_id}"
+  id = var.security_group_id
 }
 
 data "aws_subnet" "datacite-private" {
-  id = "${var.subnet_datacite-private_id}"
+  id = var.subnet_datacite-private_id
 }
 
 data "aws_subnet" "datacite-alt" {
-  id = "${var.subnet_datacite-alt_id}"
+  id = var.subnet_datacite-alt_id
 }
 
 data "aws_ecs_cluster" "test" {
@@ -29,13 +29,5 @@ data "aws_iam_role" "ecs_task_execution_role" {
 }
 
 data "aws_lb" "test" {
-  name = "${var.lb_name}"
-}
-
-data "template_file" "http-redirect_task" {
-  template = "${file("http-redirect.json")}"
-
-  vars {
-    version            = "${var.http-redirect_tags["version"]}"
-  }
+  name = var.lb_name
 }
