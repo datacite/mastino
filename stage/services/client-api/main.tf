0b890c0ac9210e869c731a19c799ae489972d07c
Martin Fenner <martin.fenner@datacite.org>
2020-05-22 18:53:16 +0200

    setup service registry

--- stage/services/client-api/main.tf
+++ stage/services/client-api/main.tf
@@ -19,9 +19,9 @@ resource "aws_ecs_service" "client-api-stage" {
     container_port   = "80"
   }
 
-  // service_registries {
-  //   registry_arn = aws_service_discovery_service.client-api-stage.arn
-  // }
+  service_registries {
+    registry_arn = aws_service_discovery_service.client-api-stage.arn
+  }
 
   depends_on = [
     "data.aws_lb_listener.stage"
