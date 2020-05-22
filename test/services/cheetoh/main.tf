34cbf54a8c72a87553b2b2da993c0c543f6f14c2
Martin Fenner <martin.fenner@datacite.org>
2020-05-22 07:22:05 +0200

    rename dns

--- test/services/cheetoh/main.tf
+++ test/services/cheetoh/main.tf
@@ -76,7 +76,7 @@ resource "aws_lb_listener_rule" "cheetoh-test" {
 
 resource "aws_route53_record" "cheetoh-test" {
     zone_id = data.aws_route53_zone.production.zone_id
-    name = "ez1.test.datacite.org"
+    name = "ez.test.datacite.org"
     type = "CNAME"
     ttl = var.ttl
     records = [data.aws_lb.test.dns_name]
@@ -84,7 +84,7 @@ resource "aws_route53_record" "cheetoh-test" {
 
 resource "aws_route53_record" "split-cheetoh-test" {
     zone_id = data.aws_route53_zone.internal.zone_id
-    name = "ez1.test.datacite.org"
+    name = "ez.test.datacite.org"
     type = "CNAME"
     ttl = var.ttl
     records = [data.aws_lb.test.dns_name]
