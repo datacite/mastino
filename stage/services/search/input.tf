aeaf9843be346d8d30ebad8cfd73f9a6901bf3c0
Martin Fenner <martin.fenner@datacite.org>
2020-05-22 12:30:44 +0200

    terraform 0.12

--- stage/services/search/input.tf
+++ stage/services/search/input.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
   version    = "~> 1.6"
 }
 
@@ -15,15 +15,15 @@ data "aws_route53_zone" "internal" {
 }
 
 data "aws_security_group" "datacite-private" {
-  id = "${var.security_group_id}"
+  id = var.security_group_id
 }
 
 data "aws_subnet" "datacite-private" {
-  id = "${var.subnet_datacite-private_id}"
+  id = var.subnet_datacite-private_id
 }
 
 data "aws_subnet" "datacite-alt" {
-  id = "${var.subnet_datacite-alt_id}"
+  id = var.subnet_datacite-alt_id
 }
 
 data "aws_ecs_cluster" "stage" {
@@ -39,11 +39,11 @@ data "aws_iam_role" "ecs_task_execution_role" {
 }
 
 data "aws_lb" "stage" {
-  name = "${var.lb_name}"
+  name = var.lb_name
 }
 
 data "aws_lb_listener" "stage" {
-  load_balancer_arn = "${data.aws_lb.stage.arn}"
+  load_balancer_arn = data.aws_lb.stage.arn
   port = 443
 }
 
@@ -52,7 +52,7 @@ data "aws_lb" "test" {
 }
 
 data "aws_lb_listener" "test" {
-  load_balancer_arn = "${data.aws_lb.test.arn}"
+  load_balancer_arn = data.aws_lb.test.arn
   port = 443
 }
 
@@ -61,32 +61,6 @@ data "aws_lb_target_group" "solr-stage" {
 }
 
 data "aws_vpc_endpoint" "datacite" {
-  vpc_id       = "${var.vpc_id}"
+  vpc_id       = var.vpc_id
   service_name = "com.amazonaws.eu-west-1.s3"
-}
-
-data "template_file" "search_stage_task" {
-  template = "${file("search.json")}"
-
-  vars {
-    jwt_public_key     = "${var.jwt_public_key}"
-    orcid_update_uuid  = "${var.orcid_update_uuid}"
-    orcid_update_url   = "${var.orcid_update_url}"
-    orcid_update_token = "${var.orcid_update_token}"
-    orcid_url          = "${var.orcid_url}"
-    volpino_url        = "${var.volpino_url}"
-    jwt_host           = "${var.jwt_host}"
-    api_url            = "${var.api_url}"
-    fabrica_url        = "${var.fabrica_url}"
-    data_url           = "${var.data_url}"
-    cdn_url            = "${var.cdn_url}"
-    sitemaps_url       = "${var.sitemaps_url}"
-    sitemaps_bucket_url = "${var.sitemaps_bucket_url}"
-    secret_key_base    = "${var.secret_key_base}"
-    memcache_servers   = "${var.memcache_servers}"
-    sentry_dsn         = "${var.sentry_dsn}"
-    gabba_cookie       = "${var.gabba_cookie}"
-    gabba_url          = "${var.gabba_url}"
-    version            = "${var.doi-metadata-search_tags["sha"]}"
-  }
 }
--- stage/services/search/main.tf
+++ stage/services/search/main.tf
@@ -9,34 +9,34 @@ resource "aws_s3_bucket" "search-stage" {
     website {
         index_document = "index.html"
     }
-    tags {
+    tags = {
         Name = "Search Stage"
     }
 }
 
 resource "aws_ecs_service" "search-stage" {
   name = "search-stage"
-  cluster = "${data.aws_ecs_cluster.stage.id}"
+  cluster = data.aws_ecs_cluster.stage.id
   launch_type = "FARGATE"
-  task_definition = "${aws_ecs_task_definition.search-stage.arn}"
+  task_definition = aws_ecs_task_definition.search-stage.arn
   desired_count = 1
 
   network_configuration {
-    security_groups = ["${data.aws_security_group.datacite-private.id}"]
+    security_groups = [data.aws_security_group.datacite-private.id]
     subnets         = [
-      "${data.aws_subnet.datacite-private.id}",
-      "${data.aws_subnet.datacite-alt.id}"
+      data.aws_subnet.datacite-private.id,
+      data.aws_subnet.datacite-alt.id
     ]
   }
 
   load_balancer {
-    target_group_arn = "${aws_lb_target_group.search-stage.id}"
+    target_group_arn = aws_lb_target_group.search-stage.id
     container_name   = "search-stage"
     container_port   = "80"
   }
 
   depends_on = [
-    "data.aws_lb_listener.stage",
+    "data.aws_lb_listener.stage"
   ]
 }
 
@@ -44,7 +44,7 @@ resource "aws_lb_target_group" "search-stage" {
   name     = "search-stage"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = "${var.vpc_id}"
+  vpc_id   = var.vpc_id
   target_type = "ip"
 
   stickiness {
@@ -56,7 +56,7 @@ resource "aws_lb_target_group" "search-test" {
   name     = "search-test"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = "${var.vpc_id}"
+  vpc_id   = var.vpc_id
   target_type = "ip"
 
   stickiness {
@@ -70,73 +70,93 @@ resource "aws_cloudwatch_log_group" "search-stage" {
 
 resource "aws_ecs_task_definition" "search-stage" {
   family = "search-stage"
-  execution_role_arn = "${data.aws_iam_role.ecs_task_execution_role.arn}",
+  execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn,
   network_mode = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu = "512"
   memory = "2048"
-
-  container_definitions =  "${data.template_file.search_stage_task.rendered}"
+  container_definitions = templatefile("search.json",
+    {
+      jwt_public_key     = var.jwt_public_key
+      orcid_update_uuid  = var.orcid_update_uuid
+      orcid_update_url   = var.orcid_update_url
+      orcid_update_token = var.orcid_update_token
+      orcid_url          = var.orcid_url
+      volpino_url        = var.volpino_url
+      jwt_host           = var.jwt_host
+      api_url            = var.api_url
+      fabrica_url        = var.fabrica_url
+      data_url           = var.data_url
+      cdn_url            = var.cdn_url
+      sitemaps_url       = var.sitemaps_url
+      sitemaps_bucket_url = var.sitemaps_bucket_url
+      secret_key_base    = var.secret_key_base
+      memcache_servers   = var.memcache_servers
+      sentry_dsn         = var.sentry_dsn
+      gabba_cookie       = var.gabba_cookie
+      gabba_url          = var.gabba_url
+      version            = var.doi-metadata-search_tags["sha"]
+    }}
 }
 
 resource "aws_lb_listener_rule" "search-stage" {
-  listener_arn = "${data.aws_lb_listener.stage.arn}"
+  listener_arn = data.aws_lb_listener.stage.arn
   priority     = 89
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.search-stage.arn}"
+    target_group_arn = aws_lb_target_group.search-stage.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.search-stage.name}"]
+    values = [aws_route53_record.search-stage.name]
   }
 }
 
 resource "aws_lb_listener_rule" "search-test" {
-  listener_arn = "${data.aws_lb_listener.test.arn}"
+  listener_arn = data.aws_lb_listener.test.arn
   priority     = 89
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.search-test.arn}"
+    target_group_arn = aws_lb_target_group.search-test.arn
   }
 
   condition {
     field  = "host-header"
-    values = ["${aws_route53_record.search-test.name}"]
+    values = [aws_route53_record.search-test.name]
   }
 }
 
 resource "aws_route53_record" "search-stage" {
-   zone_id = "${data.aws_route53_zone.production.zone_id}"
+   zone_id = data.aws_route53_zone.production.zone_id
    name = "search.stage.datacite.org"
    type = "CNAME"
-   ttl = "${var.ttl}"
-   records = ["${data.aws_lb.stage.dns_name}"]
+   ttl = var.ttl
+   records = [data.aws_lb.stage.dns_name]
 }
 
 resource "aws_route53_record" "split-search-stage" {
-   zone_id = "${data.aws_route53_zone.internal.zone_id}"
+   zone_id = data.aws_route53_zone.internal.zone_id
    name = "search.stage.datacite.org"
    type = "CNAME"
-   ttl = "${var.ttl}"
-   records = ["${data.aws_lb.stage.dns_name}"]
+   ttl = var.ttl
+   records = [data.aws_lb.stage.dns_name]
 }
 
 resource "aws_route53_record" "search-test" {
-   zone_id = "${data.aws_route53_zone.production.zone_id}"
+   zone_id = data.aws_route53_zone.production.zone_id
    name = "search.test.datacite.org"
    type = "CNAME"
-   ttl = "${var.ttl}"
-   records = ["${data.aws_lb.test.dns_name}"]
+   ttl = var.ttl
+   records = [data.aws_lb.test.dns_name]
 }
 
 resource "aws_route53_record" "split-search-test" {
-   zone_id = "${data.aws_route53_zone.internal.zone_id}"
+   zone_id = data.aws_route53_zone.internal.zone_id
    name = "search.test.datacite.org"
    type = "CNAME"
-   ttl = "${var.ttl}"
-   records = ["${data.aws_lb.test.dns_name}"]
+   ttl = var.ttl
+   records = [data.aws_lb.test.dns_name]
 }
