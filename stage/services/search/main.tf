764fb06b4cab676270cea73ef66e468ac70a647f
Martin Fenner <martin.fenner@datacite.org>
2020-05-22 08:13:17 +0200

    search for test

--- stage/services/search/main.tf
+++ stage/services/search/main.tf
@@ -48,6 +48,18 @@ resource "aws_lb_target_group" "search-stage" {
   }
 }
 
+resource "aws_lb_target_group" "search-test" {
+  name     = "search-test"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "${var.vpc_id}"
+  target_type = "ip"
+
+  stickiness {
+    type   = "lb_cookie"
+  }
+}
+
 resource "aws_cloudwatch_log_group" "search-stage" {
   name = "/ecs/search-stage"
 }
@@ -78,9 +90,24 @@ resource "aws_lb_listener_rule" "search-stage" {
   }
 }
 
+resource "aws_lb_listener_rule" "search-test" {
+  listener_arn = "${data.aws_lb_listener.test.arn}"
+  priority     = 89
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.search-test.arn}"
+  }
+
+  condition {
+    field  = "host-header"
+    values = ["${aws_route53_record.search-test.name}"]
+  }
+}
+
 resource "aws_route53_record" "search-stage" {
    zone_id = "${data.aws_route53_zone.production.zone_id}"
-   name = "search.test.datacite.org"
+   name = "search.stage.datacite.org"
    type = "CNAME"
    ttl = "${var.ttl}"
    records = ["${data.aws_lb.stage.dns_name}"]
@@ -88,8 +115,24 @@ resource "aws_route53_record" "search-stage" {
 
 resource "aws_route53_record" "split-search-stage" {
    zone_id = "${data.aws_route53_zone.internal.zone_id}"
-   name = "search.test.datacite.org"
+   name = "search.stage.datacite.org"
    type = "CNAME"
    ttl = "${var.ttl}"
    records = ["${data.aws_lb.stage.dns_name}"]
+}
+
+resource "aws_route53_record" "search-test" {
+   zone_id = "${data.aws_route53_zone.production.zone_id}"
+   name = "search.test.datacite.org"
+   type = "CNAME"
+   ttl = "${var.ttl}"
+   records = ["${data.aws_lb.test.dns_name}"]
+}
+
+resource "aws_route53_record" "split-search-test" {
+   zone_id = "${data.aws_route53_zone.internal.zone_id}"
+   name = "search.test.datacite.org"
+   type = "CNAME"
+   ttl = "${var.ttl}"
+   records = ["${data.aws_lb.test.dns_name}"]
 }
