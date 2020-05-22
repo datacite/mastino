9c420ea4516ffb70ece55791753aaeab866ca087
Martin Fenner <martin.fenner@datacite.org>
2020-05-22 12:49:58 +0200

    change memcached dns

--- stage/data-storage/memcached/main.tf
+++ stage/data-storage/memcached/main.tf
@@ -20,9 +20,9 @@ resource "aws_elasticache_subnet_group" "memcached-stage" {
                   "${data.aws_subnet.datacite-alt.id}"]
 }
 
-resource "aws_route53_record" "memcached-test" {
+resource "aws_route53_record" "memcached-stage" {
     zone_id = "${data.aws_route53_zone.internal.zone_id}"
-    name = "memcached.test.datacite.org"
+    name = "memcached.stage.datacite.org"
     type = "CNAME"
     ttl = "${var.ttl}"
     records = ["${aws_elasticache_cluster.memcached-stage.cache_nodes.0.address}"]
