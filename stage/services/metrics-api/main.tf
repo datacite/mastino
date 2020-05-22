c5e96f7851fece236fe4ba24a43379d11c0871b0
Martin Fenner <martin.fenner@datacite.org>
2020-05-22 10:37:10 +0200

    fix service discovery

--- stage/services/metrics-api/main.tf
+++ stage/services/metrics-api/main.tf
@@ -21,12 +21,12 @@ resource "aws_ecs_service" "metrics-api-stage" {
     container_port   = "80"
   }
 
-  service_registries {
-    registry_arn = "${aws_service_discovery_service.metrics-api-stage.arn}"
-  }
+  // service_registries {
+  //   registry_arn = "${aws_service_discovery_service.metrics-api-stage.arn}"
+  // }
 
    depends_on = [
-    "data.aws_lb_listener.stage",
+     "data.aws_lb_listener.stage"
   ]
 }
 
