ca02dfc82cf8fe39d54e952997ded428314c9b52
Martin Fenner <martin.fenner@datacite.org>
2020-05-22 09:07:36 +0200

    use stage subdomain

--- stage/services/akita/main.tf
+++ stage/services/akita/main.tf
@@ -81,7 +81,7 @@ resource "aws_lb_listener_rule" "akita-stage" {
 
 resource "aws_route53_record" "akita-stage" {
     zone_id = data.aws_route53_zone.production.zone_id
-    name = "commons.test.datacite.org"
+    name = "commons.stage.datacite.org"
     type = "CNAME"
     ttl = var.ttl
     records = [data.aws_lb.stage.dns_name]
@@ -89,14 +89,14 @@ resource "aws_route53_record" "akita-stage" {
 
 resource "aws_route53_record" "split-akita-stage" {
     zone_id = data.aws_route53_zone.internal.zone_id
-    name = "commons.test.datacite.org"
+    name = "commons.stage.datacite.org"
     type = "CNAME"
     ttl = var.ttl
     records = [data.aws_lb.stage.dns_name]
 }
 
 resource "aws_service_discovery_service" "akita-stage" {
-  name = "akita.test"
+  name = "akita.stage"
 
   health_check_custom_config {
     failure_threshold = 3
