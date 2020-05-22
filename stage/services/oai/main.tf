c89393a1bdf2360fd5c3d0670499adb89ae92344
Martin Fenner <martin.fenner@datacite.org>
2020-05-22 08:15:37 +0200

    oai for test

--- stage/services/oai/main.tf
+++ stage/services/oai/main.tf
@@ -67,12 +67,48 @@ resource "aws_lb_listener_rule" "oai-stage" {
   }
 }
 
+resource "aws_lb_target_group" "oai-test" {
+  name     = "oai-test"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "${var.vpc_id}"
+  target_type = "ip"
+  slow_start = 240
+
+  health_check {
+    path = "/heartbeat"
+  }
+}
+
+resource "aws_lb_listener_rule" "oai-test" {
+  listener_arn = "${data.aws_lb_listener.test.arn}"
+  priority     = 61
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.oai-test.arn}"
+  }
+
+  condition {
+    field  = "host-header"
+    values = ["${aws_route53_record.oai-test.name}"]
+  }
+}
+
 resource "aws_route53_record" "oai-stage" {
+    zone_id = "${data.aws_route53_zone.production.zone_id}"
+    name = "oai.stage.datacite.org"
+    type = "CNAME"
+    ttl = "${var.ttl}"
+    records = ["${data.aws_lb.stage.dns_name}"]
+}
+
+resource "aws_route53_record" "oai-test" {
     zone_id = "${data.aws_route53_zone.production.zone_id}"
     name = "oai.test.datacite.org"
     type = "CNAME"
     ttl = "${var.ttl}"
-    records = ["${data.aws_lb.stage.dns_name}"]
+    records = ["${data.aws_lb.task_definition.dns_name}"]
 }
 
 resource "aws_service_discovery_service" "oai-stage" {
