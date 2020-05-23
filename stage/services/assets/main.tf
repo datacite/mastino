9e1bf67d311f9c0b10202d8e03107e7b4df01e40
Martin Fenner <martin.fenner@datacite.org>
2020-05-23 08:22:01 +0200

    fix variable

--- stage/services/assets/main.tf
+++ stage/services/assets/main.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "assets-stage" {
     bucket = "assets.stage.datacite.org"
     acl = "public-read"
-    policy = "${data.template_file.assets-test.rendered}"
+    policy = "${data.template_file.assets-stage.rendered}"
     website {
         index_document = "index.html"
         error_document = "404.html"
