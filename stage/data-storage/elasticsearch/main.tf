8d18b1549c8fde0131792c23e69f4953150ba524
Sarala Wimalaratne <sarala.dissanayake@gmail.com>
2020-06-09 18:48:20 +0100

    user pool moved to main for testing

--- stage/data-storage/elasticsearch/main.tf
+++ stage/data-storage/elasticsearch/main.tf
@@ -93,3 +93,18 @@ resource "aws_route53_record" "elasticsearch-test" {
    ttl = "${var.ttl}"
    records = ["${aws_elasticsearch_domain.test.endpoint}"]
 }
+
+resource "aws_cognito_user_pool" "user_pool" {
+  name = "kibana-userpool"
+}
+
+resource "aws_cognito_identity_pool" "identity_pool" {
+  identity_pool_name               = "kibana-identity-pool"
+  allow_unauthenticated_identities = false
+
+  cognito_identity_providers {
+    client_id               = "${aws_cognito_user_pool_client.kibana_client.id}"
+    provider_name           = "cognito-idp.eu-west-1.amazonaws.com/${aws_cognito_user_pool.user_pool.id}"
+    server_side_token_check = false
+  }
+}
