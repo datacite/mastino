fde02486e28aef2e19329d7e3d81b3421bcadb69
Sarala Wimalaratne <sarala.dissanayake@gmail.com>
2020-06-10 15:16:35 +0100

    enable client id

--- stage/vpc/cognito.tf
+++ stage/vpc/cognito.tf
@@ -7,7 +7,7 @@ resource "aws_cognito_identity_pool" "identity_pool" {
   allow_unauthenticated_identities = false
 
   cognito_identity_providers {
-    // client_id               = "${aws_cognito_user_pool_client.kibana_client.id}"
+    client_id               = "${aws_cognito_user_pool_client.kibana_client.id}"
     provider_name           = "${aws_cognito_user_pool.user_pool.id}"
     server_side_token_check = false
   }
