d7b7ef8284b380cc57d54f09b9f407c9ee61b6de
Martin Fenner <martin.fenner@datacite.org>
2020-06-11 21:35:23 +0200

    remove obsolete resources

--- stage/data-storage/efs/main.tf
+++ stage/data-storage/efs/main.tf
@@ -5,18 +5,3 @@ resource "aws_efs_file_system" "stage" {
     Name = "EFS-Stage"
   }
 }
-
-resource "aws_elasticache_subnet_group" "memcached-stage" {
-    name = "memcached-stage"
-    description = "Elasticache memcached-stage subnet group"
-    subnet_ids = [data.aws_subnet.datacite-private.id,
-                  data.aws_subnet.datacite-alt.id]
-}
-
-resource "aws_route53_record" "memcached-stage" {
-    zone_id = data.aws_route53_zone.internal.zone_id
-    name = "memcached.stage.datacite.org"
-    type = "CNAME"
-    ttl = var.ttl
-    records = [aws_elasticache_cluster.memcached-stage.cache_nodes.0.address]
-}
