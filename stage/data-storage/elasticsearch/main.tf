c1f7ad26ac6cd29d6bb7a45199a574c2a292f0b9
Sarala Wimalaratne <sarala.dissanayake@gmail.com>
2020-06-11 07:09:24 +0100

    testing cognito

--- stage/data-storage/elasticsearch/main.tf
+++ stage/data-storage/elasticsearch/main.tf
@@ -48,9 +48,24 @@ resource "aws_elasticsearch_domain" "test" {
 
 resource "aws_cognito_user_pool_client" "kibana_client" {
   name          = "kibana-client"
-  user_pool_id  = "${aws_cognito_user_pool.identity_pool.id}"
+  user_pool_id  = "${aws_cognito_user_pool.user_pool.id}"
   callback_urls = "${aws_elasticsearch_domain.test.kibana_endpoint}"
   logout_urls   = "${aws_elasticsearch_domain.test.kibana_endpoint}"
+}
+
+resource "aws_cognito_user_pool" "user_pool" {
+  name = "kibana-userpool"
+}
+
+resource "aws_cognito_identity_pool" "identity_pool" {
+  identity_pool_name               = "kibana identity pool"
+  allow_unauthenticated_identities = false
+
+  cognito_identity_providers {
+    client_id               = "${aws_cognito_user_pool_client.kibana_client.id}"
+    provider_name           = "${aws_cognito_user_pool.user_pool.id}"
+    server_side_token_check = false
+  }
 }
 
 resource "aws_elasticsearch_domain_policy" "test" {
