88ddd480e0ce8cd9afbf05f1854bbb160db49a3b
Martin Fenner <martin.fenner@datacite.org>
2020-06-12 08:24:46 +0200

    only one efs subnet

--- stage/data-storage/efs/main.tf
+++ stage/data-storage/efs/main.tf
@@ -28,7 +28,7 @@ resource "aws_efs_access_point" "strapi-stage" {
 
 resource "aws_efs_mount_target" "stage" {
   file_system_id  = aws_efs_file_system.stage.id
-  subnet_id       = [data.aws_subnet.datacite-private.id, data.aws_subnet.datacite-alt.id]
+  subnet_id       = data.aws_subnet.datacite-private.id
   security_groups = [data.aws_security_group.datacite-private.id]
 }
 
