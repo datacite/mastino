9119402cb95c2bbc3bde966dec2fb1cf40a956e1
Martin Fenner <martin.fenner@datacite.org>
2020-06-12 09:13:06 +0200

    use only one subnet in strapi stage

--- stage/services/strapi/main.tf
+++ stage/services/strapi/main.tf
@@ -12,8 +12,7 @@ resource "aws_ecs_service" "strapi-stage" {
   network_configuration {
     security_groups = [data.aws_security_group.datacite-private.id]
     subnets         = [
-      data.aws_subnet.datacite-private.id,
-      data.aws_subnet.datacite-alt.id
+      data.aws_subnet.datacite-private.id
     ]
   }
 
