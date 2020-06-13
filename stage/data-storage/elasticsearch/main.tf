3d2c452c37f5ccfef49316a2ffae81976b09c57d
Martin Fenner <martin.fenner@datacite.org>
2020-06-13 11:47:16 +0200

    create user_pool_client

--- stage/data-storage/elasticsearch/main.tf
+++ stage/data-storage/elasticsearch/main.tf
@@ -49,8 +49,8 @@ resource "aws_elasticsearch_domain" "test" {
 resource "aws_cognito_user_pool_client" "kibana_client" {
   name          = "kibana-client"
   user_pool_id  = "${data.aws_cognito_user_pools.user_pool.ids[0]}"
-  callback_urls = ["${aws_elasticsearch_domain.test.kibana_endpoint}"]
-  logout_urls   = ["${aws_elasticsearch_domain.test.kibana_endpoint}"]
+  // callback_urls = ["${aws_elasticsearch_domain.test.kibana_endpoint}"]
+  // logout_urls   = ["${aws_elasticsearch_domain.test.kibana_endpoint}"]
 }
 
 // resource "aws_cognito_identity_pool" "identity_pool" {
