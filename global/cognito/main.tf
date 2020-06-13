de1d003e7d2405a0a0a8d94913eaf912d21a455f
Martin Fenner <martin.fenner@datacite.org>
2020-06-13 12:10:33 +0200

    add google identity provider

--- global/cognito/main.tf
+++ global/cognito/main.tf
@@ -1,3 +1,20 @@
 resource "aws_cognito_user_pool" "user_pool" {
   name = "kibana-userpool"
 }
+
+resource "aws_cognito_identity_provider" "google-stage" {
+  user_pool_id  = aws_cognito_user_pool.user_pool.id
+  provider_name = "Google-Stage"
+  provider_type = "Google"
+
+  provider_details = {
+    authorize_scopes = "email"
+    client_id        = var.google_client_id
+    client_secret    = var.google_client_secret
+  }
+
+  attribute_mapping = {
+    email    = "email"
+    username = "sub"
+  }
+}
