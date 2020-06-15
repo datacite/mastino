80cc4e9e2f23d8227083a9bd3a9b510e86604d33
Richard Hallett <rph@sis>
2020-06-15 11:41:20 +0200

    Pidservices - Add Missing cloudfront zone id var

--- pidservices/prod/var.tf
+++ pidservices/prod/var.tf
@@ -3,3 +3,8 @@ variable "secret_key" {}
 variable "region" {
     default = "eu-west-1"
 }
+
+variable "cloudfront_alias_zone_id" {
+  description = "Fixed hardcoded constant zone_id that is used for all CloudFront distributions"
+  default     = "Z2FDTNDATAQYW2"
+}
