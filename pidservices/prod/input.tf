0f119aba82a94516e7eb1e89c268a2e8bb9bbf00
Richard Hallett <rph@sis>
2020-06-15 11:25:45 +0200

    Certificates exist in AWS-US-EAST

--- pidservices/prod/input.tf
+++ pidservices/prod/input.tf
@@ -4,8 +4,16 @@ provider "aws" {
   region     = var.region
 }
 
+provider "aws" {
+  # us-east-1 instance
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region = "us-east-1"
+  alias = "use1"
+}
+
 data "aws_acm_certificate" "cloudfront-pidservices" {
-  provider = aws
+  provider = aws.use1
   domain = "pidservices.org"
   statuses = ["ISSUED"]
 }
