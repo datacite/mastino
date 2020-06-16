4442b7bd0638958a70900f578204521b99b5544c
Martin Fenner <martin.fenner@datacite.org>
2020-06-16 18:31:03 +0200

    increase volume size es stage

--- stage/data-storage/elasticsearch/main.tf
+++ stage/data-storage/elasticsearch/main.tf
@@ -17,7 +17,7 @@ resource "aws_elasticsearch_domain" "test" {
   ebs_options{
       ebs_enabled = true
       volume_type = "gp2"
-      volume_size = 80
+      volume_size = 120
   }
 
   vpc_options {
