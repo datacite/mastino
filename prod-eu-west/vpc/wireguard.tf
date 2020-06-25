ebc72f6403f6acbd76ae6441a3f9dc67e58da1ff
Martin Fenner <martin.fenner@datacite.org>
2020-06-26 00:00:57 +0200

    terraform 0.12

--- prod-eu-west/vpc/wireguard.tf
+++ prod-eu-west/vpc/wireguard.tf
@@ -1,14 +1,14 @@
 module "wireguard" {
   source                        = "github.com/datacite/terraform-aws-wireguard"
-  ssh_key_id                    = ""
-  vpc_id                        = "${var.vpc_id}"
-  additional_security_group_ids = ["${aws_security_group.wireguard_ssh_check.id}"]
-  subnet_ids                    = ["${data.aws_subnet.datacite-public.id}", "${data.aws_subnet.datacite-public-alt.id}"]
-  target_group_arns             = ["${aws_lb_target_group.wireguard.arn}"]
+  ssh_key_id                    = "1"
+  vpc_id                        = var.vpc_id
+  additional_security_group_ids = [aws_security_group.wireguard_ssh_check.id]
+  subnet_ids                    = [data.aws_subnet.datacite-public.id, data.aws_subnet.datacite-public-alt.id]
+  target_group_arns             = [aws_lb_target_group.wireguard.arn]
   asg_min_size                  = 1
   asg_desired_capacity          = 2
   asg_max_size                  = 5
-  wg_server_net                 = ""
+  wg_server_net                 = "1"
   wg_client_public_keys = []
 }
 
@@ -16,18 +16,18 @@ resource "aws_lb" "wireguard" {
   name               = "wireguard"
   load_balancer_type = "network"
   internal           = false
-  subnets            = ["${data.aws_subnet.datacite-public.id}", "${data.aws_subnet.datacite-public-alt.id}"]
+  subnets            = [data.aws_subnet.datacite-public.id, data.aws_subnet.datacite-public-alt.id]
 }
 
 resource "aws_security_group" "wireguard_ssh_check" {
   name   = "wireguard_ssh_check"
-  vpc_id = "${var.vpc_id}"
+  vpc_id = var.vpc_id
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${data.aws_subnet.datacite-public.cidr_block}", "${data.aws_subnet.datacite-public-alt.cidr_block}"]
+    cidr_blocks = [data.aws_subnet.datacite-public.cidr_block, data.aws_subnet.datacite-public-alt.cidr_block]
   }
 }
 
@@ -35,7 +35,7 @@ resource "aws_lb_target_group" "wireguard" {
   name_prefix = "wg"
   port        = 51820
   protocol    = "UDP"
-  vpc_id      = "${var.vpc_id}"
+  vpc_id      = var.vpc_id
 
   health_check {
     port     = 22
@@ -45,12 +45,12 @@ resource "aws_lb_target_group" "wireguard" {
 }
 
 resource "aws_lb_listener" "wireguard" {
-  load_balancer_arn = "${aws_lb.wireguard.arn}"
+  load_balancer_arn = aws_lb.wireguard.arn
   port              = 51820
   protocol          = "UDP"
 
   default_action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.wireguard.arn}"
+    target_group_arn = aws_lb_target_group.wireguard.arn
   }
 }
