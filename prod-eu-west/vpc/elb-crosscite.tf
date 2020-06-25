06efafeb64b07d5e6a3c6a25598632dbb6e46c0c
Martin Fenner <martin.fenner@datacite.org>
2020-06-26 00:13:00 +0200

    terraform 0.12

--- prod-eu-west/vpc/elb-crosscite.tf
+++ prod-eu-west/vpc/elb-crosscite.tf
@@ -1,13 +1,13 @@
 resource "aws_lb" "crosscite" {
   name = "crosscite"
   internal = false
-  subnets = ["${data.aws_subnet.datacite-public.id}", "${data.aws_subnet.datacite-public-alt.id}"]
-  security_groups = ["${data.aws_security_group.datacite-public.id}"]
+  subnets = [data.aws_subnet.datacite-public.id, data.aws_subnet.datacite-public-alt.id]
+  security_groups = [data.aws_security_group.datacite-public.id]
 
   enable_deletion_protection = true
 
   access_logs {
-    bucket = "${data.aws_s3_bucket.logs.bucket}"
+    bucket = data.aws_s3_bucket.logs.bucket
     prefix = "crosscite"
     enabled = true
   }
@@ -19,11 +19,11 @@ resource "aws_lb" "crosscite" {
 }
 
 resource "aws_lb_listener" "crosscite" {
-  load_balancer_arn = "${aws_lb.crosscite.id}"
+  load_balancer_arn = aws_lb.crosscite.id
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = "${data.aws_acm_certificate.crosscite.arn}"
+  certificate_arn   = data.aws_acm_certificate.crosscite.arn
 
   // default_action {
   //   target_group_arn = "${data.aws_lb_target_group.content-negotation.id}"
@@ -42,7 +42,7 @@ resource "aws_lb_listener" "crosscite" {
 }
 
 resource "aws_lb_listener" "crosscite-http" {
-  load_balancer_arn = "${aws_lb.crosscite.id}"
+  load_balancer_arn = aws_lb.crosscite.id
   port              = "80"
   protocol          = "HTTP"
 
