17cef9221e8c5133a1101506d193a67c92cc13af
Martin Fenner <martin.fenner@datacite.org>
2020-06-26 00:08:15 +0200

    terraform 0.12

--- prod-eu-west/vpc/dns.tf
+++ prod-eu-west/vpc/dns.tf
@@ -1,5 +1,5 @@
 # Service Discovery Namepace
 resource "aws_service_discovery_private_dns_namespace" "internal" {
   name = "local"
-  vpc = "${data.aws_subnet.datacite-private.vpc_id}"
+  vpc = data.aws_subnet.datacite-private.vpc_id
 }
