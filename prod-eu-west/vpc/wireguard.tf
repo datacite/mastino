75db6e0fa0a25f023400d454e8f63221233954de
Martin Fenner <martin.fenner@datacite.org>
2020-06-25 22:46:54 +0200

    define source

--- prod-eu-west/vpc/wireguard.tf
+++ prod-eu-west/vpc/wireguard.tf
@@ -1,5 +1,6 @@
 module "wireguard" {
-  source                        = "git@github.com:jmhale/terraform-aws-wireguard.git"
+  source                        = "jmhale/wireguard/aws"
+  version                       = "1.0.0"
   ssh_key_id                    = ""
   vpc_id                        = "${var.vpc_id}"
   additional_security_group_ids = ["${aws_security_group.wireguard_ssh_check.id}"]
