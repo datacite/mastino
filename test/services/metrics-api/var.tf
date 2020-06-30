4a90c9645fcec31615c891e81eb9364e8344b98b
kjgarza <kgarza@datacite.org>
2020-06-30 17:11:18 +0200

    first commit

---
+++ test/services/metrics-api/var.tf
@@ -0,0 +1,50 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+  default = "eu-west-1"
+}
+variable "s3_bucket" {
+  default = "assets.test.datacite.org"
+}
+variable "vpc_id" {}
+
+variable "sashimi_tags" {
+  type = "map"
+}
+
+variable "lb_name" {
+  default = "lb-test"
+}
+
+variable "ttl" {
+  default = "300"
+}
+
+variable "memcache_servers" {
+  default = "memcached.test.datacite.org:11211"
+}
+
+variable "jwt_public_key" {}
+variable "jwt_private_key" {}
+variable "usage_url" {}
+
+variable "mysql_user" {}
+variable "mysql_password" {}
+variable "mysql_database" {
+  default = "metrics"
+}
+variable "mysql_host" {}
+variable "public_key" {}
+
+variable "security_group_id" {}
+variable "subnet_datacite-private_id" {}
+variable "subnet_datacite-alt_id" {}
+
+variable "namespace_id" {}
+
+variable "sentry_dsn" {}
+variable "rack_timeout_service_timeout" {
+  default = "40"
+}
+variable "jwt_blacklisted" {}
+
