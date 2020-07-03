fc397d349759b5666396d015d19a25ff9c955326
kjgarza <kgarza@datacite.org>
2020-07-03 13:46:51 +0200

    remove all config

--- test/services/metrics-api/main.tf
+++ test/services/metrics-api/main.tf
@@ -117,21 +117,6 @@ resource "aws_ecs_task_definition" "metrics-api-test" {
   container_definitions =  "${data.template_file.metrics-api_task.rendered}"
 }
 
-resource "aws_route53_record" "metrics-api-test" {
-  zone_id = "${data.aws_route53_zone.production.zone_id}"
-  name = "metrics.test.datacite.org"
-  type = "CNAME"
-  ttl = "${var.ttl}"
-  records = ["${data.aws_lb.test.dns_name}"]
-}
-
-resource "aws_route53_record" "split-metrics-api-test" {
-  zone_id = "${data.aws_route53_zone.internal.zone_id}"
-  name = "metrics.test.datacite.org"
-  type = "CNAME"
-  ttl = "${var.ttl}"
-  records = ["${data.aws_lb.test.dns_name}"]
-}
 
 resource "aws_service_discovery_service" "metrics-api-test" {
   name = "metrics-api.test"
