bd10476eb8a789c2458e06f952b31eb77759c8ab
Martin Fenner <martin.fenner@datacite.org>
2020-07-08 15:09:20 +0200

    rename route 53 for cms stage

--- stage/services/strapi/main.tf
+++ stage/services/strapi/main.tf
@@ -62,7 +62,7 @@ resource "aws_ecs_task_definition" "strapi-stage" {
 
 resource "aws_route53_record" "strapi-stage" {
    zone_id = data.aws_route53_zone.production.zone_id
-   name = "strapi.stage.datacite.org"
+   name = "cms.stage.datacite.org"
    type = "CNAME"
    ttl = var.ttl
    records = [data.aws_lb.stage.dns_name]
@@ -70,7 +70,7 @@ resource "aws_route53_record" "strapi-stage" {
 
 resource "aws_route53_record" "split-strapi-stage" {
    zone_id = data.aws_route53_zone.internal.zone_id
-   name = "strapi.stage.datacite.org"
+   name = "cms.stage.datacite.org"
    type = "CNAME"
    ttl = var.ttl
    records = [data.aws_lb.stage.dns_name]
@@ -104,7 +104,7 @@ resource "aws_lb_listener_rule" "strapi-stage" {
 }
 
 resource "aws_service_discovery_service" "strapi-stage" {
-  name = "strapi.stage"
+  name = "cms.stage"
 
   health_check_custom_config {
     failure_threshold = 3
