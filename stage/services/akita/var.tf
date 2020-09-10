0324a3bc77d6e4378b6504d946e3b27b5fb39cf9
Martin Fenner <martin.fenner@datacite.org>
2020-09-10 08:22:29 +0200

    remove obsolete variable

--- stage/services/akita/var.tf
+++ stage/services/akita/var.tf
@@ -34,5 +34,4 @@ variable "next_public_profiles_url" {
 variable "next_public_jwt_public_key" {}
 variable "next_public_ga_tracking_id" {}
 variable "sentry_dsn" {}
-variable "tracking_id" {}
 variable "namespace_id" {}
