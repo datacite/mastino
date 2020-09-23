a927d9e18dc64d209bfdbad50fc1acecd91aefbd
Kristian Garza <kj.garza@gmail.com>
2020-09-23 13:24:24 +0200

    S3 bucket for resolution logs

---
+++ prod-eu-west/services/raw-resolution-logs/input.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+provider "aws" {
+  # us-east-1 instance
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "us-east-1"
+  alias = "use1"
+}
+
