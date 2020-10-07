ae3d20807e537ad8954f031a052dc7629931d1bd
Martin Fenner <martin.fenner@datacite.org>
2020-10-07 07:22:24 +0200

    tweak api configuration

--- prod-eu-west/services/api/main.tf
+++ prod-eu-west/services/api/main.tf
@@ -134,17 +134,17 @@ resource "aws_ecs_task_definition" "api" {
   container_definitions =  "${data.template_file.api_task.rendered}"
 }
 
-// resource "aws_lb_target_group" "api" {
-//   name     = "api"
-//   port     = 80
-//   protocol = "HTTP"
-//   vpc_id   = "${var.vpc_id}"
-//   target_type = "ip"
+resource "aws_lb_target_group" "api" {
+  name     = "api"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "${var.vpc_id}"
+  target_type = "ip"
 
-//   health_check {
-//     path = "/heartbeat"
-//   }
-// }
+  health_check {
+    path = "/heartbeat"
+  }
+}
 
 // resource "aws_lb_listener_rule" "api-works" {
 //   listener_arn = "${data.aws_lb_listener.default.arn}"
