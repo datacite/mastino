79ad393914d8ecc6033d9e45833ab58cba1bde44
Martin Fenner <martin.fenner@datacite.org>
2020-10-14 13:13:16 +0200

    automatically use latest release of federation api

--- prod-eu-west/services/federation/input.tf
+++ prod-eu-west/services/federation/input.tf
@@ -55,6 +55,6 @@ data "template_file" "federation_task" {
     client_api_url     = "${var.client_api_url}"
     api_url            = "${var.api_url}"
     apollo_api_key     = "${var.apollo_api_key}"
-    version            = "1.0.1"
+    version            = "${var.vaestgoetaspets_tags["version"]}"
   }
 }
