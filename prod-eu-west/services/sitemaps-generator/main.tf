f180f445d1cc671f4a1f4ac6968a4d1260a8be6f
Martin Fenner <martin.fenner@datacite.org>
2020-11-01 14:13:12 +0100

    schedule sitemaps generator

--- prod-eu-west/services/sitemaps-generator/main.tf
+++ prod-eu-west/services/sitemaps-generator/main.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_event_rule" "sitemaps-generator" {
   name = "sitemaps-generator"
   description = "Run sitemaps-generator container via cron"
-  schedule_expression = "cron(20 0 ? * MON *)"
+  schedule_expression = "cron(20 13 ? * SUN *)"
 }
 
 resource "aws_cloudwatch_event_target" "sitemaps-generator" {
