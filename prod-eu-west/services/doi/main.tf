5ce22c83cf52ebf4fb3ba4c9f14ba73a52c2859d
Martin Fenner <martin.fenner@datacite.org>
2020-11-20 06:53:45 +0100

    increase health check grace period

--- prod-eu-west/services/doi/main.tf
+++ prod-eu-west/services/doi/main.tf
@@ -13,7 +13,7 @@ resource "aws_ecs_service" "doi" {
   }
 
   # give container time to start up
-  health_check_grace_period_seconds = 900
+  health_check_grace_period_seconds = 1200
 
   network_configuration {
     security_groups = ["${data.aws_security_group.datacite-private.id}"]
