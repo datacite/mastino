e1727588c99e817b50a7871c9b2bcd34f7de4dcc
svogt0511 <svogt@sumarc.net>
2020-12-17 15:40:57 -0500

    Update to Terraform V.12

--- test/services/metrics-api/main.tf
+++ test/services/metrics-api/main.tf
@@ -66,13 +66,15 @@ resource "aws_lb_listener_rule" "metrics-api-test" {
   }
 
   condition {
-    field  = "host-header"
-    values = ["api.test.datacite.org"]
+    host_header {
+      values = ["api.test.datacite.org"]
+    }
   }
 
   condition {
-    field  = "path-pattern"
-    values = ["/reports*"]
+    path_pattern {
+      values = ["/reports*"]
+    }
   }
 
 }
@@ -87,13 +89,15 @@ resource "aws_lb_listener_rule" "metrics-api-test-subset" {
   }
 
   condition {
-    field  = "host-header"
-    values = ["api.test.datacite.org"]
+    host_header {
+      values = ["api.test.datacite.org"]
+      }
   }
 
   condition {
-    field  = "path-pattern"
-    values = ["/report-subsets*"]
+    path_pattern {
+      values = ["/report-subsets*"]
+    }
   }
 }
 
@@ -107,13 +111,15 @@ resource "aws_lb_listener_rule" "metrics-api-test-repositories" {
   }
 
   condition {
-    field  = "host-header"
-    values = ["api.test.datacite.org"]
+    host_header {
+      values = ["api.test.datacite.org"]
+    }
   }
 
   condition {
-    field  = "path-pattern"
-    values = ["/repositories-usage-reports*"]
+    path_pattern {
+      values = ["/repositories-usage-reports*"]
+    }
   }
 }
 
