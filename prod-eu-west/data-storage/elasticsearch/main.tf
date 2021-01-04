362cc6d232c1e95bae3d6c7cea0843532bc6e4c1
Richard Hallett <rhallett@datacite.org>
2021-01-04 16:10:19 +0100

    Revert ES disk size back to 1500

--- prod-eu-west/data-storage/elasticsearch/main.tf
+++ prod-eu-west/data-storage/elasticsearch/main.tf
@@ -18,7 +18,7 @@ resource "aws_elasticsearch_domain" "default" {
   ebs_options{
       ebs_enabled = true
       volume_type = "gp2"
-      volume_size = 750
+      volume_size = 1500
   }
 
   vpc_options {
