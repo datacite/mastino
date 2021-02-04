2a283dd80b7ef0cfeb04927c93dd59813d11d08e
Martin Fenner <martin.fenner@datacite.org>
2021-02-04 20:57:16 +0100

    update dns für doi test

--- test/services/doi/main.tf
+++ test/services/doi/main.tf
@@ -159,7 +159,7 @@ resource "aws_route53_record" "doi-test" {
     name = "doi.test.datacite.org"
     type = "CNAME"
     ttl = var.ttl
-    records = ["cname.vercel-dns.com"]
+    records = ["bracco-test.vercel.app"]
 }
 
 resource "aws_route53_record" "split-doi-test" {
