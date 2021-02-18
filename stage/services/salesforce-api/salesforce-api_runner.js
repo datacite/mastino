caeefa6e18423f7368a441b6b1b0a09a96d65c20
Martin Fenner <martin.fenner@datacite.org>
2021-02-18 21:39:15 +0100

    use axios library

--- stage/services/salesforce-api/salesforce-api_runner.js
+++ stage/services/salesforce-api/salesforce-api_runner.js
@@ -1,45 +1,27 @@
 exports.handler = async function (event, context) {
-  const https = require("https");
-  const querystring = require("querystring");
+  const username = process.env.username;
+  const password = process.env.password;
+  const host = process.env.host;
+  const client_id = process.env.client_id;
+  const client_secret = process.env.client_secret;
 
-  let username = process.env.username;
-  let password = process.env.password;
-  let host = process.env.host;
-  let client_id = process.env.client_id;
-  let client_secret = process.env.client_secret;
+  const axios = require("axios");
+  axios.defaults.baseURL = host;
 
-  let params = querystring.stringify({
-    grant_type: "password",
-    username: username,
-    password: password,
-    client_id: client_id,
-    client_secret: client_secret,
-  });
-
-  let options = {
-    host: host,
-    path: `/services/oauth2/token?${params}`,
-    method: "POST",
-  };
-
-  const req = https.request(options, (res) => {
-    console.log("status:", res.statusCode);
-
-    res.setEncoding("utf8");
-    res.on("access_token", (t) => {
-      console.log("message:", t);
+  axios
+    .post("/services/oauth2/token", {
+      grant_type: "password",
+      username: username,
+      password: password,
+      client_id: client_id,
+      client_secret: client_secret,
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
     });
-    // res.on("data", (d) => {
-    //   let json = JSON.parse(d);
-    //   console.log("message:", json.message);
-    // });
-  });
-
-  req.on("error", (e) => {
-    console.error(e);
-  });
-
-  req.end();
 
   // event.Records.forEach((record) => {
   //   const { body } = record;
