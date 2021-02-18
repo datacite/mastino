579880b3a724f092d42a50788eb4d0459b048fdb
Martin Fenner <martin.fenner@datacite.org>
2021-02-18 15:37:38 +0100

    remove unused variables

--- stage/services/salesforce-api/main.tf
+++ stage/services/salesforce-api/main.tf
@@ -1,4 +1,4 @@
-resource "aws_lambda_function" "salesforce-api-stage" {
+resource "aws_lambda_function" "salesforce-api_runner-stage" {
   filename = "salesforce-api_runner.js.zip"
   function_name = "salesforce-api-stage"
   role = data.aws_iam_role.lambda.arn
@@ -11,13 +11,13 @@ resource "aws_lambda_function" "salesforce-api-stage" {
     subnet_ids = [data.aws_subnet.datacite-private.id, data.aws_subnet.datacite-alt.id]
     security_group_ids = [data.aws_security_group.datacite-private.id]
   }
-  environment {
-    variables = {
-      host     = var.host
-      username = var.username
-      password = var.password
-    }
-  }
+  // environment {
+  //   variables = {
+  //     host     = var.host
+  //     username = var.username
+  //     password = var.password
+  //   }
+  // }
 }
 
 resource "aws_lambda_permission" "salesforce-api_runner-stage" {
@@ -25,5 +25,4 @@ resource "aws_lambda_permission" "salesforce-api_runner-stage" {
   action = "lambda:InvokeFunction"
   function_name = aws_lambda_function.salesforce-api_runner-stage.function_name
   principal = "events.amazonaws.com"
-  source_arn = aws_cloudwatch_event_rule.salesforce-api-stage.arn
 }
