9faaa6677a51e845184528545a3eba0a44d09540
Martin Fenner <martin.fenner@datacite.org>
2021-02-18 22:28:13 +0100

    include host directly

--- stage/services/salesforce-api/salesforce-api_runner.js
+++ stage/services/salesforce-api/salesforce-api_runner.js
@@ -1,15 +1,14 @@
 exports.handler = async function (event, context) {
   const username = process.env.username;
   const password = process.env.password;
-  const host = "https://" + process.env.host;
+  const host = process.env.host;
   const client_id = process.env.client_id;
   const client_secret = process.env.client_secret;
 
   const axios = require("axios");
-  axios.defaults.baseURL = host;
 
   axios
-    .post("/services/oauth2/token", {
+    .post(`https://${host}/services/oauth2/token`, {
       grant_type: "password",
       username: username,
       password: password,
