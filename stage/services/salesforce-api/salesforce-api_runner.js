83eb7f27fee8dfcd52a06ce6364865b59d8cbec3
Martin Fenner <martin.fenner@datacite.org>
2021-02-18 19:24:46 +0100

    correctly send query params

--- stage/services/salesforce-api/salesforce-api_runner.js
+++ stage/services/salesforce-api/salesforce-api_runner.js
@@ -1,22 +1,24 @@
 exports.handler = async function (event, context) {
+  const https = require("https");
+  const querystring = require("querystring");
+
   const username = process.env.username;
   const password = process.env.password;
   const host = process.env.host;
   const client_id = process.env.client_id;
   const client_secret = process.env.client_secret;
 
-  const querystring = require("querystring");
-  const postData = querystring.stringify({
+  const params = querystring.stringify({
     grant_type: "password",
     username: username,
     password: password,
     client_id: client_id,
     client_secret: client_secret,
   });
-  const https = require("https");
+
   const options = {
     host: host,
-    path: "/services/oauth2/token",
+    path: `/services/oauth2/token${params}`,
     method: "POST",
   };
 
@@ -33,7 +35,6 @@ exports.handler = async function (event, context) {
   req.on("error", (e) => {
     console.error(e);
   });
-  req.write(postData);
   req.end();
 
   // event.Records.forEach((record) => {
