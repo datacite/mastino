2a2db36193dbf1f5e6d62c22ab2f2cad390d565a
Martin Fenner <martin.fenner@datacite.org>
2021-02-23 13:25:22 +0100

    reformat request

--- stage/services/salesforce-api/salesforce-api_runner.js
+++ stage/services/salesforce-api/salesforce-api_runner.js
@@ -6,30 +6,24 @@ exports.handler = async function (event, context) {
   const client_secret = process.env.client_secret;
 
   const axios = require("axios");
-
-  const params = {
-    grant_type: "password",
-    username: username,
-    password: password,
-    client_id: client_id,
-    client_secret: client_secret,
-  };
-  console.log(params);
-
   axios
-    .post(url, {}, { params })
+    .post(
+      url,
+      {},
+      {
+        params: {
+          grant_type: "password",
+          username: username,
+          password: password,
+          client_id: client_id,
+          client_secret: client_secret,
+        },
+      }
+    )
     .then((response) => {
       console.log(response);
     })
-    .catch((err) => {
-      if (err.response) {
-        console.log(err.response);
-      } else if (err.request) {
-        console.log(err.request);
-      } else {
-        console.log(err);
-      }
-    });
+    .catch((err) => console.warn(err));
 
   // event.Records.forEach((record) => {
   //   const { body } = record;
