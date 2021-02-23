bc375693748547d9967be99c2ec3166d9ee04ca9
Martin Fenner <martin.fenner@datacite.org>
2021-02-23 11:23:30 +0100

    use jsforce library with salesforce api

--- stage/services/salesforce-api/salesforce-api_runner.js
+++ stage/services/salesforce-api/salesforce-api_runner.js
@@ -2,25 +2,23 @@ exports.handler = async function (event, context) {
   const username = process.env.username;
   const password = process.env.password;
   const host = process.env.host;
-  const client_id = process.env.client_id;
-  const client_secret = process.env.client_secret;
 
-  const axios = require("axios");
+  var jsforce = require("jsforce");
+  var conn = new jsforce.Connection({
+    loginUrl: "https://" + host,
+  });
 
-  axios
-    .post(`https://${host}/services/oauth2/token`, {
-      grant_type: "password",
-      username: username,
-      password: password,
-      client_id: client_id,
-      client_secret: client_secret,
-    })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
+  conn.login(username, password, function (err, res) {
+    if (err) {
+      return console.error(err);
+    }
+    conn.query("SELECT Id, Name FROM Account", function (err, res) {
+      if (err) {
+        return console.error(err);
+      }
+      console.log(res);
     });
+  });
 
   // event.Records.forEach((record) => {
   //   const { body } = record;
