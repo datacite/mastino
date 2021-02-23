7855c00ec2f2730f0c50d5982f33f4d0764b34d8
Martin Fenner <martin.fenner@datacite.org>
2021-02-23 13:50:21 +0100

    enable running the lambda function locally

--- stage/services/salesforce-api/salesforce-api_runner.js
+++ stage/services/salesforce-api/salesforce-api_runner.js
@@ -1,29 +1,28 @@
 exports.handler = async function (event, context) {
-  const username = process.env.username;
-  const password = process.env.password;
-  const url = `https://${process.env.host}/services/oauth2/token`;
-  const client_id = process.env.client_id;
-  const client_secret = process.env.client_secret;
+  require("dotenv").config();
 
   const axios = require("axios");
-  axios
+  const url = `https://${process.env.host}/services/oauth2/token`;
+  const data = await axios
     .post(
       url,
       {},
       {
         params: {
           grant_type: "password",
-          username: username,
-          password: password,
-          client_id: client_id,
-          client_secret: client_secret,
+          username: process.env.username,
+          password: process.env.password,
+          client_id: process.env.client_id,
+          client_secret: process.env.client_secret,
         },
       }
     )
     .then((response) => {
-      console.log(response);
+      return response.data;
     })
     .catch((err) => console.warn(err));
+
+  console.log(data);
 
   // event.Records.forEach((record) => {
   //   const { body } = record;
