f0be72f81bbaca14e6a1a11a923b5c8499d15ede
Martin Fenner <martin.fenner@datacite.org>
2021-02-24 07:29:20 +0100

    patch api call for contacts

--- stage/services/salesforce-api/salesforce-api_runner.js
+++ stage/services/salesforce-api/salesforce-api_runner.js
@@ -1,11 +1,12 @@
 exports.handler = async function (event, context) {
   require("dotenv").config();
 
+  const apiVersion = "v51.0"
   const axios = require("axios");
-  const url = `https://${process.env.host}/services/oauth2/token`;
+  const authUrl = `https://${process.env.host}/services/oauth2/token`;
   const auth = await axios
     .post(
-      url,
+      authUrl,
       {},
       {
         params: {
@@ -23,14 +24,37 @@ exports.handler = async function (event, context) {
     .catch((err) => console.warn(err));
 
   event.Records.forEach((record) => {
-    let response = JSON.parse(record.body);
-    if (response.type === "contacts") {
-      console.log("Contact");
-      console.log(response.attributes);
-    } else if (response.type === "providers") {
+    let res = JSON.parse(record.body);
+    if (res.type === "contacts") {
+      let url = `${auth.instance_url}/services/data/${apiVersion}/sobjects/Contact/Uid__c/${res.attributes.uid}`
+      let body = {
+        "FirstName": res.attributes.given_name,
+        "LastName": res.attributes.family_name,
+        "Email": res.attributes.email,
+        "Fabrica_ID__c": `${res.attributes.provider_id.toUpperCase()}-${res.attributes.email}`,
+        "Type__c": res.attributes.role_name.join(";"),
+        "CreatedAt__c": res.attributes.created_at,
+        "ModifiedAt__c": res.attributes.updated_at,
+        "DeletedAt__c": res.attributes.deleted_at,
+        "Active__c": !res.attributes.deleted_at
+      }
+
+      await axios
+        .patch(
+          url,
+          body,
+          {
+            headers: { Authorization: `Bearer ${auth.access_token}` }
+          }
+        )
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((err) => console.warn(err));
+    } else if (res.type === "providers") {
       console.log("Organization");
       console.log(response.attributes);
-    } else if (response.type === "repositories") {
+    } else if (res.type === "repositories") {
       console.log("Repository");
       console.log(response.attributes);
     }
