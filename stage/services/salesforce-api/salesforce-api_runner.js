e70bdb9bf287f154509e097e6cbd15deadcb318b
Martin Fenner <martin.fenner@datacite.org>
2021-02-24 07:42:39 +0100

    more logging

--- stage/services/salesforce-api/salesforce-api_runner.js
+++ stage/services/salesforce-api/salesforce-api_runner.js
@@ -41,6 +41,8 @@ exports.handler = async function (event, context) {
         Active__c: !res.attributes.deleted_at,
       };
 
+      console.log(body);
+
       axios
         .patch(url, body, {
           headers: { Authorization: `Bearer ${auth.access_token}` },
