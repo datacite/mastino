558bdcfef88ca9289348c5424056fbce65124d84
Martin Fenner <martin.fenner@datacite.org>
2021-02-26 07:17:59 +0100

    better error handling

--- stage/services/salesforce-api/salesforce-api_runner.js
+++ stage/services/salesforce-api/salesforce-api_runner.js
@@ -26,7 +26,7 @@ exports.handler = async function (event, context) {
   // each message has a single record
   let res = JSON.parse(event.Records[0].body);
   if (res.type === "contacts") {
-    let url = `${auth.instance_url}/services/data/${apiVersion}/sobjects/Contact/Uid__c/${res.attributes.uid}`;
+    let url = `${auth.instance_url}/services/data/${apiVersion}/sobjects/Contact/Uid__c/${res.id}`;
     let body = {
       FirstName: res.attributes.given_name,
       LastName: res.attributes.family_name,
@@ -58,7 +58,8 @@ exports.handler = async function (event, context) {
         }
       });
   } else if (res.type === "providers") {
-    let url = `${auth.instance_url}/services/data/${apiVersion}/sobjects/Account/Fabrica__c/${res.attributes.symbol}`;
+    const regions = { AMER: "Americas", EMEA: "EMEA", APAC: "Asia Pacific" };
+    let url = `${auth.instance_url}/services/data/${apiVersion}/sobjects/Account/Fabrica__c/${res.id}`;
     let body = {
       Name: res.attributes.name,
       ParentId: res.attributes.parent_organization,
@@ -71,7 +72,7 @@ exports.handler = async function (event, context) {
       Member_Type__c: res.attributes.member_type,
       Sector__c: res.attributes.organization_type,
       Focus_Area__c: res.attributes.focus_area,
-      Region__c: res.attributes.region,
+      Region__c: res.attributes.region ? regions[res.attributes.region] : null,
       Assign_DOIs__c: [
         "Direct Member",
         "Consortium",
@@ -113,7 +114,9 @@ exports.handler = async function (event, context) {
     let url = `${auth.instance_url}/services/data/${apiVersion}/sobjects/Repositories__c/Repository_ID__c/${res.attributes.symbol}`;
     let body = {
       Name: res.attributes.name,
-      Organization__c: res.attributes.provider_id,
+      Organization__c: res.attributes.provider_id
+        ? res.attributes.provider_id.toUpperCase()
+        : null,
       Repository_URL__c: res.attributes.url,
       re3data_Record__c: res.attributes.re3data_id,
       Description__c: res.attributes.description,
@@ -135,7 +138,7 @@ exports.handler = async function (event, context) {
       })
       .catch((err) => {
         if (err.response) {
-          console.log(err.response);
+          console.log(err.response.data);
         } else if (err.request) {
           console.log(err.request);
         } else {
