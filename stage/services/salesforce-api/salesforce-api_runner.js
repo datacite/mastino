c9ac0381fa41b13973b9b378af82f4b11db39b8f
Martin Fenner <martin.fenner@datacite.org>
2021-03-30 21:31:15 +0200

    more generic error handling

--- stage/services/salesforce-api/salesforce-api_runner.js
+++ stage/services/salesforce-api/salesforce-api_runner.js
@@ -142,7 +142,7 @@ exports.handler = async function (event, context) {
       })
       .catch((err) => {
         if (err.response) {
-          console.log(error.response.data);
+          console.log(error.response);
           slackMessage({
             text: "Error updating organization in Salesforce.",
             attachments: [
@@ -197,7 +197,7 @@ exports.handler = async function (event, context) {
       })
       .catch((err) => {
         if (err.response) {
-          console.log(error.response.data);
+          console.log(error.response);
           slackMessage({
             text: "Error updating contact in Salesforce.",
             attachments: [
@@ -273,7 +273,7 @@ exports.handler = async function (event, context) {
       })
       .catch((err) => {
         if (err.response) {
-          console.log(error.response.data);
+          console.log(error.response);
           slackMessage({
             text: "Error updating contact in Salesforce.",
             attachments: [
@@ -367,7 +367,7 @@ exports.handler = async function (event, context) {
       })
       .catch((err) => {
         if (err.response) {
-          console.log(error.response.data);
+          console.log(error.response);
           slackMessage({
             text: "Error updating repository in Salesforce.",
             attachments: [
