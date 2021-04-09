c62c7840a22eecf96ad0c6d7f2df235cdc3d85f0
Martin Fenner <martin.fenner@datacite.org>
2021-04-09 12:27:51 +0200

    update salesforce field names

--- stage/services/salesforce-api/salesforce-api_runner.js
+++ stage/services/salesforce-api/salesforce-api_runner.js
@@ -343,8 +343,8 @@ exports.handler = async function (event, context) {
       dois_count_current_year__c: res.attributes.dois_current_year,
       dois_count_previous_year__c: res.attributes.dois_last_year,
       dois_count_total__c: res.attributes.dois_total,
-      Fabrica_Creation_Date__c: res.attributes.created,
-      Fabrica_Modification_Date__c: res.attributes.updated,
+      Fabrica_creation_date__c: res.attributes.created,
+      Fabrica_modified_date__c: res.attributes.updated,
       Fabrica_Deletion_Date__c: res.attributes.deleted_at,
       IsActive__c: res.attributes.is_active,
     };
