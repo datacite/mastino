7395b758f4b572d70985e9e5b8a5996681140315
Martin Fenner <martin.fenner@datacite.org>
2021-04-15 19:01:56 +0200

    remove extra logging

--- prod-eu-west/services/salesforce-api/salesforce-api_runner.js
+++ prod-eu-west/services/salesforce-api/salesforce-api_runner.js
@@ -55,14 +55,11 @@ exports.handler = async function (event, context) {
     return null;
   }
 
-  console.log(auth);
-
   let url, body, organization, result;
 
   // each message has a single record
   let res = JSON.parse(event.Records[0].body);
   if (res.type === "providers") {
-    console.log(res.attributes);
     const regions = { AMER: "Americas", EMEA: "EMEA", APAC: "Asia Pacific" };
     if (res.attributes.parent_organization) {
       url = `${auth.instance_url}/services/data/${apiVersion}/sobjects/Account/Fabrica__c/${res.attributes.parent_organization}`;
@@ -230,12 +227,9 @@ exports.handler = async function (event, context) {
       }
     }
   } else if (res.type === "contacts") {
-    console.log(res.attributes);
-
     url = `${
       auth.instance_url
     }/services/data/${apiVersion}/sobjects/Account/Fabrica__c/${res.attributes.provider_id.toUpperCase()}`;
-    console.log(url);
     try {
       organization = await axios.get(url, {
         headers: { Authorization: `Bearer ${auth.access_token}` },
