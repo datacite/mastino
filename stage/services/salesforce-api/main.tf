7bf519b3f1edc0e229c034cd266e71c4a76cb277
Martin Fenner <martin.fenner@datacite.org>
2021-04-15 13:58:12 +0200

    run scripts daily

--- stage/services/salesforce-api/main.tf
+++ stage/services/salesforce-api/main.tf
@@ -1,34 +1,22 @@
-resource "aws_cloudwatch_event_rule" "update-salesforce-hourly-stage" {
-  name = "update-salesforce-hourly-stage"
-  description = "Update salesforce every hour via cron"
-  schedule_expression = "cron(50 * * * ? *)"
-}
-
-resource "aws_cloudwatch_event_rule" "update-salesforce-weekly-stage" {
-  name = "update-salesforce-weekly-stage"
+resource "aws_cloudwatch_event_rule" "update-salesforce-daily-stage" {
+  name = "update-salesforce-daily-stage"
   description = "Update salesforce daily via cron"
   schedule_expression = "cron(40 11 * * ? *)"
 }
 
-resource "aws_cloudwatch_event_target" "update-salesforce-hourly-stage" {
-  target_id = "update-salesforce-hourly-stage"
-  rule = aws_cloudwatch_event_rule.update-salesforce-hourly-stage.name
-  arn = aws_lambda_function.update-salesforce-hourly-stage.arn
+resource "aws_cloudwatch_event_target" "update-salesforce-daily-stage" {
+  target_id = "update-salesforce-daily-stage"
+  rule = aws_cloudwatch_event_rule.update-salesforce-daily-stage.name
+  arn = aws_lambda_function.update-salesforce-daily-stage.arn
 }
 
-resource "aws_cloudwatch_event_target" "update-salesforce-weekly-stage" {
-  target_id = "update-salesforce-weekly-stage"
-  rule = aws_cloudwatch_event_rule.update-salesforce-weekly-stage.name
-  arn = aws_lambda_function.update-salesforce-weekly-stage.arn
-}
-
-resource "aws_lambda_function" "update-salesforce-hourly-stage" {
-  filename = "salesforce-hourly_runner.js.zip"
-  function_name = "update-salesforce-hourly-stage"
+resource "aws_lambda_function" "update-salesforce-daily-stage" {
+  filename = "salesforce-daily_runner.js.zip"
+  function_name = "update-salesforce-daily-stage"
   role = data.aws_iam_role.lambda.arn
-  handler = "salesforce-hourly_runner.handler"
+  handler = "salesforce-daily_runner.handler"
   runtime = "nodejs12.x"
-  source_code_hash = sha256(filebase64("salesforce-hourly_runner.js.zip"))
+  source_code_hash = sha256(filebase64("salesforce-daily_runner.js.zip"))
   timeout = "270"
 
   vpc_config {
@@ -43,28 +31,6 @@ resource "aws_lambda_function" "update-salesforce-hourly-stage" {
       datacite_api_url = var.datacite_api_url
       datacite_username = var.datacite_username
       datacite_password = var.datacite_password
-    }
-  }
-}
-
-resource "aws_lambda_function" "update-salesforce-weekly-stage" {
-  filename = "salesforce-weekly_runner.js.zip"
-  function_name = "update-salesforce-weekly-stage"
-  role = data.aws_iam_role.lambda.arn
-  handler = "salesforce-weekly_runner.handler"
-  runtime = "nodejs12.x"
-  source_code_hash = sha256(filebase64("salesforce-weekly_runner.js.zip"))
-  timeout = "270"
-
-  vpc_config {
-    subnet_ids = [data.aws_subnet.datacite-private.id, data.aws_subnet.datacite-alt.id]
-    security_group_ids = [data.aws_security_group.datacite-private.id]
-  }
-  environment {
-    variables = {
-      host     = var.host
-      username = var.username
-      password = var.password
     }
   }
 }
@@ -95,20 +61,12 @@ resource "aws_lambda_function" "salesforce-api-stage" {
   }
 }
 
-resource "aws_lambda_permission" "update-salesforce-hourly-stage" {
+resource "aws_lambda_permission" "update-salesforce-daily-stage" {
   statement_id = "AllowExecutionFromCloudWatch"
   action = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.update-salesforce-hourly-stage.function_name
+  function_name = aws_lambda_function.update-salesforce-daily-stage.function_name
   principal = "events.amazonaws.com"
-  source_arn = aws_cloudwatch_event_rule.update-salesforce-hourly-stage.arn
-}
-
-resource "aws_lambda_permission" "update-salesforce-weekly-stage" {
-  statement_id = "AllowExecutionFromCloudWatch"
-  action = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.update-salesforce-weekly-stage.function_name
-  principal = "events.amazonaws.com"
-  source_arn = aws_cloudwatch_event_rule.update-salesforce-weekly-stage.arn
+  source_arn = aws_cloudwatch_event_rule.update-salesforce-daily-stage.arn
 }
 
 resource "aws_lambda_permission" "salesforce-api-stage" {
