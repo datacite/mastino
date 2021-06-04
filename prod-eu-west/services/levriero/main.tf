de8748a0b11dc3fab4ebababb3a5bc866d4cf426
Martin Fenner <martin.fenner@datacite.org>
2021-06-04 10:14:41 +0200

    fix syntax

--- prod-eu-west/services/levriero/main.tf
+++ prod-eu-west/services/levriero/main.tf
@@ -1,8 +1,8 @@
 resource "aws_ecs_service" "levriero" {
   name = "levriero"
-  cluster = "${data.aws_ecs_cluster.default.id}"
+  cluster = data.aws_ecs_cluster.default.id
   launch_type = "FARGATE"
-  task_definition = "${aws_ecs_task_definition.levriero.arn}"
+  task_definition = aws_ecs_task_definition.levriero.arn
   
   # Create service with 2 instances to start
   desired_count = 2
@@ -13,21 +13,21 @@ resource "aws_ecs_service" "levriero" {
   }
 
   network_configuration {
-    security_groups = ["${data.aws_security_group.datacite-private.id}"]
+    security_groups = [data.aws_security_group.datacite-private.id]
     subnets         = [
-      "${data.aws_subnet.datacite-private.id}",
-      "${data.aws_subnet.datacite-alt.id}"
+      data.aws_subnet.datacite-private.id,
+      data.aws_subnet.datacite-alt.id
     ]
   }
 
   load_balancer {
-    target_group_arn = "${aws_lb_target_group.levriero.id}"
+    target_group_arn = aws_lb_target_group.levriero.id
     container_name   = "levriero"
     container_port   = "80"
   }
 
   service_registries {
-    registry_arn = "${aws_service_discovery_service.levriero.arn}"
+    registry_arn = aws_service_discovery_service.levriero.arn
   }
 
   depends_on = [
@@ -46,9 +46,9 @@ resource "aws_appautoscaling_target" "levriero" {
 resource "aws_appautoscaling_policy" "levriero_scale_up" {
   name               = "scale-up"
   policy_type        = "StepScaling"
-  resource_id        = "${aws_appautoscaling_target.levriero.resource_id}"
-  scalable_dimension = "${aws_appautoscaling_target.levriero.scalable_dimension}"
-  service_namespace  = "${aws_appautoscaling_target.levriero.service_namespace}"
+  resource_id        = aws_appautoscaling_target.levriero.resource_id
+  scalable_dimension = aws_appautoscaling_target.levriero.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.levriero.service_namespace
 
   step_scaling_policy_configuration {
     adjustment_type         = "ChangeInCapacity"
@@ -65,9 +65,9 @@ resource "aws_appautoscaling_policy" "levriero_scale_up" {
 resource "aws_appautoscaling_policy" "levriero_scale_down" {
   name               = "scale-down"
   policy_type        = "StepScaling"
-  resource_id        = "${aws_appautoscaling_target.levriero.resource_id}"
-  scalable_dimension = "${aws_appautoscaling_target.levriero.scalable_dimension}"
-  service_namespace  = "${aws_appautoscaling_target.levriero.service_namespace}"
+  resource_id        = aws_appautoscaling_target.levriero.resource_id
+  scalable_dimension = aws_appautoscaling_target.levriero.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.levriero.service_namespace
 
   step_scaling_policy_configuration {
     adjustment_type         = "ChangeInCapacity"
@@ -93,11 +93,11 @@ resource "aws_cloudwatch_metric_alarm" "levriero_cpu_scale_up" {
 
   dimensions {
     ClusterName = "default"
-    ServiceName = "${aws_ecs_service.levriero.name}"
+    ServiceName = aws_ecs_service.levriero.name
   }
 
   alarm_description = "This metric monitors ecs cpu utilization"
-  alarm_actions     = ["${aws_appautoscaling_policy.levriero_scale_up.arn}"]
+  alarm_actions     = [aws_appautoscaling_policy.levriero_scale_up.arn]
 }
 
 resource "aws_cloudwatch_metric_alarm" "levriero_cpu_scale_down" {
@@ -110,13 +110,13 @@ resource "aws_cloudwatch_metric_alarm" "levriero_cpu_scale_down" {
   statistic           = "Average"
   threshold           = "20"
 
-  dimensions {
+  dimensions = {
     ClusterName = "default"
-    ServiceName = "${aws_ecs_service.levriero.name}"
+    ServiceName = aws_ecs_service.levriero.name
   }
 
   alarm_description = "This metric monitors ecs cpu utilization"
-  alarm_actions     = ["${aws_appautoscaling_policy.levriero_scale_down.arn}"]
+  alarm_actions     = [aws_appautoscaling_policy.levriero_scale_down.arn]
 }
 
 resource "aws_cloudwatch_log_group" "levriero" {
@@ -125,19 +125,19 @@ resource "aws_cloudwatch_log_group" "levriero" {
 
 resource "aws_ecs_task_definition" "levriero" {
   family = "levriero"
-  execution_role_arn = "${data.aws_iam_role.ecs_task_execution_role.arn}"
+  execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
   network_mode = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu = "1024"
   memory = "2048"
-  container_definitions =  "${data.template_file.levriero_task.rendered}"
+  container_definitions =  data.template_file.levriero_task.rendered
 }
 
 resource "aws_lb_target_group" "levriero" {
   name     = "levriero"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = "${var.vpc_id}"
+  vpc_id   = var.vpc_id
   target_type = "ip"
 
   health_check {
