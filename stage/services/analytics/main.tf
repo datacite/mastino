7ed8a4a32f625a2ed80fd431555fd99f67348c82
Suzanne Vogt <suzanne.vogt@datacite.org>
2021-11-01 14:55:38 -0400

    geoip

--- stage/services/analytics/main.tf
+++ stage/services/analytics/main.tf
@@ -48,8 +48,8 @@ resource "aws_ecs_task_definition" "analytics-stage" {
     efs_volume_configuration {
       file_system_id = "fs-f6817e3c"
       root_directory = "/geoip"
-      authorizationConfig = {
-          accessPointId = "fsap-09f51dfb1417fb9e3"
+      authorization_config = {
+          access_point_id = "fsap-09f51dfb1417fb9e3"
       }
     }
   }
