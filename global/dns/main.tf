bc4564e3fb71428e4593750255003b62bf639b79
kjgarza <kgarza@datacite.org>
2021-11-11 16:18:00 +0100

    Update main.tf

--- global/dns/main.tf
+++ global/dns/main.tf
@@ -81,12 +81,12 @@ resource "aws_route53_record" "support" {
     records = ["${var.support_dns_name}"]
 }
 
-resource "aws_route53_record" "support" {
+resource "aws_route53_record" "design" {
     zone_id = "${aws_route53_zone.production.zone_id}"
     name = "design.datacite.org"
     type = "CNAME"
     ttl = "300"
-    records = ["${var.support_dns_name}"]
+    records = ["${var.design_dns_name}"]
 }
 
 // resource "aws_route53_record" "stage" {
