b4ebf637df30c61c4b6cf6bde656c35446cf648b
Richard Hallett <richard@richardhallett.com>
2021-12-16 13:24:05 +0100

    Bump minimum to run for member-api

--- prod-eu-west/services/client-api/member.tf
+++ prod-eu-west/services/client-api/member.tf
@@ -37,7 +37,7 @@ resource "aws_ecs_service" "member-api" {
 
 resource "aws_appautoscaling_target" "member-api" {
   max_capacity       = 8
-  min_capacity       = 2
+  min_capacity       = 4
   resource_id        = "service/default/${aws_ecs_service.member-api.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
