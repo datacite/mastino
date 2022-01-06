65b56b5460154b8a336bf23f6128d04bd4c5da74
Richard Hallett <richard@richardhallett.com>
2022-01-06 07:02:41 +0100

    Increase content negotiation LB min

--- prod-eu-west/services/content-negotiation/main.tf
+++ prod-eu-west/services/content-negotiation/main.tf
@@ -33,7 +33,7 @@ resource "aws_ecs_service" "content-negotiation" {
 
 resource "aws_appautoscaling_target" "content-negotiation" {
   max_capacity       = 10
-  min_capacity       = 2
+  min_capacity       = 4
   resource_id        = "service/default/${aws_ecs_service.content-negotiation.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
