9e2e04ba5753047d94a1e988a318ec455b1ff466
Richard Hallett <richard@richardhallett.com>
2022-01-06 07:18:34 +0100

    Specify 2.7 for CN AWS

--- prod-eu-west/services/content-negotiation/input.tf
+++ prod-eu-west/services/content-negotiation/input.tf
@@ -2,7 +2,7 @@ provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
   region     = "${var.region}"
-  version    = "~> 2.70"
+  version = "v2.70.0"
 }
 
 data "aws_route53_zone" "crosscite" {
