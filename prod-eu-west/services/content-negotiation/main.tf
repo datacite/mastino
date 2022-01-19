144c668a606e478a832a1bc58cd5de47a561411c
Mike Bennett <93522067+digitaldogsbody@users.noreply.github.com>
2022-01-19 10:39:59 +0000

    Temporarily make CN run at max capacity

--- prod-eu-west/services/content-negotiation/main.tf
+++ prod-eu-west/services/content-negotiation/main.tf
@@ -5,7 +5,7 @@ resource "aws_ecs_service" "content-negotiation" {
   task_definition = "${aws_ecs_task_definition.content-negotiation.arn}"
 
   # Create service with 2 instances to start
-  desired_count = 4
+  desired_count = 10
 
   # Allow external changes without Terraform plan difference
   lifecycle {
