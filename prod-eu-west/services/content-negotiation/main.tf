0a13f018133be1dd81c14b33812a8b6396b5e835
Mike Bennett <93522067+digitaldogsbody@users.noreply.github.com>
2022-01-20 09:35:23 +0000

    Bump CN containers

--- prod-eu-west/services/content-negotiation/main.tf
+++ prod-eu-west/services/content-negotiation/main.tf
@@ -32,8 +32,8 @@ resource "aws_ecs_service" "content-negotiation" {
 }
 
 resource "aws_appautoscaling_target" "content-negotiation" {
-  max_capacity       = 10
-  min_capacity       = 8
+  max_capacity       = 15
+  min_capacity       = 15
   resource_id        = "service/default/${aws_ecs_service.content-negotiation.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
