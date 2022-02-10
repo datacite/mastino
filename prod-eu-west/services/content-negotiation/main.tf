8d4bdcb66431ab85162bcc7f5bf7dfd67835e895
Richard Hallett <richard@richardhallett.com>
2022-02-10 10:00:19 +0100

    Lower CN capacity

--- prod-eu-west/services/content-negotiation/main.tf
+++ prod-eu-west/services/content-negotiation/main.tf
@@ -32,8 +32,8 @@ resource "aws_ecs_service" "content-negotiation" {
 }
 
 resource "aws_appautoscaling_target" "content-negotiation" {
-  max_capacity       = 15
-  min_capacity       = 15
+  max_capacity       = 8
+  min_capacity       = 6
   resource_id        = "service/default/${aws_ecs_service.content-negotiation.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
