c6a6740022eb44527a74209e242a0e3e29d43a20
Mike Bennett <93522067+digitaldogsbody@users.noreply.github.com>
2022-04-19 16:10:43 +0100

    Temporarily bump levriero to handle events queue backlog

--- prod-eu-west/services/levriero/main.tf
+++ prod-eu-west/services/levriero/main.tf
@@ -36,8 +36,8 @@ resource "aws_ecs_service" "levriero" {
 }
 
 resource "aws_appautoscaling_target" "levriero" {
-  max_capacity       = 8
-  min_capacity       = 6
+  max_capacity       = 16
+  min_capacity       = 12
   resource_id        = "service/default/${aws_ecs_service.levriero.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
