0fa8f9f66ae2a4210f3b76415cf5da57ad9df92c
Richard Hallett <richard@richardhallett.com>
2022-05-17 19:55:17 +0200

    Add clickhouse setup for production

---
+++ prod-eu-west/data-storage/clickhouse/ec2_launch_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo docker plugin install rexray/ebs:latest REXRAY_PREEMPT=true EBS_REGION=eu-west-1 --grant-all-permissions
+
+
+# Adding cluster name in ecs config
+echo ECS_CLUSTER=default >> /etc/ecs/ecs.config
+cat /etc/ecs/ecs.config | grep "ECS_CLUSTER"
+
+echo ""
+echo "CLICKHOUSE-PROD LAUNCHED"
+echo ""
---
+++ prod-eu-west/data-storage/clickhouse/var.tf
@@ -0,0 +1,36 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+  default = "eu-west-1"
+}
+
+variable "public_key" {}
+
+variable "vpc_id" {}
+
+variable "lb_name" {
+  default = "lb"
+}
+
+variable "security_group_id" {}
+
+variable "ttl" {
+  default = "300"
+}
+
+variable "subnet_datacite-private_id" {}
+variable "subnet_datacite-alt_id" {}
+
+variable "namespace_id" {}
+
+variable "hostname" {}
+variable "key_name" {}
+
+variable "ami" {
+  type = map(string)
+  description = "Amazon Linux default AMI"
+
+  default = {
+    eu-west-1 = "ami-06c11ea68c61e5570"
+  }
+}
