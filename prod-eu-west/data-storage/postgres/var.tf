4f5613b2e5db725d8594f886c65e3e8a9c13ceae
Richard Hallett <richard@richardhallett.com>
2022-05-31 21:40:56 +0200

    Add postgres prod

---
+++ prod-eu-west/data-storage/postgres/var.tf
@@ -0,0 +1,10 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+    default = "eu-west-1"
+}
+
+variable "postgres_user" {}
+variable "postgres_password" {}
+
+variable "security_group_id" {}
