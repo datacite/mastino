45dc2e240a3c6c589dd5360bcc7f7af081fd1116
Richard Hallett <richard@richardhallett.com>
2022-06-01 12:14:30 +0200

    Fix container name for prod analytics

--- prod-eu-west/services/analytics/terraform.tf
+++ prod-eu-west/services/analytics/terraform.tf
@@ -2,6 +2,6 @@ terraform {
   required_version = ">= 0.12"
 
   backend "atlas" {
-    name         = "datacite-ng/prod-eu-west--services-analytics"
+    name         = "datacite-ng/prod-eu-west-services-analytics"
   }
 }
