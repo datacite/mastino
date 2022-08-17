5bd978e1431e38dc01e0d7b7ebc4abb295b1acbd
Richard Hallett <richard@richardhallett.com>
2022-08-17 16:27:52 +0200

    Upgrade python lambdas to 0.12

--- prod-eu-west/services/check-links/main.tf
+++ prod-eu-west/services/check-links/main.tf
@@ -6,29 +6,29 @@ resource "aws_cloudwatch_event_rule" "check-links" {
 
 resource "aws_cloudwatch_event_target" "check-links" {
   target_id = "check-links"
-  rule      = "${aws_cloudwatch_event_rule.check-links.name}"
-  arn       = "${aws_lambda_function.check-links.arn}"
+  rule      = aws_cloudwatch_event_rule.check-links.name
+  arn       = aws_lambda_function.check-links.arn
 }
 
 resource "aws_lambda_function" "check-links" {
   filename         = "check_links.py.zip"
   function_name    = "check-links"
-  role             = "${data.aws_iam_role.lambda.arn}"
+  role             = data.aws_iam_role.lambda.arn
   handler          = "check_links_runner.lambda_handler"
-  runtime          = "python3.9"
-  source_code_hash = "${base64sha256(file("check_links.py.zip"))}"
+  runtime          = "python3.8"
+  source_code_hash = sha256(filebase64("check_links.py.zip"))
   timeout          = "270"
 
   vpc_config {
-    subnet_ids         = ["${data.aws_subnet.datacite-private.id}", "${data.aws_subnet.datacite-alt.id}"]
-    security_group_ids = ["${data.aws_security_group.datacite-private.id}"]
+    subnet_ids         = [data.aws_subnet.datacite-private.id, data.aws_subnet.datacite-alt.id]
+    security_group_ids = [data.aws_security_group.datacite-private.id]
   }
 
   environment {
     variables = {
-      REDIS_HOST     = "${var.redis_host}"
-      START_URLS_KEY = "${var.start_urls_key}"
-      API_ENDPOINT   = "${var.api_endpoint}"
+      REDIS_HOST     = var.redis_host
+      START_URLS_KEY = var.start_urls_key
+      API_ENDPOINT   = var.api_endpoint
     }
   }
 }
@@ -36,7 +36,7 @@ resource "aws_lambda_function" "check-links" {
 resource "aws_lambda_permission" "check-links" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.check-links.function_name}"
+  function_name = aws_lambda_function.check-links.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_event_rule.check-links.arn}"
+  source_arn    = aws_cloudwatch_event_rule.check-links.arn
 }
