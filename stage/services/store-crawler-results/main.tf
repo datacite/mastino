8bcf0c5d413470f9208464d8ca4f1d184b48cc96
Richard Hallett <richard@richardhallett.com>
2022-08-17 15:50:07 +0200

    Upgrade stage store lambda terraform .12

--- stage/services/store-crawler-results/main.tf
+++ stage/services/store-crawler-results/main.tf
@@ -6,30 +6,30 @@ resource "aws_cloudwatch_event_rule" "store-crawler-results-test" {
 
 resource "aws_cloudwatch_event_target" "store-crawler-results-test" {
   target_id = "store-crawler-results-test"
-  rule      = "${aws_cloudwatch_event_rule.store-crawler-results-test.name}"
-  arn       = "${aws_lambda_function.store-crawler-results-test.arn}"
+  rule      = aws_cloudwatch_event_rule.store-crawler-results-test.name
+  arn       = aws_lambda_function.store-crawler-results-test.arn
 }
 
 resource "aws_lambda_function" "store-crawler-results-test" {
   filename         = "store_crawler_results.py.zip"
   function_name    = "store-crawler-results-test"
-  role             = "${data.aws_iam_role.lambda.arn}"
+  role             = data.aws_iam_role.lambda.arn
   handler          = "store_crawler_results_runner.lambda_handler"
-  runtime          = "python3.9"
-  source_code_hash = "${base64sha256(file("store_crawler_results.py.zip"))}"
+  runtime          = "python3.8"
+  source_code_hash = sha256(filebase64("store_crawler_results.py.zip"))
   timeout          = "270"
 
   vpc_config {
-    subnet_ids         = ["${data.aws_subnet.datacite-private.id}", "${data.aws_subnet.datacite-alt.id}"]
-    security_group_ids = ["${data.aws_security_group.datacite-private.id}"]
+    subnet_ids         = [data.aws_subnet.datacite-private.id, data.aws_subnet.datacite-alt.id]
+    security_group_ids = [data.aws_security_group.datacite-private.id]
   }
 
   environment {
     variables = {
-      REDIS_HOST     = "${var.redis_host}"
-      API_ENDPOINT   = "${var.api_endpoint}"
-      ADMIN_USERNAME = "${var.admin_username}"
-      ADMIN_PASSWORD = "${var.admin_password}"
+      REDIS_HOST     = var.redis_host
+      API_ENDPOINT   = var.api_endpoint
+      ADMIN_USERNAME = var.admin_username
+      ADMIN_PASSWORD = var.admin_password
     }
   }
 }
@@ -37,7 +37,7 @@ resource "aws_lambda_function" "store-crawler-results-test" {
 resource "aws_lambda_permission" "store-crawler-results-test" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.store-crawler-results-test.function_name}"
+  function_name = aws_lambda_function.store-crawler-results-test.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_event_rule.store-crawler-results-test.arn}"
+  source_arn    = aws_cloudwatch_event_rule.store-crawler-results-test.arn
 }
