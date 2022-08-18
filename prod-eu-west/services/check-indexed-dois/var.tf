8352b7c09a346b57c9aeb327219a02ffcd7d7c8c
Richard Hallett <richard@richardhallett.com>
2022-08-18 12:24:55 +0200

    Fix missing creds, needed to disable

--- prod-eu-west/services/check-indexed-dois/var.tf
+++ prod-eu-west/services/check-indexed-dois/var.tf
@@ -1,13 +1,13 @@
-# variable "access_key" {}
-# variable "secret_key" {}
-# variable "region" {
-#     default = "eu-west-1"
-# }
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+    default = "eu-west-1"
+}
 
-# variable "security_group_id" {}
-# variable "subnet_datacite-private_id" {}
-# variable "subnet_datacite-alt_id" {}
+variable "security_group_id" {}
+variable "subnet_datacite-private_id" {}
+variable "subnet_datacite-alt_id" {}
 
-# variable "host" {}
-# variable "username" {}
-# variable "password" {}
+variable "host" {}
+variable "username" {}
+variable "password" {}
