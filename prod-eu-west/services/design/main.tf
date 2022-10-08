7cda4667ef56ff9e6afc6d674e92b3b27a23fece
kjgarza <kgarza@datacite.org>
2022-10-08 12:04:08 +0200

    resource page for the design

---
+++ prod-eu-west/services/design/main.tf
@@ -0,0 +1,103 @@
+resource "aws_s3_bucket" "design" {
+    bucket = "design.datacite.org"
+    acl = "public-read"
+    policy = data.template_file.design.rendered
+    website {
+        index_document = "index.html"
+        error_document = "404.html"
+    }
+    cors_rule {
+      allowed_headers = ["*"]
+      allowed_methods = ["GET", "PUT", "POST"]
+      allowed_origins = ["*"]
+      expose_headers  = ["ETag"]
+      max_age_seconds = 3000
+    }
+    tags = {
+        Name = "design"
+    }
+    versioning {
+      enabled = true
+    }
+}
+
+resource "aws_cloudfront_distribution" "design" {
+  origin {
+    domain_name = aws_s3_bucket.design.website_endpoint
+    origin_id   = "design.datacite.org"
+
+    custom_origin_config {
+      origin_protocol_policy = "http-only"
+      http_port = "80"
+      https_port = "443"
+      origin_ssl_protocols = ["TLSv1"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  logging_config {
+    include_cookies = false
+    bucket          = data.aws_s3_bucket.logs.bucket_domain_name
+    prefix          = "design/"
+  }
+
+  aliases = ["design.datacite.org"]
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "design.datacite.org"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "whitelist"
+        whitelisted_names = ["_datacite_jwt"]
+      }
+
+      headers = ["Host", "Origin", "Access-Control-Request-Headers", "Access-Control-Request-Method", "Access-Control-Allow-Origin"]
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_All"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Environment = "prod"
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = data.aws_acm_certificate.cloudfront.arn
+    ssl_support_method  = "sni-only"
+  }
+}
+
+resource "aws_route53_record" "design" {
+   zone_id = data.aws_route53_zone.production.zone_id
+   name = "design.datacite.org"
+   type = "CNAME"
+   ttl = var.ttl
+   records = [aws_cloudfront_distribution.design.domain_name]
+}
+
+resource "aws_route53_record" "split-design" {
+   zone_id = data.aws_route53_zone.internal.zone_id
+   name = "design.datacite.org"
+   type = "CNAME"
+   ttl = var.ttl
+   records = [aws_cloudfront_distribution.design.domain_name]
+}
---
+++ prod-eu-west/services/design/terraform.tf
@@ -0,0 +1,7 @@
+terraform {
+  required_version = ">= 0.12"
+
+  backend "atlas" {
+    name         = "datacite-ng/prod-eu-west-services-design"
+  }
+}
