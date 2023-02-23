2c2839b0438d530867ed4cc89e6a02d32df7f05e
kjgarza <kgarza@datacite.org>
2023-02-23 11:31:05 +0100

    remove due to error in deployment
    The object was stored using a form of Server Side Encryption. The correct parameters must be provided to retrieve the object.

--- prod-eu-west/services/design/main.tf
+++ prod-eu-west/services/design/main.tf
@@ -19,32 +19,7 @@ resource "aws_s3_bucket" "design" {
     versioning {
       enabled = true
     }
-
-     server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.mykey.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
 }
-
-resource "aws_s3_bucket_public_access_block" "design" {
-  bucket = aws_s3_bucket.design.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-resource "aws_kms_key" "mykey" {
-  description             = "This key is used to encrypt bucket objects"
-  deletion_window_in_days = 10
-  enable_key_rotation= true
-}
-
 
 resource "aws_cloudfront_distribution" "design" {
   origin {
