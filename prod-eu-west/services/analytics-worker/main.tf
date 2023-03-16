1a7e0c4d21f070aa2e99d8f1282abb3fc21457ae
Richard Hallett <richard@richardhallett.com>
2023-03-16 11:11:58 +0100

    Add prod analytics worker

---
+++ prod-eu-west/services/analytics-worker/main.tf
@@ -0,0 +1,45 @@
+resource "aws_ecs_task_definition" "analytics-worker" {
+  family = "analytics-worker"
+  execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
+  network_mode = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu = "256"
+  memory = "512"
+  container_definitions = templatefile("analytics-worker.json",
+    {
+      version            = var.keeshond_tags["sha"]
+      analytics_database_dbname    = var.analytics_database_dbname
+      analytics_database_host      = var.analytics_database_host
+      analytics_database_user      = var.analytics_database_user
+      analytics_database_password  = var.analytics_database_password
+      datacite_jwt = var.datacite_jwt
+    })
+}
+
+resource "aws_cloudwatch_log_group" "analytics-worker" {
+  name = "/ecs/analytics-worker"
+}
+
+resource "aws_lambda_function" "analytics-worker" {
+  filename         = "analytics_worker.py.zip"
+  function_name    = "analytics-worker"
+  role             = data.aws_iam_role.lambda.arn
+  handler          = "analytics_worker_runner.lambda_handler"
+  runtime          = "python3.8"
+  source_code_hash = sha256(filebase64("analytics_worker.py.zip"))
+  timeout          = "270"
+
+  vpc_config {
+    subnet_ids         = [data.aws_subnet.datacite-private.id, data.aws_subnet.datacite-alt.id]
+    security_group_ids = [data.aws_security_group.datacite-private.id]
+  }
+
+  environment {
+    variables = {
+      AWS_SECURITY_GROUP = data.aws_security_group.datacite-private.id
+      AWS_DATACITE_SUBNET_PRIVATE       = data.aws_subnet.datacite-private.id
+      AWS_DATACITE_SUBNET_ALT       = data.aws_subnet.datacite-alt.id
+      AWS_CLUSTER        = data.aws_ecs_cluster.stage.id
+    }
+  }
+}
---
+++ prod-eu-west/services/analytics-worker/var.tf
@@ -0,0 +1,42 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+  default = "eu-west-1"
+}
+
+variable "vpc_id" {}
+
+variable "keeshond_tags" {
+  type = "map"
+}
+
+variable "lb_name" {
+  default = "lb"
+}
+
+variable "ttl" {
+  default = "300"
+}
+
+variable "security_group_id" {}
+variable "subnet_datacite-private_id" {}
+variable "subnet_datacite-alt_id" {}
+variable "namespace_id" {}
+
+variable "analytics_database_dbname" {
+    default = "analytics_events_db"
+}
+
+variable "analytics_database_host" {
+    default = "clickhouse.local"
+}
+
+variable "analytics_database_user" {
+    default = "admin"
+}
+
+variable "analytics_database_password" {
+    default = ""
+}
+
+variable "datacite_jwt" {}
