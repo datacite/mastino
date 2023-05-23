314858ea73a8fd65947eae9fe9e8dba198c636fd
Suzanne Vogt <suzanne.vogt@datacite.org>
2023-05-22 20:49:11 -0400

    Update to tf v12.  (Blocks of type tags are not expected here.)

--- stage/services/assets/main.tf
+++ stage/services/assets/main.tf
@@ -13,7 +13,7 @@ resource "aws_s3_bucket" "assets-stage" {
       expose_headers  = ["ETag"]
       max_age_seconds = 3000
     }
-    tags {
+    tags = {
         Name = "assetsStage"
     }
     versioning {
