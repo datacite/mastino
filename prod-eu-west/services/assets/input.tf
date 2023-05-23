b5e94306d6822a40c157c40c62a770b080e64a0b
Suzanne Vogt <suzanne.vogt@datacite.org>
2023-05-23 14:29:15 -0400

    Upgrade to terraform v12.

--- prod-eu-west/services/assets/input.tf
+++ prod-eu-west/services/assets/input.tf
@@ -2,6 +2,7 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
   region     = var.region
+  version    = "~> 2.70"
 }
 
 provider "aws" {
@@ -9,6 +10,7 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
   region = "us-east-1"
+  version    = "~> 2.70"
   alias = "use1"
 }
 
