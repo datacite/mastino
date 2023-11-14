a1edcf6836a29c8d5d3aaca3be387ff9a11a7e13
Richard Hallett <richard@richardhallett.com>
2023-11-14 15:17:51 +0100

    Move provider version to terraform.tf

--- global/dns/terraform.tf
+++ global/dns/terraform.tf
@@ -2,10 +2,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = "~> 2.70"
     }
   }
 
-  required_version = ">= 0.14"
+  required_version = ">= 1.14"
 
   backend "atlas" {
     name         = "datacite-ng/global-dns"
