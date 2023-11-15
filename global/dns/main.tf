4a0899e665d787ab4c3f2a864830de5b9edc5196
Richard Hallett <richard@richardhallett.com>
2023-11-15 12:27:38 +0100

    Move google dns entries to dns global

--- global/dns/main.tf
+++ global/dns/main.tf
@@ -168,3 +168,37 @@ resource "aws_route53_record" "corpus-prototype" {
     ttl = "300"
     records = ["54.229.227.84"]
 }
+
+// Google DataCite dns entries
+
+resource "aws_route53_record" "calendar" {
+   zone_id = aws_route53_zone.production.zone_id
+   name = "calendar.datacite.org"
+   type = "CNAME"
+   ttl = "86400"
+   records = ["ghs.googlehosted.com"]
+}
+
+resource "aws_route53_record" "drive" {
+   zone_id = aws_route53_zone.production.zone_id
+   name = "drive.datacite.org"
+   type = "CNAME"
+   ttl = "86400"
+   records = ["ghs.googlehosted.com"]
+}
+
+resource "aws_route53_record" "groups" {
+   zone_id = aws_route53_zone.production.zone_id
+   name = "groups.datacite.org"
+   type = "CNAME"
+   ttl = "86400"
+   records = ["ghs.googlehosted.com"]
+}
+
+resource "aws_route53_record" "mail" {
+   zone_id = aws_route53_zone.production.zone_id
+   name = "mail.datacite.org"
+   type = "CNAME"
+   ttl = "86400"
+   records = ["ghs.googlehosted.com"]
+}
