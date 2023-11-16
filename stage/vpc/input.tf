840fb17d502404ab0abffef0ce80b043c10ddb98
Richard Hallett <richard@richardhallett.com>
2023-11-16 13:59:27 +0100

    stage/vpc Upgrade terraform 0.14

--- stage/vpc/input.tf
+++ stage/vpc/input.tf
@@ -2,7 +2,6 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
   region     = var.region
-  version    = "~> 2.70"
 }
 
 data "aws_security_group" "datacite-public" {
--- stage/vpc/terraform.tf
+++ stage/vpc/terraform.tf
@@ -2,12 +2,13 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = "~> 2.7"
     }
     template = {
       source = "hashicorp/template"
     }
   }
-  required_version = ">= 0.13"
+  required_version = ">= 0.14"
 
   backend "atlas" {
     name = "datacite-ng/stage-vpc"
