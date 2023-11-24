d9bfcc0ab93ff8476f5df9d8aef76c7460f88653
Richard Hallett <richard@richardhallett.com>
2023-11-24 22:34:26 +0100

    Add lupo-testing queue back

--- dev/services/message-queue/main.tf
+++ dev/services/message-queue/main.tf
@@ -1,8 +1,20 @@
-
 module "datacite_queues" {
   source = "../../../modules/datacite_queues"
 
   environment = "development"
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+# This should not be defined here as it's used by test
+resource "aws_sqs_queue" "lupo-testing" {
+  name                      = "test_lupo"
+  redrive_policy            = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.dead-letter-dev.arn
+    maxReceiveCount     = 4
+  })
 
   tags = {
     Environment = "dev"
