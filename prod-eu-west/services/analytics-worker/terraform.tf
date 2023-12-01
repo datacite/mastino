9c6eab09ea45b206daf158cd6e353a27606c03b9
Richard Hallett <richard@richardhallett.com>
2023-12-01 13:07:46 +0100

    prod/analytics-worker Upgrade terraform 0.13

--- prod-eu-west/services/analytics-worker/terraform.tf
+++ prod-eu-west/services/analytics-worker/terraform.tf
@@ -1,5 +1,15 @@
 terraform {
-  required_version = ">= 0.12"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    template = {
+      source = "hashicorp/template"
+    }
+  }
+
+  required_version = ">= 0.13"
 
   backend "atlas" {
     name         = "datacite-ng/prod-eu-west-services-analytics-api"
