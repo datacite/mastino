d4286e6bfb267a1a2dd4945e0b9aeb363bdd19fc
Richard Hallett <richard@richardhallett.com>
2023-12-18 13:59:53 +0100

    stage/oai - 0.12 fixes

--- stage/services/oai/input.tf
+++ stage/services/oai/input.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
   version    = "v2.70.0"
 }
 
@@ -15,15 +15,15 @@ data "aws_route53_zone" "internal" {
 }
 
 data "aws_security_group" "datacite-private" {
-  id = "${var.security_group_id}"
+  id = var.security_group_id
 }
 
 data "aws_subnet" "datacite-private" {
-  id = "${var.subnet_datacite-private_id}"
+  id = var.subnet_datacite-private_id
 }
 
 data "aws_subnet" "datacite-alt" {
-  id = "${var.subnet_datacite-alt_id}"
+  id = var.subnet_datacite-alt_id
 }
 
 data "aws_ecs_cluster" "stage" {
@@ -39,11 +39,11 @@ data "aws_iam_role" "ecs_task_execution_role" {
 }
 
 data "aws_lb" "stage" {
-  name = "${var.lb_name}"
+  name = var.lb_name
 }
 
 data "aws_lb_listener" "stage" {
-  load_balancer_arn = "${data.aws_lb.stage.arn}"
+  load_balancer_arn = data.aws_lb.stage.arn
   port = 443
 }
 
@@ -52,19 +52,19 @@ data "aws_lb" "test" {
 }
 
 data "aws_lb_listener" "test" {
-  load_balancer_arn = "${data.aws_lb.stage.arn}"
+  load_balancer_arn = data.aws_lb.stage.arn
   port = 443
 }
 
 data "template_file" "oai_task" {
-  template = "${file("oai.json")}"
+  template = file("oai.json")
 
-  vars {
-    api_url            = "${var.api_url}"
-    api_password       = "${var.api_password}"
-    base_url           = "${var.base_url}"
-    public_key         = "${var.public_key}"
-    version            = "${var.viringo_tags["sha"]}"
-    sentry_dsn         = "${var.sentry_dsn}"
+  vars = {
+    api_url            = var.api_url
+    api_password       = var.api_password
+    base_url           = var.base_url
+    public_key         = var.public_key
+    version            = var.viringo_tags["sha"]
+    sentry_dsn         = var.sentry_dsn
   }
 }
