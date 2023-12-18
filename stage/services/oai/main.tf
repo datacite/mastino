90a289ce978341db33226f47d088d0913d81e706
Richard Hallett <richard@richardhallett.com>
2023-12-18 16:28:46 +0100

    oai/stage Switch to new ecs module service

--- stage/services/oai/main.tf
+++ stage/services/oai/main.tf
@@ -1,71 +1,109 @@
-resource "aws_ecs_service" "oai-stage" {
-  name = "oai-stage"
-  cluster = data.aws_ecs_cluster.stage.id
-  launch_type = "FARGATE"
-  task_definition = aws_ecs_task_definition.oai-stage.arn
-  desired_count = 1
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
 
-  network_configuration {
-    security_groups = [data.aws_security_group.datacite-private.id]
-    subnets         = [
-      data.aws_subnet.datacite-private.id,
-      data.aws_subnet.datacite-alt.id
-    ]
-  }
+data "aws_route53_zone" "production" {
+  name = "datacite.org"
+}
 
-  load_balancer {
-    target_group_arn = aws_lb_target_group.oai-stage.id
-    container_name   = "oai-stage"
-    container_port   = "80"
-  }
+data "aws_route53_zone" "internal" {
+  name = "datacite.org"
+  private_zone = true
+}
 
-  depends_on = [
-    data.aws_lb_listener.stage,
+data "aws_lb" "this" {
+  name = var.lb_name
+}
+
+locals {
+  app_name = "oai"
+  env = "stage"
+  service_name = "${local.app_name}-${local.env}"
+  awslogs_group = "/ecs/${local.service_name}"
+
+  oai_container_definition = jsonencode(
+    [{
+      "name": local.service_name
+      "image": "datacite/viringo",
+      "cpu": 512,
+      "memory": 1024,
+      "networkMode": "awsvpc",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 80,
+          "hostPort": 80
+        }
+      ],
+      "logConfiguration": {
+        "logDriver": "awslogs",
+        "options": {
+          "awslogs-group" : local.awslogs_group,
+          "awslogs-region": "eu-west-1",
+          "awslogs-stream-prefix": "ecs"
+        }
+      },
+      "environment" : [
+        {
+          "name" : "PUBLIC_KEY",
+          "value" : var.public_key
+        },
+        {
+          "name" : "DATACITE_API_URL",
+          "value" : var.api_url
+        },
+        {
+          "name" : "DATACITE_API_ADMIN_PASSWORD",
+          "value" : var.api_password
+        },
+        {
+          "name" : "LOG_LEVEL",
+          "value" : "INFO"
+        },
+        {
+          "name": "OAIPMH_BASE_URL",
+          "value": var.base_url
+        },
+        {
+          "name" : "SENTRY_DSN",
+          "value" : var.sentry_dsn
+        },
+        {
+          "name" : "GITHUB_VERSION",
+          "value" : var.viringo_tags["sha"]
+        }
+      ]
+    }]
+  )
+}
+
+module "ecs-service" {
+  source = "git@github.com:datacite/terraform-aws-ecs-service.git"
+
+  app_name = local.app_name
+  env = local.env
+  lb_name = var.lb_name
+  vpc_id = var.vpc_id
+  subnet_ids = [
+    var.subnet_datacite-private_id,
+    var.subnet_datacite-alt_id
   ]
-}
+  security_group_ids = [
+    var.security_group_id
+  ]
+  namespace_id = var.namespace_id
 
-resource "aws_cloudwatch_log_group" "oai-stage" {
-  name = "/ecs/oai-stage"
-}
+  desired_container_count = 1
+  use_fargate = true
+  fargate_cpu = 512
+  fargate_memory = 1024
 
-resource "aws_ecs_task_definition" "oai-stage" {
-  family = "oai-stage"
-  execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
-  network_mode = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu = "512"
-  memory = "1024"
+  container_definitions = local.oai_container_definition
 
-  container_definitions =  data.template_file.oai_task.rendered
-}
-
-resource "aws_lb_target_group" "oai-stage" {
-  name     = "oai-stage"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
-  target_type = "ip"
-  slow_start = 240
-
-  health_check {
-    path = "/heartbeat"
-  }
-}
-
-resource "aws_lb_listener_rule" "oai-stage" {
-  listener_arn = data.aws_lb_listener.stage.arn
-  priority     = 62
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.oai-stage.arn
-  }
-
-  condition {
-    host_header {
-      values = [aws_route53_record.oai-stage.name]
-    }
-  }
+  setup_alb = true
+  dns_record_name = "oai.stage.datacite.org"
 }
 
 resource "aws_route53_record" "oai-stage" {
@@ -73,22 +111,5 @@ resource "aws_route53_record" "oai-stage" {
     name = "oai.stage.datacite.org"
     type = "CNAME"
     ttl = var.ttl
-    records = [data.aws_lb.stage.dns_name]
-}
-
-resource "aws_service_discovery_service" "oai-stage" {
-  name = "oai.test"
-
-  health_check_custom_config {
-    failure_threshold = 3
-  }
-
-  dns_config {
-    namespace_id = var.namespace_id
-
-    dns_records {
-      ttl = 300
-      type = "A"
-    }
-  }
+    records = [data.aws_lb.this.dns_name]
 }
