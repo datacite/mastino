13aa65ce1b396ac9e4a2e20ad06dd8e9e28e5adf
Richard Hallett <richard@richardhallett.com>
2023-12-18 16:05:37 +0100

    oai/stage remove test lb

--- stage/services/oai/main.tf
+++ stage/services/oai/main.tf
@@ -52,19 +52,6 @@ resource "aws_lb_target_group" "oai-stage" {
   }
 }
 
-resource "aws_lb_target_group" "oai-test" {
-  name     = "oai-test"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
-  target_type = "ip"
-  slow_start = 240
-
-  health_check {
-    path = "/heartbeat"
-  }
-}
-
 resource "aws_lb_listener_rule" "oai-stage" {
   listener_arn = data.aws_lb_listener.stage.arn
   priority     = 62
@@ -87,14 +74,6 @@ resource "aws_route53_record" "oai-stage" {
     type = "CNAME"
     ttl = var.ttl
     records = [data.aws_lb.stage.dns_name]
-}
-
-resource "aws_route53_record" "oai-test" {
-    zone_id = data.aws_route53_zone.production.zone_id
-    name = "oai.test.datacite.org"
-    type = "CNAME"
-    ttl = var.ttl
-    records = [data.aws_lb.test.dns_name]
 }
 
 resource "aws_service_discovery_service" "oai-stage" {
