e5a41199761ecdcd0601cd91c625c8d141b685de
Richard Hallett <richard@richardhallett.com>
2024-01-11 10:56:23 +0100

    prod/datadog Upgrade to tf 0.12

--- prod-eu-west/services/datadog/input.tf
+++ prod-eu-west/services/datadog/input.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
 }
 
 data "aws_ecs_cluster" "default" {
@@ -21,21 +21,21 @@ data "aws_iam_role" "ecs_task_execution_role" {
 }
 
 data "aws_security_group" "datacite-private" {
-  id = "${var.security_group_id}"
+  id = var.security_group_id
 }
 
 data "aws_subnet" "datacite-private" {
-  id = "${var.subnet_datacite-private_id}"
+  id = var.subnet_datacite-private_id
 }
 
 data "aws_subnet" "datacite-alt" {
-  id = "${var.subnet_datacite-alt_id}"
+  id = var.subnet_datacite-alt_id
 }
 
 data "template_file" "datadog_task" {
-  template = "${file("datadog.json")}"
+  template = file("datadog.json")
 
-  vars {
-    dd_api_key         = "${var.dd_api_key}"
+  vars = {
+    dd_api_key         = var.dd_api_key
   }
 }
--- prod-eu-west/services/datadog/main.tf
+++ prod-eu-west/services/datadog/main.tf
@@ -1,20 +1,20 @@
 resource "aws_ecs_service" "datadog" {
   name = "datadog"
-  cluster = "${data.aws_ecs_cluster.default.id}"
+  cluster = data.aws_ecs_cluster.default.id
   launch_type = "FARGATE"
-  task_definition = "${aws_ecs_task_definition.datadog.arn}"
+  task_definition = aws_ecs_task_definition.datadog.arn
   desired_count = 1
 
   network_configuration {
-    security_groups = ["${data.aws_security_group.datacite-private.id}"]
+    security_groups = [data.aws_security_group.datacite-private.id]
     subnets         = [
-      "${data.aws_subnet.datacite-private.id}",
-      "${data.aws_subnet.datacite-alt.id}"
+      data.aws_subnet.datacite-private.id,
+      data.aws_subnet.datacite-alt.id
     ]
   }
 
   service_registries {
-    registry_arn = "${aws_service_discovery_service.datadog.arn}"
+    registry_arn = aws_service_discovery_service.datadog.arn
   }
 }
 
@@ -24,13 +24,13 @@ resource "aws_cloudwatch_log_group" "datadog" {
 
 resource "aws_ecs_task_definition" "datadog" {
   family = "datadog"
-  execution_role_arn = "${data.aws_iam_role.ecs_task_execution_role.arn}"
+  execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
   network_mode = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu = "256"
   memory = "512"
 
-  container_definitions =  "${data.template_file.datadog_task.rendered}"
+  container_definitions =  data.template_file.datadog_task.rendered
 }
 
 resource "aws_service_discovery_service" "datadog" {
@@ -41,8 +41,8 @@ resource "aws_service_discovery_service" "datadog" {
   }
 
   dns_config {
-    namespace_id = "${var.namespace_id}"
-    
+    namespace_id = var.namespace_id
+
     dns_records {
       ttl = 300
       type = "A"
