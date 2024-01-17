089eed0961d3392cab29445a09eb92a4afc0b550
Richard Hallett <richard@richardhallett.com>
2024-01-17 14:12:34 +0100

    stage/assets Upgrade tf 0.13

--- stage/services/assets/input.tf
+++ stage/services/assets/input.tf
@@ -2,16 +2,6 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
   region     = var.region
-  version    = "~> 2.70"
-}
-
-provider "aws" {
-  # us-east-1 instance
-  access_key = var.access_key
-  secret_key = var.secret_key
-  region = "us-east-1"
-  version = "~> 2.70"
-  alias = "use1"
 }
 
 data "template_file" "assets-stage" {
