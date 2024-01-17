fdb890a871f8751bcc4f1e3afac9aec46f8e40cc
Richard Hallett <richard@richardhallett.com>
2024-01-17 15:15:19 +0100

    stage/assets Use us-east for acm

--- stage/services/assets/input.tf
+++ stage/services/assets/input.tf
@@ -2,7 +2,14 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
   region     = var.region
-  version    = "~> 2.70"
+}
+
+provider "aws" {
+  # us-east-1 instance
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region = "us-east-1"
+  alias = "use1"
 }
 
 data "template_file" "assets-stage" {
@@ -14,7 +21,7 @@ data "template_file" "assets-stage" {
 }
 
 data "aws_acm_certificate" "cloudfront-stage" {
-  provider = aws
+  provider = aws.use1
   domain = "*.stage.datacite.org"
   statuses = ["ISSUED"]
   most_recent = true
