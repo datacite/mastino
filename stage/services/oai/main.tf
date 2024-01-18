9b0c13906f7188349c21c4c2e748c3228d88773e
Richard Hallett <richard@richardhallett.com>
2024-01-18 17:25:57 +0100

    stage/oai Update old syntax

--- stage/services/oai/main.tf
+++ stage/services/oai/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
 }
 
 data "aws_route53_zone" "production" {
