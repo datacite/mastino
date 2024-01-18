4f46a06ff90088deeb1ad7dabe5864505f5d6067
Richard Hallett <richard@richardhallett.com>
2024-01-18 16:11:01 +0100

    stage/handle Upgrade tf 0.12

--- stage/services/handle/main.tf
+++ stage/services/handle/main.tf
@@ -1,20 +1,20 @@
 resource "aws_ecs_service" "handle-stage" {
   name            = "handle-stage"
-  cluster         = "${data.aws_ecs_cluster.stage.id}"
+  cluster         = data.aws_ecs_cluster.stage.id
   launch_type     = "FARGATE"
-  task_definition = "${aws_ecs_task_definition.handle-stage.arn}"
+  task_definition = aws_ecs_task_definition.handle-stage.arn
   desired_count   = 1
 
   network_configuration {
-    security_groups = ["${data.aws_security_group.datacite-private.id}"]
+    security_groups = [data.aws_security_group.datacite-private.id]
     subnets         = [
-      "${data.aws_subnet.datacite-private.id}",
-      "${data.aws_subnet.datacite-alt.id}"
+      data.aws_subnet.datacite-private.id,
+      data.aws_subnet.datacite-alt.id
     ]
   }
 
   load_balancer {
-    target_group_arn = "${aws_lb_target_group.handle-stage.id}"
+    target_group_arn = aws_lb_target_group.handle-stage.id
     container_name   = "handle-stage"
     container_port   = "8000"
   }
@@ -30,8 +30,8 @@ resource "aws_cloudwatch_log_group" "handle-stage" {
 
 resource "aws_ecs_task_definition" "handle-stage" {
   family                = "handle-stage"
-  execution_role_arn = "${data.aws_iam_role.ecs_task_execution_role.arn}"
-  container_definitions = "${data.template_file.handle_task.rendered}"
+  execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
+  container_definitions = data.template_file.handle_task.rendered
   network_mode = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu = "512"
@@ -40,7 +40,7 @@ resource "aws_ecs_task_definition" "handle-stage" {
 
 resource "aws_lb_target_group" "handle-stage" {
   name     = "handle-stage"
-  vpc_id   = "${var.vpc_id}"
+  vpc_id   = var.vpc_id
   port     = 8000
   protocol = "HTTPS"
   target_type = "ip"
@@ -51,32 +51,33 @@ resource "aws_lb_target_group" "handle-stage" {
 }
 
 resource "aws_lb_listener_rule" "handle-stage" {
-  listener_arn = "${data.aws_lb_listener.stage.arn}"
+  listener_arn = data.aws_lb_listener.stage.arn
   priority     = 3
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.handle-stage.arn}"
+    target_group_arn = aws_lb_target_group.handle-stage.arn
   }
 
   condition {
-    field  = "host-header"
-    values = ["${aws_route53_record.handle-stage.name}"]
+    host_header {
+      values = [aws_route53_record.handle-stage.name]
+    }
   }
 }
 
 resource "aws_route53_record" "handle-stage" {
-   zone_id = "${data.aws_route53_zone.production.zone_id}"
+   zone_id = data.aws_route53_zone.production.zone_id
    name = "handle.stage.datacite.org"
    type = "CNAME"
-   ttl = "${var.ttl}"
-   records = ["${data.aws_lb.stage.dns_name}"]
+   ttl = var.ttl
+   records = [data.aws_lb.stage.dns_name]
 }
 
 resource "aws_route53_record" "split-handle-stage" {
-   zone_id = "${data.aws_route53_zone.internal.zone_id}"
+   zone_id = data.aws_route53_zone.internal.zone_id
    name = "handle.stage.datacite.org"
    type = "CNAME"
-   ttl = "${var.ttl}"
-   records = ["${data.aws_lb.stage.dns_name}"]
+   ttl = var.ttl
+   records = [data.aws_lb.stage.dns_name]
 }
--- stage/services/handle/terraform.tf
+++ stage/services/handle/terraform.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.11"
+  required_version = ">= 0.12"
 
   backend "atlas" {
     name         = "datacite-ng/stage-services-handle"
