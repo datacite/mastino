c053ebb57cbe0228a1195ac49bcfd8739584d444
Richard Hallett <richard@richardhallett.com>
2024-01-18 16:55:21 +0100

    stage/mds Upgrade tf 0.13

--- stage/services/mds/main.tf
+++ stage/services/mds/main.tf
@@ -60,7 +60,7 @@ resource "aws_route53_record" "split-mds-stage" {
    zone_id = data.aws_route53_zone.internal.zone_id
    name = "mds.stage.datacite.org"
    type = "CNAME"
-   ttl = "${var.ttl}"
+   ttl = var.ttl
    records = [data.aws_lb.stage.dns_name]
 }
 
@@ -86,87 +86,8 @@ resource "aws_lb_listener_rule" "mds-stage" {
   }
 
   condition {
-    field  = "host-header"
-    values = [aws_route53_record.mds-stage.name]
+    host_header {
+      values = [aws_route53_record.mds-stage.name]
+    }
   }
 }
-
-// resource "aws_lb_listener_rule" "mds-stage-doi" {
-//   listener_arn = "${data.aws_lb_listener.stage.arn}"
-//   priority     = 2
-
-//   action {
-//     type             = "forward"
-//     target_group_arn = "${aws_lb_target_group.mds-stage.arn}"
-//   }
-
-//   condition {
-//     field  = "host-header"
-//     values = ["${aws_route53_record.mds-stage.name}"]
-//   }
-
-//   condition {
-//     field  = "path-pattern"
-//     values = ["/doi*"]
-//   }
-// }
-
-// resource "aws_lb_listener_rule" "mds-stage-metadata" {
-//   listener_arn = "${data.aws_lb_listener.stage.arn}"
-//   priority     = 5
-
-//   action {
-//     type             = "forward"
-//     target_group_arn = "${aws_lb_target_group.mds-stage.arn}"
-//   }
-
-//   condition {
-//     field  = "host-header"
-//     values = ["${aws_route53_record.mds-stage.name}"]
-//   }
-
-//   condition {
-//     field  = "path-pattern"
-//     values = ["/metadata*"]
-//   }
-// }
-
-// resource "aws_lb_listener_rule" "mds-stage-media" {
-//   listener_arn = "${data.aws_lb_listener.stage.arn}"
-//   priority     = 6
-
-//   action {
-//     type             = "forward"
-//     target_group_arn = "${aws_lb_target_group.mds-stage.arn}"
-//   }
-
-//   condition {
-//     field  = "host-header"
-//     values = ["${aws_route53_record.mds-stage.name}"]
-//   }
-
-//   condition {
-//     field  = "path-pattern"
-//     values = ["/media*"]
-//   }
-// }
-
-// resource "aws_lb_listener_rule" "mds-stage-heartbeat" {
-//   listener_arn = "${data.aws_lb_listener.stage.arn}"
-//   priority     = 7
-
-//   action {
-//     type             = "forward"
-//     target_group_arn = "${aws_lb_target_group.mds-stage.arn}"
-//   }
-
-//   condition {
-//     field  = "host-header"
-//     values = ["${aws_route53_record.mds-stage.name}"]
-//   }
-
-//   condition {
-//     field  = "path-pattern"
-//     values = ["/heartbeat"]
-//   }
-// }
