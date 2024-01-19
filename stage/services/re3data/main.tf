e0cfa99156274050e4ef9d9f50c75e94ebcec325
Richard Hallett <richard@richardhallett.com>
2024-01-19 11:33:37 +0100

    stage/re3data Upgrade tf 0.12

--- stage/services/re3data/main.tf
+++ stage/services/re3data/main.tf
@@ -1,20 +1,20 @@
 resource "aws_ecs_service" "re3data-stage" {
   name = "re3data-stage"
-  cluster = "${data.aws_ecs_cluster.stage.id}"
+  cluster = data.aws_ecs_cluster.stage.id
   launch_type = "FARGATE"
-  task_definition = "${aws_ecs_task_definition.re3data-stage.arn}"
+  task_definition = aws_ecs_task_definition.re3data-stage.arn
   desired_count = 1
 
   network_configuration {
-    security_groups = ["${data.aws_security_group.datacite-private.id}"]
+    security_groups = [data.aws_security_group.datacite-private.id]
     subnets         = [
-      "${data.aws_subnet.datacite-private.id}",
-      "${data.aws_subnet.datacite-alt.id}"
+      data.aws_subnet.datacite-private.id,
+      data.aws_subnet.datacite-alt.id
     ]
   }
 
   load_balancer {
-    target_group_arn = "${aws_lb_target_group.re3data-stage.id}"
+    target_group_arn = aws_lb_target_group.re3data-stage.id
     container_name   = "re3data-stage"
     container_port   = "80"
   }
@@ -26,20 +26,20 @@ resource "aws_cloudwatch_log_group" "re3data-stage" {
 
 resource "aws_ecs_task_definition" "re3data-stage" {
   family = "re3data-stage"
-  execution_role_arn = "${data.aws_iam_role.ecs_task_execution_role.arn}",
+  execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
   network_mode = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu = "512"
   memory = "2048"
 
-  container_definitions =  "${data.template_file.re3data_task.rendered}"
+  container_definitions =  data.template_file.re3data_task.rendered
 }
 
 resource "aws_lb_target_group" "re3data-stage" {
   name     = "re3data-stage"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = "${var.vpc_id}"
+  vpc_id   = var.vpc_id
   target_type = "ip"
 
   health_check {
@@ -48,21 +48,23 @@ resource "aws_lb_target_group" "re3data-stage" {
 }
 
 resource "aws_lb_listener_rule" "re3data-stage" {
-  listener_arn = "${data.aws_lb_listener.stage.arn}"
+  listener_arn = data.aws_lb_listener.stage.arn
   priority     = 32
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.re3data-stage.arn}"
+    target_group_arn = aws_lb_target_group.re3data-stage.arn
   }
 
   condition {
-    field  = "host-header"
-    values = ["api.stage.datacite.org"]
+    host_header {
+      values = ["api.stage.datacite.org"]
+    }
   }
 
   condition {
-    field  = "path-pattern"
-    values = ["/re3data*"]
+    path_pattern {
+      values = ["/re3data*"]
+    }
   }
 }
--- stage/services/re3data/var.tf
+++ stage/services/re3data/var.tf
@@ -7,7 +7,7 @@ variable "region" {
 variable "vpc_id" {}
 
 variable "schnauzer_tags" {
-  type = "map"
+  type = map(string)
 }
 
 variable "lb_name" {
