b97c119a5dc95fe5d2910efe568c7ab8b62fb365
Richard Hallett <richard@richardhallett.com>
2024-01-22 09:23:24 +0100

    prod/client-api Increase min containers

--- prod-eu-west/services/client-api/main.tf
+++ prod-eu-west/services/client-api/main.tf
@@ -35,8 +35,8 @@ resource "aws_ecs_service" "client-api" {
 }
 
 resource "aws_appautoscaling_target" "client-api" {
-  max_capacity       = 14
-  min_capacity       = 12
+  max_capacity       = 18
+  min_capacity       = 16
   resource_id        = "service/default/${aws_ecs_service.client-api.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
