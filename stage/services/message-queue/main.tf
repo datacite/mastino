01e79a36494942cb5bf26f9dbfeaad651b447356
Richard Hallett <richard@richardhallett.com>
2024-01-24 16:55:23 +0100

    stage/message-queue Move to shared module

--- stage/services/message-queue/main.tf
+++ stage/services/message-queue/main.tf
@@ -1,144 +1,86 @@
-resource "aws_sqs_queue" "doi-stage" {
-  name                      = "stage_doi"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-stage.arn}\",\"maxReceiveCount\":4}"
+module "datacite_queues" {
+  source = "../../../modules/datacite_queues"
+
+  environment = "stage"
 
   tags = {
     Environment = "stage"
   }
 }
 
-resource "aws_sqs_queue" "event-stage" {
-  name                      = "stage_event"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-stage.arn}\",\"maxReceiveCount\":4}"
-
-  tags = {
-    Environment = "stage"
-  }
+moved {
+  from = aws_sqs_queue.doi-stage
+  to   = module.datacite_queues.aws_sqs_queue.doi
 }
 
-resource "aws_sqs_queue" "volpino-stage" {
-  name                      = "stage_volpino"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-stage.arn}\",\"maxReceiveCount\":4}"
-  visibility_timeout_seconds = 120
-
-  tags = {
-    Environment = "stage"
-  }
+moved {
+  from = aws_sqs_queue.event-stage
+  to   = module.datacite_queues.aws_sqs_queue.event
 }
 
-resource "aws_sqs_queue" "lupo-stage" {
-  name                      = "stage_lupo"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-stage.arn}\",\"maxReceiveCount\":4}"
-  visibility_timeout_seconds = 120
-
-  tags = {
-    Environment = "stage"
-  }
+moved {
+  from = aws_sqs_queue.volpino-stage
+  to   = module.datacite_queues.aws_sqs_queue.volpino
 }
 
-resource "aws_sqs_queue" "lupo-background-stage" {
-  name                      = "stage_lupo_background"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-stage.arn}\",\"maxReceiveCount\":4}"
-  visibility_timeout_seconds = 3600
-
-  tags = {
-    Environment = "stage"
-  }
+moved {
+  from = aws_sqs_queue.lupo-stage
+  to   = module.datacite_queues.aws_sqs_queue.lupo
 }
 
-resource "aws_sqs_queue" "lupo-import-stage" {
-  name                      = "stage_lupo_import"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-stage.arn}\",\"maxReceiveCount\":4}"
-  visibility_timeout_seconds = 3600
-
-  tags = {
-    Environment = "stage"
-  }
+moved {
+  from = aws_sqs_queue.lupo-background-stage
+  to   = module.datacite_queues.aws_sqs_queue.lupo_background
 }
 
-resource "aws_sqs_queue" "lupo-import-other-doi-stage" {
-  name                      = "stage_lupo_import_other_doi"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-stage.arn}\",\"maxReceiveCount\":4}"
-  visibility_timeout_seconds = 3600
-
-  tags = {
-    Environment = "stage"
-  }
+moved {
+  from = aws_sqs_queue.lupo-import-stage
+  to   = module.datacite_queues.aws_sqs_queue.lupo_import
 }
 
-resource "aws_sqs_queue" "lupo-transfer-stage" {
-  name                      = "stage_lupo_transfer"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-stage.arn}\",\"maxReceiveCount\":4}"
-  visibility_timeout_seconds = 3600
-
-  tags = {
-    Environment = "stage"
-  }
+moved {
+  from = aws_sqs_queue.lupo-import-other-doi-stage
+  to   = module.datacite_queues.aws_sqs_queue.lupo_import_other_doi
 }
 
-resource "aws_sqs_queue" "levriero-stage" {
-  name                      = "stage_levriero"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-stage.arn}\",\"maxReceiveCount\":4}"
-
-  tags = {
-    Environment = "stage"
-  }
+moved {
+  from = aws_sqs_queue.lupo-transfer-stage
+  to   = module.datacite_queues.aws_sqs_queue.lupo_transfer
 }
 
-resource "aws_sqs_queue" "levriero-usage-stage" {
-  name                      = "stage_levriero_usage"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-stage.arn}\",\"maxReceiveCount\":4}"
-
-  tags = {
-    Environment = "stage"
-  }
+moved {
+  from = aws_sqs_queue.levriero-stage
+  to   = module.datacite_queues.aws_sqs_queue.levriero
 }
 
-
-resource "aws_sqs_queue" "usage-stage" {
-  name                      = "stage_usage"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-stage.arn}\",\"maxReceiveCount\":4}"
-
-  tags = {
-    Environment = "stage"
-  }
+moved {
+  from = aws_sqs_queue.levriero-usage-stage
+  to   = module.datacite_queues.aws_sqs_queue.levriero_usage
 }
 
-resource "aws_sqs_queue" "sashimi-stage" {
-  name                      = "stage_sashimi"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-stage.arn}\",\"maxReceiveCount\":4}"
-
-  tags = {
-    Environment = "stage"
-  }
+moved {
+  from = aws_sqs_queue.usage-stage
+  to   = module.datacite_queues.aws_sqs_queue.usage
 }
 
-resource "aws_sqs_queue" "salesforce-stage" {
-  name                      = "stage_salesforce"
-  visibility_timeout_seconds = 60
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-stage.arn}\",\"maxReceiveCount\":4}"
-
-  tags = {
-    Environment = "stage"
-  }
+moved {
+  from = aws_sqs_queue.sashimi-stage
+  to   = module.datacite_queues.aws_sqs_queue.sashimi
 }
 
-resource "aws_sqs_queue" "analytics" {
-  name                      = "stage_analytics"
-  visibility_timeout_seconds = 60
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-stage.arn}\",\"maxReceiveCount\":4}"
-
-  tags = {
-    Environment = "stage"
-  }
+moved {
+  from = aws_sqs_queue.salesforce-stage
+  to   = module.datacite_queues.aws_sqs_queue.salesforce
 }
 
-resource "aws_sqs_queue" "dead-letter-stage" {
-  name                      = "stage_dead-letter"
+moved {
+  from = aws_sqs_queue.analytics
+  to   = module.datacite_queues.aws_sqs_queue.analytics
+}
 
-  tags = {
-    Environment = "stage"
-  }
+moved {
+  from = aws_sqs_queue.dead-letter-stage
+  to   = module.datacite_queues.aws_sqs_queue.dead-letter
 }
 
 resource "aws_iam_policy" "sqs-stage" {
