a64009dce6c9b7c863bc8e9584c2b919d1a62b88
Richard Hallett <richard@richardhallett.com>
2024-01-24 17:09:46 +0100

    test/message-queue Move to use shared module

--- test/services/message-queue/main.tf
+++ test/services/message-queue/main.tf
@@ -1,132 +1,81 @@
-resource "aws_sqs_queue" "doi-test" {
-  name                      = "test_doi"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-test.arn}\",\"maxReceiveCount\":4}"
+module "datacite_queues" {
+  source = "../../../modules/datacite_queues"
+
+  environment = "test"
 
   tags = {
     Environment = "test"
   }
 }
 
-resource "aws_sqs_queue" "event-test" {
-  name                      = "test_event"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-test.arn}\",\"maxReceiveCount\":4}"
-
-  tags = {
-    Environment = "test"
-  }
+moved {
+  from = aws_sqs_queue.doi-test
+  to   = module.datacite_queues.aws_sqs_queue.doi
 }
 
-resource "aws_sqs_queue" "volpino-test" {
-  name                      = "test_volpino"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-test.arn}\",\"maxReceiveCount\":4}"
-  visibility_timeout_seconds = 120
-
-  tags = {
-    Environment = "test"
-  }
+moved {
+  from = aws_sqs_queue.event-test
+  to   = module.datacite_queues.aws_sqs_queue.event
 }
 
-resource "aws_sqs_queue" "lupo-test" {
-  name                      = "test_lupo"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-test.arn}\",\"maxReceiveCount\":4}"
-  visibility_timeout_seconds = 120
-
-  tags = {
-    Environment = "test"
-  }
+moved {
+  from = aws_sqs_queue.volpino-test
+  to   = module.datacite_queues.aws_sqs_queue.volpino
 }
 
-resource "aws_sqs_queue" "lupo-background-test" {
-  name                      = "test_lupo_background"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-test.arn}\",\"maxReceiveCount\":4}"
-  visibility_timeout_seconds = 3600
-
-  tags = {
-    Environment = "test"
-  }
+moved {
+  from = aws_sqs_queue.lupo-test
+  to   = module.datacite_queues.aws_sqs_queue.lupo
 }
 
-resource "aws_sqs_queue" "lupo-import-test" {
-  name                      = "test_lupo_import"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-test.arn}\",\"maxReceiveCount\":4}"
-  visibility_timeout_seconds = 3600
-
-  tags = {
-    Environment = "test"
-  }
+moved {
+  from = aws_sqs_queue.lupo-background-test
+  to   = module.datacite_queues.aws_sqs_queue.lupo_background
 }
 
-resource "aws_sqs_queue" "lupo-import-other-doi-test" {
-  name                      = "test_lupo_import_other_doi"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-test.arn}\",\"maxReceiveCount\":4}"
-  visibility_timeout_seconds = 3600
-
-  tags = {
-    Environment = "test"
-  }
+moved {
+  from = aws_sqs_queue.lupo-import-test
+  to   = module.datacite_queues.aws_sqs_queue.lupo_import
 }
 
-resource "aws_sqs_queue" "lupo-transfer-test" {
-  name                      = "test_lupo_transfer"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-test.arn}\",\"maxReceiveCount\":4}"
-  visibility_timeout_seconds = 3600
-
-  tags = {
-    Environment = "test"
-  }
+moved {
+  from = aws_sqs_queue.lupo-import-other-doi-test
+  to   = module.datacite_queues.aws_sqs_queue.lupo_import_other_doi
 }
 
-resource "aws_sqs_queue" "levriero-test" {
-  name                      = "test_levriero"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-test.arn}\",\"maxReceiveCount\":4}"
-
-  tags = {
-    Environment = "test"
-  }
+moved {
+  from = aws_sqs_queue.lupo-transfer-test
+  to   = module.datacite_queues.aws_sqs_queue.lupo_transfer
 }
 
-resource "aws_sqs_queue" "levriero-usage-test" {
-  name                      = "test_levriero_usage"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-test.arn}\",\"maxReceiveCount\":4}"
-
-  tags = {
-    Environment = "test"
-  }
+moved {
+  from = aws_sqs_queue.levriero-test
+  to   = module.datacite_queues.aws_sqs_queue.levriero
 }
 
-
-resource "aws_sqs_queue" "usage-test" {
-  name                      = "test_usage"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-test.arn}\",\"maxReceiveCount\":4}"
-
-  tags = {
-    Environment = "test"
-  }
+moved {
+  from = aws_sqs_queue.levriero-usage-test
+  to   = module.datacite_queues.aws_sqs_queue.levriero_usage
 }
 
-resource "aws_sqs_queue" "sashimi-test" {
-  name                      = "test_sashimi"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-test.arn}\",\"maxReceiveCount\":4}"
-
-  tags = {
-    Environment = "test"
-  }
+moved {
+  from = aws_sqs_queue.usage-test
+  to   = module.datacite_queues.aws_sqs_queue.usage
 }
 
-resource "aws_sqs_queue" "analytics" {
-  name                      = "test_analytics"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.dead-letter-test.arn}\",\"maxReceiveCount\":4}"
-  tags = {
-    Environment = "test"
-  }
+moved {
+  from = aws_sqs_queue.sashimi-test
+  to   = module.datacite_queues.aws_sqs_queue.sashimi
 }
 
-resource "aws_sqs_queue" "dead-letter-test" {
-  name                      = "test_dead-letter"
+moved {
+  from = aws_sqs_queue.analytics
+  to   = module.datacite_queues.aws_sqs_queue.analytics
+}
 
-  tags = {
-    Environment = "test"
-  }
+moved {
+  from = aws_sqs_queue.dead-letter-test
+  to   = module.datacite_queues.aws_sqs_queue.dead-letter
 }
 
 resource "aws_iam_policy" "sqs-test" {
