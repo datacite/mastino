d8ed46f0c81505fbf24587aa0e646d7d97827834
Richard Hallett <richard@richardhallett.com>
2024-01-24 11:21:44 +0100

    dev/message-queue
    Removing test lupo from here, should be defined in test/message-queue

--- dev/services/message-queue/main.tf
+++ dev/services/message-queue/main.tf
@@ -7,16 +7,3 @@ module "datacite_queues" {
     Environment = "dev"
   }
 }
-
-# This should not be defined here as it's used by test
-resource "aws_sqs_queue" "lupo-testing" {
-  name                      = "test_lupo"
-  redrive_policy            = jsonencode({
-    deadLetterTargetArn = module.datacite_queues.dead_letter_arn
-    maxReceiveCount     = 4
-  })
-
-  tags = {
-    Environment = "dev"
-  }
-}
