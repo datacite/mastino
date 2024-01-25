3df363fb8ec8b7e3d3ddfae71302085668091354
Richard Hallett <richard@richardhallett.com>
2024-01-25 15:12:36 +0100

    test/handle Upgrade tf 0.12

--- test/services/handle/input.tf
+++ test/services/handle/input.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
 }
 
 data "aws_route53_zone" "production" {
@@ -14,11 +14,11 @@ data "aws_route53_zone" "internal" {
 }
 
 data "aws_lb" "test" {
-  name = "${var.lb_name}"
+  name = var.lb_name
 }
 
 data "aws_lb_listener" "test" {
-  load_balancer_arn = "${data.aws_lb.test.arn}"
+  load_balancer_arn = data.aws_lb.test.arn
   port = 443
 }
 
@@ -27,28 +27,28 @@ data "aws_ecs_cluster" "test" {
 }
 
 data "aws_security_group" "datacite-private" {
-  id = "${var.security_group_id}"
+  id = var.security_group_id
 }
 
 data "aws_subnet" "datacite-private" {
-  id = "${var.subnet_datacite-private_id}"
+  id = var.subnet_datacite-private_id
 }
 
 data "aws_subnet" "datacite-alt" {
-  id = "${var.subnet_datacite-alt_id}"
+  id = var.subnet_datacite-alt_id
 }
 data "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
 }
 
 data "template_file" "handle_task" {
-  template = "${file("handle.json")}"
+  template = file("handle.json")
 
-  vars {
-    mysql_host = "${var.mysql_host}"
-    mysql_user = "${var.mysql_user}"
-    mysql_password = "${var.mysql_password}"
-    handle_svr_private_key = "${var.handle_svr_private_key}"
-    handle_svr_public_key = "${var.handle_svr_public_key}"
+  vars = {
+    mysql_host = var.mysql_host
+    mysql_user = var.mysql_user
+    mysql_password = var.mysql_password
+    handle_svr_private_key = var.handle_svr_private_key
+    handle_svr_public_key = var.handle_svr_public_key
   }
 }
