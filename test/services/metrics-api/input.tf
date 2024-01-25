5d8b1990f9b48307003d640deb193405451c3699
Richard Hallett <richard@richardhallett.com>
2024-01-25 15:39:33 +0100

    test/metrics-api Upgrade tf 0.13

--- test/services/metrics-api/input.tf
+++ test/services/metrics-api/input.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
 }
 
 data "aws_route53_zone" "production" {
@@ -9,15 +9,15 @@ data "aws_route53_zone" "production" {
 }
 
 data "aws_security_group" "datacite-private" {
-  id = "${var.security_group_id}"
+  id = var.security_group_id
 }
 
 data "aws_subnet" "datacite-private" {
-  id = "${var.subnet_datacite-private_id}"
+  id = var.subnet_datacite-private_id
 }
 
 data "aws_subnet" "datacite-alt" {
-  id = "${var.subnet_datacite-alt_id}"
+  id = var.subnet_datacite-alt_id
 }
 
 data "aws_route53_zone" "internal" {
@@ -38,42 +38,42 @@ data "aws_iam_role" "ecs_task_execution_role" {
 }
 
 data "aws_lb" "test" {
-  name = "${var.lb_name}"
+  name = var.lb_name
 }
 
 data "aws_lb_listener" "test" {
-  load_balancer_arn = "${data.aws_lb.test.arn}"
+  load_balancer_arn = data.aws_lb.test.arn
   port = 443
 }
 
 data "template_file" "metrics-api_task" {
-  template = "${file("metrics-api.json")}"
+  template = file("metrics-api.json")
 
   vars = {
-    public_key         = "${var.public_key}"
-    jwt_public_key     = "${var.jwt_public_key}"
-    jwt_private_key    = "${var.jwt_private_key}"
-    memcache_servers   = "${var.memcache_servers}"
-    access_key         = "${var.access_key}"
-    secret_key         = "${var.secret_key}"
-    region             = "${var.region}"
-    s3_bucket          = "${var.s3_bucket}"
-    mysql_database     = "${var.mysql_database}"
-    mysql_user         = "${var.mysql_user}"
-    mysql_password     = "${var.mysql_password}"
-    mysql_host         = "${var.mysql_host}"
-    usage_url          = "${var.usage_url}"
-    sentry_dsn         = "${var.sentry_dsn}"
-    jwt_blacklisted    = "${var.jwt_blacklisted}"
-    rack_timeout_service_timeout = "${var.rack_timeout_service_timeout}"
-    version            = "${var.sashimi_tags["version"]}"
+    public_key         = var.public_key
+    jwt_public_key     = var.jwt_public_key
+    jwt_private_key    = var.jwt_private_key
+    memcache_servers   = var.memcache_servers
+    access_key         = var.access_key
+    secret_key         = var.secret_key
+    region             = var.region
+    s3_bucket          = var.s3_bucket
+    mysql_database     = var.mysql_database
+    mysql_user         = var.mysql_user
+    mysql_password     = var.mysql_password
+    mysql_host         = var.mysql_host
+    usage_url          = var.usage_url
+    sentry_dsn         = var.sentry_dsn
+    jwt_blacklisted    = var.jwt_blacklisted
+    rack_timeout_service_timeout = var.rack_timeout_service_timeout
+    version            = var.sashimi_tags["version"]
   }
 }
 
 data "template_file" "metrics-api_s3" {
-  template = "${file("s3_public_read.json")}"
+  template = file("s3_public_read.json")
 
   vars = {
-      bucket_name = "${var.s3_bucket}"
+      bucket_name = var.s3_bucket
   }
 }
--- test/services/metrics-api/main.tf
+++ test/services/metrics-api/main.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "metrics" {
-    bucket = "${var.s3_bucket}"
+    bucket = var.s3_bucket
     acl = "public-read"
-    policy = "${data.template_file.metrics-api_s3.rendered}"
+    policy = data.template_file.metrics-api_s3.rendered
     tags = {
         Name = "metricsApiTest"
     }
@@ -9,33 +9,33 @@ resource "aws_s3_bucket" "metrics" {
 
 resource "aws_ecs_service" "metrics-api-test" {
   name            = "metrics-api-test"
-  cluster         = "${data.aws_ecs_cluster.test.id}"
-  task_definition = "${aws_ecs_task_definition.metrics-api-test.arn}"
+  cluster         = data.aws_ecs_cluster.test.id
+  task_definition = aws_ecs_task_definition.metrics-api-test.arn
   desired_count   = 1
   launch_type = "FARGATE"
 
 
   network_configuration {
-    security_groups = ["${data.aws_security_group.datacite-private.id}"]
+    security_groups = [data.aws_security_group.datacite-private.id]
     subnets         = [
-      "${data.aws_subnet.datacite-private.id}",
-      "${data.aws_subnet.datacite-alt.id}"
+      data.aws_subnet.datacite-private.id,
+      data.aws_subnet.datacite-alt.id
     ]
   }
 
 
   load_balancer {
-    target_group_arn = "${aws_lb_target_group.metrics-api-test.id}"
+    target_group_arn = aws_lb_target_group.metrics-api-test.id
     container_name   = "metrics-api-test"
     container_port   = "80"
   }
 
   service_registries {
-    registry_arn = "${aws_service_discovery_service.metrics-api-test.arn}"
+    registry_arn = aws_service_discovery_service.metrics-api-test.arn
   }
 
    depends_on = [
-     "data.aws_lb_listener.test"
+     data.aws_lb_listener.test
   ]
 }
 
@@ -44,7 +44,7 @@ resource "aws_lb_target_group" "metrics-api-test" {
   port     = 80
   protocol = "HTTP"
   target_type = "ip"
-  vpc_id   = "${var.vpc_id}"
+  vpc_id   = var.vpc_id
 
   health_check {
     path = "/heartbeat"
@@ -57,21 +57,21 @@ resource "aws_cloudwatch_log_group" "metrics-api-test" {
 
 resource "aws_ecs_task_definition" "metrics-api-test" {
   family = "metrics-api-test"
-  execution_role_arn = "${data.aws_iam_role.ecs_task_execution_role.arn}"
+  execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
   network_mode = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu = "512"
   memory = "2048"
-  container_definitions =  "${data.template_file.metrics-api_task.rendered}"
+  container_definitions =  data.template_file.metrics-api_task.rendered
 }
 
 resource "aws_lb_listener_rule" "metrics-api-test" {
-  listener_arn = "${data.aws_lb_listener.test.arn}"
+  listener_arn = data.aws_lb_listener.test.arn
   priority     = 29
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.metrics-api-test.arn}"
+    target_group_arn = aws_lb_target_group.metrics-api-test.arn
   }
 
   condition {
@@ -79,7 +79,7 @@ resource "aws_lb_listener_rule" "metrics-api-test" {
       values = ["api.test.datacite.org"]
     }
   }
-  
+
   condition {
     path_pattern {
       values = ["/reports*"]
@@ -88,20 +88,20 @@ resource "aws_lb_listener_rule" "metrics-api-test" {
 }
 
 resource "aws_lb_listener_rule" "metrics-api-test-subset" {
-  listener_arn = "${data.aws_lb_listener.test.arn}"
+  listener_arn = data.aws_lb_listener.test.arn
   priority     = 28
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.metrics-api-test.arn}"
+    target_group_arn = aws_lb_target_group.metrics-api-test.arn
   }
-  
+
   condition {
     host_header {
       values = ["api.test.datacite.org"]
     }
   }
-  
+
   condition {
     path_pattern {
       values = ["/report-subsets*"]
@@ -110,12 +110,12 @@ resource "aws_lb_listener_rule" "metrics-api-test-subset" {
 }
 
 resource "aws_lb_listener_rule" "metrics-api-test-repositories" {
-  listener_arn = "${data.aws_lb_listener.test.arn}"
+  listener_arn = data.aws_lb_listener.test.arn
   priority     = 31
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.metrics-api-test.arn}"
+    target_group_arn = aws_lb_target_group.metrics-api-test.arn
   }
 
   condition {
@@ -139,7 +139,7 @@ resource "aws_service_discovery_service" "metrics-api-test" {
   }
 
    dns_config {
-    namespace_id = "${var.namespace_id}"
+    namespace_id = var.namespace_id
 
      dns_records {
       ttl = 300
