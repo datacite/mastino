450826871085a230adde450b0ba8e1558893de21
Richard Hallett <richard@richardhallett.com>
2024-02-29 17:47:17 +0100

    prod/datafiles-generator Disable datafiles generator

--- prod-eu-west/services/datafiles-generator/main.tf
+++ prod-eu-west/services/datafiles-generator/main.tf
@@ -1,30 +1,30 @@
-resource "aws_cloudwatch_event_rule" "datafiles-generator" {
-  name = "datafiles-generator"
-  description = "Run datafiles-generator container via cron"
-  // schedule_expression = "cron(15 4 1 10 ? *)"
-  schedule_expression = "cron(15 4 1 10 ? *)"
-}
+# resource "aws_cloudwatch_event_rule" "datafiles-generator" {
+#   name = "datafiles-generator"
+#   description = "Run datafiles-generator container via cron"
+#   // schedule_expression = "cron(15 4 1 10 ? *)"
+#   schedule_expression = "cron(15 4 1 10 ? *)"
+# }
 
-resource "aws_cloudwatch_event_target" "datafiles-generator" {
-  target_id = "datafiles-generator"
-  arn = data.aws_ecs_cluster.default.arn
-  rule = aws_cloudwatch_event_rule.datafiles-generator.name
-  role_arn  = data.aws_iam_role.ecs_events.arn
+# resource "aws_cloudwatch_event_target" "datafiles-generator" {
+#   target_id = "datafiles-generator"
+#   arn = data.aws_ecs_cluster.default.arn
+#   rule = aws_cloudwatch_event_rule.datafiles-generator.name
+#   role_arn  = data.aws_iam_role.ecs_events.arn
 
-  ecs_target {
-    task_count          = 1
-    launch_type         = "FARGATE"
-    task_definition_arn = aws_ecs_task_definition.datafiles-generator.arn
+#   ecs_target {
+#     task_count          = 1
+#     launch_type         = "FARGATE"
+#     task_definition_arn = aws_ecs_task_definition.datafiles-generator.arn
 
-    network_configuration {
-      security_groups = [data.aws_security_group.datacite-private.id]
-      subnets         = [
-        data.aws_subnet.datacite-private.id,
-        data.aws_subnet.datacite-alt.id
-      ]
-    }
-  }
-}
+#     network_configuration {
+#       security_groups = [data.aws_security_group.datacite-private.id]
+#       subnets         = [
+#         data.aws_subnet.datacite-private.id,
+#         data.aws_subnet.datacite-alt.id
+#       ]
+#     }
+#   }
+# }
 
 resource "aws_s3_bucket" "datafiles" {
     bucket = "datafiles.datacite.org"
@@ -42,23 +42,23 @@ resource "aws_s3_bucket" "datafiles" {
     }
 }
 
-resource "aws_cloudwatch_log_group" "datafiles-generator" {
-  name = "/ecs/datafiles-generator"
-}
+# resource "aws_cloudwatch_log_group" "datafiles-generator" {
+#   name = "/ecs/datafiles-generator"
+# }
 
-resource "aws_ecs_task_definition" "datafiles-generator" {
-  family = "datafiles-generator"
-  execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
-  network_mode = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu = "512"
-  memory = "2048"
+# resource "aws_ecs_task_definition" "datafiles-generator" {
+#   family = "datafiles-generator"
+#   execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
+#   network_mode = "awsvpc"
+#   requires_compatibilities = ["FARGATE"]
+#   cpu = "512"
+#   memory = "2048"
 
-  container_definitions =  templatefile("datafiles-generator.json",
-    {
-      access_key  = var.access_key,
-      secret_key  = var.secret_key,
-      region      = var.region,
-      slack_webhook_url = var.slack_webhook_url
-    })
-}
+#   container_definitions =  templatefile("datafiles-generator.json",
+#     {
+#       access_key  = var.access_key,
+#       secret_key  = var.secret_key,
+#       region      = var.region,
+#       slack_webhook_url = var.slack_webhook_url
+#     })
+# }
