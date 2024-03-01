3761f78b1e86a8c8c50ec240892b057abd509fa8
Richard Hallett <richard@richardhallett.com>
2024-03-01 11:18:04 +0100

    prod/re3data - Upgrade tf 0.12

--- prod-eu-west/services/re3data/input.tf
+++ prod-eu-west/services/re3data/input.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
   version    = "v2.70.0"
 }
 
@@ -15,15 +15,15 @@ data "aws_route53_zone" "internal" {
 }
 
 data "aws_security_group" "datacite-private" {
-  id = "${var.security_group_id}"
+  id = var.security_group_id
 }
 
 data "aws_subnet" "datacite-private" {
-  id = "${var.subnet_datacite-private_id}"
+  id = var.subnet_datacite-private_id
 }
 
 data "aws_subnet" "datacite-alt" {
-  id = "${var.subnet_datacite-alt_id}"
+  id = var.subnet_datacite-alt_id
 }
 
 data "aws_ecs_cluster" "default" {
@@ -39,25 +39,25 @@ data "aws_iam_role" "ecs_task_execution_role" {
 }
 
 data "aws_lb" "default" {
-  name = "${var.lb_name}"
+  name = var.lb_name
 }
 
 data "aws_lb_listener" "default" {
-  load_balancer_arn = "${data.aws_lb.default.arn}"
+  load_balancer_arn = data.aws_lb.default.arn
   port = 443
 }
 
 data "template_file" "re3data_task" {
-  template = "${file("re3data.json")}"
+  template = file("re3data.json")
 
-  vars {
-    es_host            = "${var.es_host}"
-    elastic_user       = "${var.elastic_user}"
-    elastic_password   = "${var.elastic_password}"
-    access_key         = "${var.access_key}"
-    secret_key         = "${var.secret_key}"
-    sentry_dsn         = "${var.sentry_dsn}"
-    memcache_servers   = "${var.memcache_servers}"
-    version            = "${var.schnauzer_tags["version"]}"
+  vars = {
+    es_host            = var.es_host
+    elastic_user       = var.elastic_user
+    elastic_password   = var.elastic_password
+    access_key         = var.access_key
+    secret_key         = var.secret_key
+    sentry_dsn         = var.sentry_dsn
+    memcache_servers   = var.memcache_servers
+    version            = var.schnauzer_tags["version"]
   }
 }
--- prod-eu-west/services/re3data/main.tf
+++ prod-eu-west/services/re3data/main.tf
@@ -1,27 +1,27 @@
 resource "aws_ecs_service" "re3data" {
   name = "re3data"
-  cluster = "${data.aws_ecs_cluster.default.id}"
+  cluster = data.aws_ecs_cluster.default.id
   launch_type = "FARGATE"
-  task_definition = "${aws_ecs_task_definition.re3data.arn}"
+  task_definition = aws_ecs_task_definition.re3data.arn
 
   # Create service with 2 instances to start
   desired_count = 2
 
   # Allow external changes without Terraform plan difference
   lifecycle {
-    ignore_changes = ["desired_count"]
+    ignore_changes = [desired_count]
   }
 
   network_configuration {
-    security_groups = ["${data.aws_security_group.datacite-private.id}"]
+    security_groups = [data.aws_security_group.datacite-private.id]
     subnets         = [
-      "${data.aws_subnet.datacite-private.id}",
-      "${data.aws_subnet.datacite-alt.id}"
+      data.aws_subnet.datacite-private.id,
+      data.aws_subnet.datacite-alt.id
     ]
   }
 
   load_balancer {
-    target_group_arn = "${aws_lb_target_group.re3data.id}"
+    target_group_arn = aws_lb_target_group.re3data.id
     container_name   = "re3data"
     container_port   = "80"
   }
@@ -38,9 +38,9 @@ resource "aws_appautoscaling_target" "re3data" {
 resource "aws_appautoscaling_policy" "re3data_scale_up" {
   name               = "scale-up"
   policy_type        = "StepScaling"
-  resource_id        = "${aws_appautoscaling_target.re3data.resource_id}"
-  scalable_dimension = "${aws_appautoscaling_target.re3data.scalable_dimension}"
-  service_namespace  = "${aws_appautoscaling_target.re3data.service_namespace}"
+  resource_id        = aws_appautoscaling_target.re3data.resource_id
+  scalable_dimension = aws_appautoscaling_target.re3data.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.re3data.service_namespace
 
   step_scaling_policy_configuration {
     adjustment_type         = "ChangeInCapacity"
@@ -57,9 +57,9 @@ resource "aws_appautoscaling_policy" "re3data_scale_up" {
 resource "aws_appautoscaling_policy" "re3data_scale_down" {
   name               = "scale-down"
   policy_type        = "StepScaling"
-  resource_id        = "${aws_appautoscaling_target.re3data.resource_id}"
-  scalable_dimension = "${aws_appautoscaling_target.re3data.scalable_dimension}"
-  service_namespace  = "${aws_appautoscaling_target.re3data.service_namespace}"
+  resource_id        = aws_appautoscaling_target.re3data.resource_id
+  scalable_dimension = aws_appautoscaling_target.re3data.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.re3data.service_namespace
 
   step_scaling_policy_configuration {
     adjustment_type         = "ChangeInCapacity"
@@ -83,13 +83,13 @@ resource "aws_cloudwatch_metric_alarm" "re3data_memory_scale_up" {
   statistic           = "Average"
   threshold           = "85"
 
-  dimensions {
+  dimensions = {
     ClusterName = "default"
-    ServiceName = "${aws_ecs_service.re3data.name}"
+    ServiceName = aws_ecs_service.re3data.name
   }
 
   alarm_description = "This metric monitors ecs memory utilization"
-  alarm_actions     = ["${aws_appautoscaling_policy.re3data_scale_up.arn}"]
+  alarm_actions     = [aws_appautoscaling_policy.re3data_scale_up.arn]
 }
 
 resource "aws_cloudwatch_metric_alarm" "re3data_memory_scale_down" {
@@ -102,13 +102,13 @@ resource "aws_cloudwatch_metric_alarm" "re3data_memory_scale_down" {
   statistic           = "Average"
   threshold           = "70"
 
-  dimensions {
+  dimensions = {
     ClusterName = "default"
-    ServiceName = "${aws_ecs_service.re3data.name}"
+    ServiceName = aws_ecs_service.re3data.name
   }
 
   alarm_description = "This metric monitors ecs memory utilization"
-  alarm_actions     = ["${aws_appautoscaling_policy.re3data_scale_down.arn}"]
+  alarm_actions     = [aws_appautoscaling_policy.re3data_scale_down.arn]
 }
 
 resource "aws_cloudwatch_log_group" "re3data" {
@@ -117,20 +117,20 @@ resource "aws_cloudwatch_log_group" "re3data" {
 
 resource "aws_ecs_task_definition" "re3data" {
   family = "re3data"
-  execution_role_arn = "${data.aws_iam_role.ecs_task_execution_role.arn}"
+  execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
   network_mode = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu = "256"
   memory = "512"
 
-  container_definitions =  "${data.template_file.re3data_task.rendered}"
+  container_definitions =  data.template_file.re3data_task.rendered
 }
 
 resource "aws_lb_target_group" "re3data" {
   name     = "re3data"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = "${var.vpc_id}"
+  vpc_id   = var.vpc_id
   target_type = "ip"
 
   health_check {
@@ -139,21 +139,23 @@ resource "aws_lb_target_group" "re3data" {
 }
 
 resource "aws_lb_listener_rule" "re3data" {
-  listener_arn = "${data.aws_lb_listener.default.arn}"
+  listener_arn = data.aws_lb_listener.default.arn
   priority     = 34
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.re3data.arn}"
+    target_group_arn = aws_lb_target_group.re3data.arn
   }
 
   condition {
-    field  = "host-header"
-    values = ["api.datacite.org"]
+    host_header {
+      values = ["api.datacite.org"]
+    }
   }
 
   condition {
-    field  = "path-pattern"
-    values = ["/re3data*"]
+    path_pattern {
+      values = ["/re3data*"]
+    }
   }
 }
