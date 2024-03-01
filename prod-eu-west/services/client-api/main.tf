2372e95c8038fbc856387ca8cf862cc981b778f4
Richard Hallett <richard@richardhallett.com>
2024-03-01 16:37:25 +0100

    prod/client-api - Reset failure threshold
    Wanted to trigger complete replacement

--- prod-eu-west/services/client-api/main.tf
+++ prod-eu-west/services/client-api/main.tf
@@ -326,7 +326,7 @@ resource "aws_service_discovery_service" "client-api" {
   name = "client-api"
 
   health_check_custom_config {
-    failure_threshold = 6
+    failure_threshold = 3
   }
 
   dns_config {
