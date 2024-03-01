0e35f6ecd7d1c46ba1416bf73b69a68900430182
Richard Hallett <richard@richardhallett.com>
2024-03-01 12:24:46 +0100

    prod/schema - Upgrade tf 0.12

--- prod-eu-west/services/schema/main.tf
+++ prod-eu-west/services/schema/main.tf
@@ -1,20 +1,20 @@
 resource "aws_s3_bucket" "schema" {
     bucket = "schema.datacite.org"
     acl = "public-read"
-    policy = "${data.template_file.schema.rendered}"
+    policy = data.template_file.schema.rendered
     website {
         index_document = "index.html"
         error_document = "404.html"
-        routing_rules = "${file("routing-rules.json")}"
+        routing_rules = file("routing-rules.json")
     }
-    tags {
+    tags = {
         Name = "Schema"
     }
 }
 
 resource "aws_cloudfront_distribution" "schema" {
   origin {
-    domain_name = "${aws_s3_bucket.schema.website_endpoint}"
+    domain_name = aws_s3_bucket.schema.website_endpoint
     origin_id   = "schema.datacite.org"
 
     custom_origin_config {
@@ -31,7 +31,7 @@ resource "aws_cloudfront_distribution" "schema" {
 
   logging_config {
     include_cookies = false
-    bucket          = "${data.aws_s3_bucket.logs.bucket_domain_name}"
+    bucket          = data.aws_s3_bucket.logs.bucket_domain_name
     prefix          = "schema/"
   }
 
@@ -65,28 +65,28 @@ resource "aws_cloudfront_distribution" "schema" {
     }
   }
 
-  tags {
+  tags = {
     Environment = "prod"
   }
 
   viewer_certificate {
-    acm_certificate_arn = "${data.aws_acm_certificate.cloudfront.arn}"
+    acm_certificate_arn = data.aws_acm_certificate.cloudfront.arn
     ssl_support_method  = "sni-only"
   }
 }
 
 resource "aws_route53_record" "schema" {
-   zone_id = "${data.aws_route53_zone.production.zone_id}"
+   zone_id = data.aws_route53_zone.production.zone_id
    name = "schema.datacite.org"
    type = "CNAME"
-   ttl = "${var.ttl}"
-   records = ["${aws_cloudfront_distribution.schema.domain_name}"]
+   ttl = var.ttl
+   records = [aws_cloudfront_distribution.schema.domain_name]
 }
 
 resource "aws_route53_record" "split-schema" {
-   zone_id = "${data.aws_route53_zone.internal.zone_id}"
+   zone_id = data.aws_route53_zone.internal.zone_id
    name = "schema.datacite.org"
    type = "CNAME"
-   ttl = "${var.ttl}"
-   records = ["${aws_cloudfront_distribution.schema.domain_name}"]
+   ttl = var.ttl
+   records = [aws_cloudfront_distribution.schema.domain_name]
 }
