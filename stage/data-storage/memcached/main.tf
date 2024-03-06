50898036c3a120ac7ce2d16dcbec532dd369e0dc
Richard Hallett <richard@richardhallett.com>
2024-03-06 11:05:31 +0100

    stage/memcached - Upgrade tf 0.12

--- stage/data-storage/memcached/main.tf
+++ stage/data-storage/memcached/main.tf
@@ -5,8 +5,8 @@ resource "aws_elasticache_cluster" "memcached-stage" {
     node_type = "cache.m3.medium"
     port = 11211
     num_cache_nodes = 1
-    security_group_ids = ["${data.aws_security_group.datacite-private.id}"]
-    subnet_group_name = "${aws_elasticache_subnet_group.memcached-stage.name}"
+    security_group_ids = [data.aws_security_group.datacite-private.id]
+    subnet_group_name = aws_elasticache_subnet_group.memcached-stage.name
     apply_immediately = true
     lifecycle {
       prevent_destroy = "true"
@@ -16,14 +16,14 @@ resource "aws_elasticache_cluster" "memcached-stage" {
 resource "aws_elasticache_subnet_group" "memcached-stage" {
     name = "memcached-stage"
     description = "Elasticache memcached-stage subnet group"
-    subnet_ids = ["${data.aws_subnet.datacite-private.id}",
-                  "${data.aws_subnet.datacite-alt.id}"]
+    subnet_ids = [data.aws_subnet.datacite-private.id,
+                  data.aws_subnet.datacite-alt.id]
 }
 
 resource "aws_route53_record" "memcached-stage" {
-    zone_id = "${data.aws_route53_zone.internal.zone_id}"
+    zone_id = data.aws_route53_zone.internal.zone_id
     name = "memcached.stage.datacite.org"
     type = "CNAME"
-    ttl = "${var.ttl}"
-    records = ["${aws_elasticache_cluster.memcached-stage.cache_nodes.0.address}"]
+    ttl = var.ttl
+    records = [aws_elasticache_cluster.memcached-stage.cache_nodes.0.address]
 }
