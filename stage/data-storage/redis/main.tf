4c75f65f38444ee15f36ffbd1cfbf4b61d8b1e40
Richard Hallett <richard@richardhallett.com>
2024-03-08 10:45:57 +0100

    stage/redis - Upgrade tf 0.13 - Specify major.minor for version

--- stage/data-storage/redis/main.tf
+++ stage/data-storage/redis/main.tf
@@ -1,7 +1,7 @@
 resource "aws_elasticache_cluster" "redis-stage" {
   cluster_id               = "redis-stage"
   engine                   = "redis"
-  engine_version           = "6.2.6"
+  engine_version           = "6.2"
   node_type                = "cache.m4.large"
   port                     = 6379
   num_cache_nodes          = 1
