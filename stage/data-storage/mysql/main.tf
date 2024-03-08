693257e3e50ba984ea4846f0390f6f5b837a3d07
Richard Hallett <richard@richardhallett.com>
2024-03-08 10:13:05 +0100

    stage/mysql - Upgrade tf 0.12

--- stage/data-storage/mysql/main.tf
+++ stage/data-storage/mysql/main.tf
@@ -6,20 +6,20 @@ resource "aws_db_instance" "db-stage" {
   engine                      = "mysql"
   engine_version              = "5.7.21"
   instance_class              = "db.m4.large"
-  username                    = "${var.mysql_user}"
-  password                    = "${var.mysql_password}"
+  username                    = var.mysql_user
+  password                    = var.mysql_password
   maintenance_window          = "thu:00:00-thu:00:30"
   backup_window               = "01:00-01:30"
   backup_retention_period     = 8
   availability_zone           = "eu-west-1a"
   db_subnet_group_name        = "datacite"
-  vpc_security_group_ids      = ["${data.aws_security_group.datacite-private.id}"]
-  parameter_group_name        = "${aws_db_parameter_group.datacite-stage.id}"
+  vpc_security_group_ids      = [data.aws_security_group.datacite-private.id]
+  parameter_group_name        = aws_db_parameter_group.datacite-stage.id
   auto_minor_version_upgrade  = "true"
   allow_major_version_upgrade = "true"
   final_snapshot_identifier   = "db-stage-final-snapshot"
 
-  tags {
+  tags = {
     Name = "stage"
   }
 
@@ -87,33 +87,33 @@ resource "aws_db_parameter_group" "datacite-stage" {
 }
 
 resource "aws_route53_record" "internal-db-stage" {
-  zone_id = "${data.aws_route53_zone.internal.zone_id}"
+  zone_id = data.aws_route53_zone.internal.zone_id
   name    = "db.stage.datacite.org"
   type    = "CNAME"
   ttl     = "300"
-  records = ["${aws_db_instance.db-stage.address}"]
+  records = [aws_db_instance.db-stage.address]
 }
 
 resource "aws_route53_record" "internal-db-test-ec2" {
-  zone_id = "${data.aws_route53_zone.internal.zone_id}"
+  zone_id = data.aws_route53_zone.internal.zone_id
   name    = "db.test.ec2.datacite.org"
   type    = "CNAME"
   ttl     = "300"
-  records = ["${aws_db_instance.db-stage.address}"]
+  records = [aws_db_instance.db-stage.address]
 }
 
 resource "aws_route53_record" "internal-db-test" {
-  zone_id = "${data.aws_route53_zone.internal.zone_id}"
+  zone_id = data.aws_route53_zone.internal.zone_id
   name    = "db.test.datacite.org"
   type    = "CNAME"
   ttl     = "300"
-  records = ["${aws_db_instance.db-stage.address}"]
+  records = [aws_db_instance.db-stage.address]
 }
 
 resource "aws_route53_record" "rds-stage" {
-  zone_id = "${data.aws_route53_zone.internal.zone_id}"
+  zone_id = data.aws_route53_zone.internal.zone_id
   name    = "test.rds.datacite.org"
   type    = "CNAME"
   ttl     = "300"
-  records = ["${aws_db_instance.db-stage.address}"]
+  records = [aws_db_instance.db-stage.address]
 }
