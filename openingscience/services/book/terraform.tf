9cf5b6d465ee7810e28c62a370a01c416e376fe8
Richard Hallett <richard@richardhallett.com>
2024-03-11 14:03:26 +0100

    misc/openingscience - Upgrade tf 1.6

--- openingscience/services/book/terraform.tf
+++ openingscience/services/book/terraform.tf
@@ -6,9 +6,13 @@ terraform {
     }
   }
 
-  required_version = ">= 0.13"
+  required_version = ">= 1.6"
 
-  backend "atlas" {
-    name         = "datacite-ng/openingscience-services-book"
+  cloud {
+    organization = "datacite-ng"
+
+    workspaces {
+      name = "openingscience-services-book"
+    }
   }
 }
