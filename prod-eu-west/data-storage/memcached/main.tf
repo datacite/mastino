97e872cd0a0093ffd7f008026ca6a39195b113c8
Richard Hallett <richard@richardhallett.com>
2024-03-11 12:44:49 +0100

    prod/memcached - Update config

--- prod-eu-west/data-storage/memcached/main.tf
+++ prod-eu-west/data-storage/memcached/main.tf
@@ -5,8 +5,8 @@ resource "aws_elasticache_cluster" "memcached" {
     node_type = "cache.m4.large"
     port = 11211
     num_cache_nodes = 1
-    security_group_ids = ["${data.aws_security_group.datacite-private.id}"]
-    subnet_group_name = "${aws_elasticache_subnet_group.memcached.name}"
+    security_group_ids = [data.aws_security_group.datacite-private.id]
+    subnet_group_name = aws_elasticache_subnet_group.memcached.name
     apply_immediately = true
     lifecycle {
       prevent_destroy = "true"
@@ -16,14 +16,14 @@ resource "aws_elasticache_cluster" "memcached" {
 resource "aws_elasticache_subnet_group" "memcached" {
     name = "memcached"
     description = "Elasticache memcached subnet group"
-    subnet_ids = ["${data.aws_subnet.datacite-private.id}",
-                  "${data.aws_subnet.datacite-alt.id}"]
+    subnet_ids = [data.aws_subnet.datacite-private.id,
+                  data.aws_subnet.datacite-alt.id]
 }
 
 resource "aws_route53_record" "memcached1" {
-    zone_id = "${data.aws_route53_zone.internal.zone_id}"
+    zone_id = data.aws_route53_zone.internal.zone_id
     name = "memcached1.datacite.org"
     type = "CNAME"
     ttl = "30"
-    records = ["${aws_elasticache_cluster.memcached.cache_nodes.0.address}"]
+    records = [aws_elasticache_cluster.memcached.cache_nodes.0.address]
 }
