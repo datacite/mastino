7f00aafc6378ea7540a9d34fa4ba191b47e65e87
Richard Hallett <richard@richardhallett.com>
2024-03-12 11:44:06 +0100

    Remove pidnotebooks workspace
    All it does is DNS entries, this is moved to other/misc

--- pidnotebooks/stage/var.tf
+++
@@ -1,9 +0,0 @@
-variable "access_key" {}
-variable "secret_key" {}
-variable "region" {
-    default = "eu-west-1"
-}
-
-variable "github_pages_records" {
-  default = ["185.199.108.153","185.199.109.153","185.199.110.153","185.199.111.153"]
-}
