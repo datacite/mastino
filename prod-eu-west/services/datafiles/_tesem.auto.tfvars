7aac7a877e4792f50d2d2f1e62064854c736a20e
Mike Bennett <mbennett@datacite.org>
2024-03-21 03:10:07 +0000

    add first pass datafiles production code

---
+++ prod-eu-west/services/datafiles/_tesem.auto.tfvars
@@ -0,0 +1,4 @@
+tesem_tags = {
+  sha = "b2be8b1aa858e33e148156a5b72a4949130bbb6a"
+  version = "main"
+}
---
+++ prod-eu-west/services/datafiles/var.tf
@@ -0,0 +1,37 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+  default = "eu-west-1"
+}
+
+variable "vpc_id" {}
+
+variable "tesem_tags" {
+  type = map(string)
+}
+
+variable "lb_name" {
+  default = "lb"
+}
+
+variable "ttl" {
+  default = "300"
+}
+
+variable "database_url" {}
+
+variable "jwt_secret_key" {}
+variable "tesem_secret_key" {}
+
+variable "sentry_dsn" {}
+variable "mailgun_api_key" {}
+
+variable "security_group_id" {}
+variable "subnet_datacite-private_id" {}
+variable "subnet_datacite-alt_id" {}
+
+variable "dns_name" {
+  default = "datafiles.datacite.org"
+}
+
+variable "namespace_id" {}
