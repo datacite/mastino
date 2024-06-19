d71a8597e961a5007ff0249eb779d8e60771acf6
Richard Hallett <richard@richardhallett.com>
2024-06-19 14:17:00 +0200

    prod/levriero - Bump min container capacity

--- prod-eu-west/services/levriero/main.tf
+++ prod-eu-west/services/levriero/main.tf
@@ -36,8 +36,8 @@ resource "aws_ecs_service" "levriero" {
 }
 
 resource "aws_appautoscaling_target" "levriero" {
-  max_capacity       = 12
-  min_capacity       = 12
+  max_capacity       = 20
+  min_capacity       = 16
   resource_id        = "service/default/${aws_ecs_service.levriero.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
