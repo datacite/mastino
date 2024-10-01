40218d6268c73db80cdf4becd9d09355d485f044
Richard Hallett <richard@richardhallett.com>
2024-10-01 15:04:31 +0200

    Add AwsNATIPSet allow rule

--- prod-eu-west/vpc/waf.tf
+++ prod-eu-west/vpc/waf.tf
@@ -7,6 +7,14 @@
 #   }
 # }
 
+resource "aws_wafv2_ip_set" "nat" {
+  name               = "natIPSet"
+  description        = "NAT IP set"
+  scope              = "REGIONAL"
+  ip_address_version = "IPV4"
+  addresses          = [var.waf_nat_ip]
+}
+
 # resource "aws_wafregional_ipset" "whitelist" {
 #   name = "whitelistIPSet"
 
@@ -154,8 +162,29 @@ resource "aws_wafv2_web_acl" "prod-default" {
   }
 
   rule {
-    name     = "prodRateLimitingRule"
+    name     = "natIpSetAllow"
     priority = 1
+
+    action {
+      allow {}
+    }
+
+    statement {
+      ip_set_reference_statement {
+        arn = aws_wafv2_ip_set.nat.arn
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "natIpSetAllow"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "prodRateLimitingRule"
+    priority = 2
 
     action {
       block {}
