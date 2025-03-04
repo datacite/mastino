d29593badf9e110d32a2852fa4991c8957b0ab65
Richard Hallett <richard@richardhallett.com>
2025-03-04 16:53:18 +0100

    Disable automatic sitemap generation
    This is for review of what we need here and best approaches.

--- prod-eu-west/services/sitemaps-generator/main.tf
+++ prod-eu-west/services/sitemaps-generator/main.tf
@@ -1,13 +1,13 @@
-resource "aws_cloudwatch_event_rule" "sitemaps-generator" {
-  name = "sitemaps-generator"
-  description = "Run sitemaps-generator container via cron"
-  schedule_expression = "cron(50 9 ? * MON *)"
-}
+# resource "aws_cloudwatch_event_rule" "sitemaps-generator" {
+#   name = "sitemaps-generator"
+#   description = "Run sitemaps-generator container via cron"
+#   schedule_expression = "cron(50 9 ? * MON *)"
+# }
 
 resource "aws_cloudwatch_event_target" "sitemaps-generator" {
   target_id = "sitemaps-generator"
-  arn = data.aws_ecs_cluster.default.arn
-  rule = aws_cloudwatch_event_rule.sitemaps-generator.name
+  arn       = data.aws_ecs_cluster.default.arn
+  rule      = aws_cloudwatch_event_rule.sitemaps-generator.name
   role_arn  = data.aws_iam_role.ecs_events.arn
 
   ecs_target {
@@ -17,7 +17,7 @@ resource "aws_cloudwatch_event_target" "sitemaps-generator" {
 
     network_configuration {
       security_groups = [data.aws_security_group.datacite-private.id]
-      subnets         = [
+      subnets = [
         data.aws_subnet.datacite-private.id,
         data.aws_subnet.datacite-alt.id
       ]
@@ -26,19 +26,19 @@ resource "aws_cloudwatch_event_target" "sitemaps-generator" {
 }
 
 resource "aws_s3_bucket" "akita" {
-    bucket = "commons.datacite.org"
-    acl = "public-read"
-    policy = templatefile("s3_public_read.json",
-      {
-        vpce_id = data.aws_vpc_endpoint.datacite.id
-        bucket_name = "commons.datacite.org"
-      })
-    website {
-        index_document = "index.html"
-    }
-    tags = {
-        Name = "Commons"
-    }
+  bucket = "commons.datacite.org"
+  acl    = "public-read"
+  policy = templatefile("s3_public_read.json",
+    {
+      vpce_id     = data.aws_vpc_endpoint.datacite.id
+      bucket_name = "commons.datacite.org"
+  })
+  website {
+    index_document = "index.html"
+  }
+  tags = {
+    Name = "Commons"
+  }
 }
 
 resource "aws_cloudwatch_log_group" "sitemaps-generator" {
@@ -46,18 +46,18 @@ resource "aws_cloudwatch_log_group" "sitemaps-generator" {
 }
 
 resource "aws_ecs_task_definition" "sitemaps-generator" {
-  family = "sitemaps-generator"
-  execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
-  network_mode = "awsvpc"
+  family                   = "sitemaps-generator"
+  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
+  network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu = "512"
-  memory = "2048"
+  cpu                      = "512"
+  memory                   = "2048"
 
   container_definitions = templatefile("sitemaps-generator.json",
     {
-      access_key  = var.access_key,
-      secret_key  = var.secret_key,
-      region      = var.region,
+      access_key        = var.access_key,
+      secret_key        = var.secret_key,
+      region            = var.region,
       slack_webhook_url = var.slack_webhook_url
-    })
+  })
 }
--- stage/services/sitemaps-generator/main.tf
+++ stage/services/sitemaps-generator/main.tf
@@ -1,23 +1,23 @@
-resource "aws_cloudwatch_event_rule" "sitemaps-generator-stage" {
-  name = "sitemaps-generator-stage"
-  description = "Run sitemaps-generator-stage container via cron"
-  schedule_expression = "cron(50 8 ? * SUN *)"
-}
+# resource "aws_cloudwatch_event_rule" "sitemaps-generator-stage" {
+#   name = "sitemaps-generator-stage"
+#   description = "Run sitemaps-generator-stage container via cron"
+#   schedule_expression = "cron(50 8 ? * SUN *)"
+# }
 
 resource "aws_cloudwatch_event_target" "sitemaps-generator-stage" {
   target_id = "sitemaps-generator-stage"
-  arn = data.aws_ecs_cluster.stage.arn
-  rule = aws_cloudwatch_event_rule.sitemaps-generator-stage.name
+  arn       = data.aws_ecs_cluster.stage.arn
+  rule      = aws_cloudwatch_event_rule.sitemaps-generator-stage.name
   role_arn  = data.aws_iam_role.ecs_events-stage.arn
 
   ecs_target {
     task_count          = 1
     launch_type         = "FARGATE"
     task_definition_arn = aws_ecs_task_definition.sitemaps-generator-stage.arn
-    
+
     network_configuration {
       security_groups = [data.aws_security_group.datacite-private.id]
-      subnets         = [
+      subnets = [
         data.aws_subnet.datacite-private.id,
         data.aws_subnet.datacite-alt.id
       ]
@@ -26,19 +26,19 @@ resource "aws_cloudwatch_event_target" "sitemaps-generator-stage" {
 }
 
 resource "aws_s3_bucket" "akita-stage" {
-    bucket = "commons.stage.datacite.org"
-    acl = "public-read"
-    policy = templatefile("s3_public_read.json",
-      {
-        vpce_id = data.aws_vpc_endpoint.datacite.id
-        bucket_name = "commons.stage.datacite.org"
-      })
-    website {
-        index_document = "index.html"
-    }
-    tags = {
-        Name = "Commons Stage"
-    }
+  bucket = "commons.stage.datacite.org"
+  acl    = "public-read"
+  policy = templatefile("s3_public_read.json",
+    {
+      vpce_id     = data.aws_vpc_endpoint.datacite.id
+      bucket_name = "commons.stage.datacite.org"
+  })
+  website {
+    index_document = "index.html"
+  }
+  tags = {
+    Name = "Commons Stage"
+  }
 }
 
 resource "aws_cloudwatch_log_group" "sitemaps-generator-stage" {
@@ -46,18 +46,18 @@ resource "aws_cloudwatch_log_group" "sitemaps-generator-stage" {
 }
 
 resource "aws_ecs_task_definition" "sitemaps-generator-stage" {
-  family = "sitemaps-generator-stage"
-  execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
-  network_mode = "awsvpc"
+  family                   = "sitemaps-generator-stage"
+  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
+  network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu = "256"
-  memory = "1024"
+  cpu                      = "256"
+  memory                   = "1024"
 
-  container_definitions =  templatefile("sitemaps-generator.json",
+  container_definitions = templatefile("sitemaps-generator.json",
     {
-      access_key  = var.access_key,
-      secret_key  = var.secret_key,
-      region      = var.region,
+      access_key        = var.access_key,
+      secret_key        = var.secret_key,
+      region            = var.region,
       slack_webhook_url = var.slack_webhook_url
-    })
+  })
 }
