21c6c9d94a7b266d30360fd022af2f93099205b0
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-06-12 12:27:00 +0200

    Set the target threshold very high

--- prod-eu-west/services/events/main.tf
+++ prod-eu-west/services/events/main.tf
@@ -49,7 +49,7 @@ resource "aws_appautoscaling_policy" "events_sqs" {
   service_namespace  = aws_appautoscaling_target.events.service_namespace
 
   target_tracking_scaling_policy_configuration {
-    target_value = 100000
+    target_value = 300000
     predefined_metric_specification {
       predefined_metric_type = "SQSQueueMessagesVisible"
       resource_label         = aws_sqs_queue.events.name
