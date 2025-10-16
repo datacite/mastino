bd7b4129c91d3a08f1243b322b62f82920669dc7
codycooperross <50597551+codycooperross@users.noreply.github.com>
2025-10-16 16:57:12 -0400

    Reduce queue-worker max

--- prod-eu-west/services/client-api/queue-worker.tf
+++ prod-eu-west/services/client-api/queue-worker.tf
@@ -93,7 +93,7 @@ resource "aws_service_discovery_service" "queue-worker" {
 
 // Autoscaling target for queue-worker service
 resource "aws_appautoscaling_target" "queue-worker" {
-  max_capacity       = 40
+  max_capacity       = 30
   min_capacity       = 1
   resource_id        = "service/default/${aws_ecs_service.queue-worker.name}"
   scalable_dimension = "ecs:service:DesiredCount"
