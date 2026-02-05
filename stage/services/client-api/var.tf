69dd5213629853b5a4f94e01d6f4b7387d2d1cf7
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2026-02-05 17:44:04 +0200

    Remove stage specific bucket

--- stage/services/client-api/var.tf
+++ stage/services/client-api/var.tf
@@ -105,5 +105,5 @@ variable "monthly_datafile_bucket" {
 variable "monthly_datafile_access_role" {}
 
 variable "enrichments_ingestion_files_bucket_name" {
-  default = "stage-enrichments-ingestion-files"
+  default = "enrichments-ingestion-files"
 }
