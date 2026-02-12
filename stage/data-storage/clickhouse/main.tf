45d678cba5d9189bd6c07a9849871c498c03b85d
Richard Hallett <richard@richardhallett.com>
2026-02-12 12:37:55 +0100

    stage/ch - Use docker env vars to setup a default password instead

--- stage/data-storage/clickhouse/main.tf
+++ stage/data-storage/clickhouse/main.tf
@@ -18,11 +18,12 @@ resource "aws_ecs_task_definition" "clickhouse-stage" {
   requires_compatibilities = ["EC2"]
 
   container_definitions = templatefile("clickhouse.json", {
-    clickhouse_config  = base64encode(templatefile("${path.module}/config/clickhouse-config.xml", {datacite_clickhouse_password = var.datacite_clickhouse_password}))
+    clickhouse_config  = base64encode(data.local_file.clickhouse-config.content)
     access_key         = var.access_key
     secret_key         = var.secret_key
     region             = var.region
     public_key         = var.public_key
+    clickhouse_password = var.datacite_clickhouse_password
   })
 
   volume {
