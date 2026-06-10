e4770494cccc4aaa040d9113b826e17713266a80
Mike Bennett <mbennett@datacite.org>
2026-06-10 17:37:55 +0100

    Enable new scaling configuration for member-api

--- prod-eu-west/services/client-api/member.tf
+++ prod-eu-west/services/client-api/member.tf
@@ -174,7 +174,7 @@ resource "aws_appautoscaling_policy" "member-api-worker_util_scale_up" {
 
   step_scaling_policy_configuration {
     adjustment_type         = "ChangeInCapacity"
-    cooldown                = 120 # TODO: Evaluate this during alarm testing
+    cooldown                = 120
     metric_aggregation_type = "Average"
 
     step_adjustment {
@@ -193,7 +193,7 @@ resource "aws_appautoscaling_policy" "member-api-worker_util_scale_down" {
 
   step_scaling_policy_configuration {
     adjustment_type         = "ChangeInCapacity"
-    cooldown                = 300 # TODO: Evaluate this during alarm testing
+    cooldown                = 300
     metric_aggregation_type = "Maximum"
 
     step_adjustment {
@@ -213,7 +213,7 @@ resource "aws_appautoscaling_policy" "member-api-emergency_scale_up" {
 
   step_scaling_policy_configuration {
     adjustment_type         = "ChangeInCapacity"
-    cooldown                = 120 # TODO: Evaluate this during alarm testing
+    cooldown                = 120
     metric_aggregation_type = "Maximum"
 
     step_adjustment {
@@ -248,10 +248,10 @@ resource "aws_appautoscaling_policy" "member-api-response_time_scale_up" {
 resource "aws_cloudwatch_metric_alarm" "member-api-worker_util_scale_up" {
   alarm_name          = "member-api-worker-utilisation-high"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "3" # TODO: Evaluate this during alarm testing
+  evaluation_periods  = "3"
   metric_name         = "PassengerWorkerUtilisation"
   namespace           = "Custom/LupoPassenger"
-  period              = "60" # TODO: Evaluate this during alarm testing
+  period              = "60"
   statistic           = "Average"
   threshold           = 70
 
@@ -261,20 +261,19 @@ resource "aws_cloudwatch_metric_alarm" "member-api-worker_util_scale_up" {
 
   alarm_description = "Scale up member-api when average worker utilisation is high"
   alarm_actions     = [
-    #aws_appautoscaling_policy.member-api-worker_util_scale_up.arn,
+    aws_appautoscaling_policy.member-api-worker_util_scale_up.arn,
     aws_sns_topic.member-api-scaling-alarms.arn
   ]
   ok_actions = [aws_sns_topic.member-api-scaling-alarms.arn]
-  #actions_enabled   = false  # TODO: Remove this once alarms are verified
 }
 
 resource "aws_cloudwatch_metric_alarm" "member-api-worker_util_scale_down" {
   alarm_name          = "member-api-worker-utilisation-low"
   comparison_operator = "LessThanOrEqualToThreshold"
-  evaluation_periods  = "2" # TODO: Evaluate this during alarm testing
+  evaluation_periods  = "2"
   metric_name         = "PassengerWorkerUtilisation"
   namespace           = "Custom/LupoPassenger"
-  period              = "300" # TODO: Evaluate this during alarm testing
+  period              = "300"
   statistic           = "Maximum"
   threshold           = 35
 
@@ -284,21 +283,20 @@ resource "aws_cloudwatch_metric_alarm" "member-api-worker_util_scale_down" {
 
   alarm_description = "Scale down member-api when max worker utilisation has lowered"
   alarm_actions     = [
-    #aws_appautoscaling_policy.member-api-worker_util_scale_down.arn,
+    aws_appautoscaling_policy.member-api-worker_util_scale_down.arn,
     aws_sns_topic.member-api-scaling-alarms.arn
   ]
   ok_actions = [aws_sns_topic.member-api-scaling-alarms.arn]
-  #actions_enabled   = false # TODO: Remove this once alarms are verified
 }
 
 ## Queue Size
 resource "aws_cloudwatch_metric_alarm" "member-api-queue_size_scale_up" {
   alarm_name          = "member-api-queue-size-high"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "1" # TODO: Evaluate this during alarm testing
+  evaluation_periods  = "1"
   metric_name         = "PassengerRequestQueue"
   namespace           = "Custom/LupoPassenger"
-  period              = "60" # TODO: Evaluate this during alarm testing
+  period              = "60"
   statistic           = "Maximum"
   threshold           = 1
 
@@ -308,11 +306,10 @@ resource "aws_cloudwatch_metric_alarm" "member-api-queue_size_scale_up" {
 
   alarm_description = "Emergency scale up: requests are queuing in member-api"
   alarm_actions     = [
-    #aws_appautoscaling_policy.member-api-emergency_scale_up.arn,
+    aws_appautoscaling_policy.member-api-emergency_scale_up.arn,
     aws_sns_topic.member-api-scaling-alarms.arn
   ]
   ok_actions = [aws_sns_topic.member-api-scaling-alarms.arn]
-  #actions_enabled = false # TODO: Remove this once alarms are verified
 }
 
 resource "aws_cloudwatch_metric_alarm" "member-api-response_time_scale_up" {
@@ -343,11 +340,10 @@ resource "aws_cloudwatch_metric_alarm" "member-api-response_time_scale_up" {
 
   alarm_description = "Safety net: scale up member-api when P95 response time exceeds 750ms"
   alarm_actions     = [
-    #aws_appautoscaling_policy.member-api-response_time_scale_up.arn,
+    aws_appautoscaling_policy.member-api-response_time_scale_up.arn,
     aws_sns_topic.member-api-scaling-alarms.arn
   ]
   ok_actions = [aws_sns_topic.member-api-scaling-alarms.arn]
-  #actions_enabled = false # TODO: Remove this once alarms are verified
 }
 
 resource "aws_cloudwatch_log_group" "member-api" {
