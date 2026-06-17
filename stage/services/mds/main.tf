9c2c6c05a806bfdfebe881d9b858c3a5e1e4fb25
Richard Hallett <richard@richardhallett.com>
2026-06-17 18:14:26 +0200

    stage/mds - Remove health check deprecated config

--- stage/services/mds/main.tf
+++ stage/services/mds/main.tf
@@ -100,10 +100,6 @@ resource "aws_lb_listener_rule" "mds-stage" {
 resource "aws_service_discovery_service" "mds-stage" {
   name = "mds.stage"
 
-  health_check_custom_config {
-    failure_threshold = 3
-  }
-
   dns_config {
     namespace_id = var.namespace_id
 
